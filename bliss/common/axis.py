22002f710ecf1d047a389c7eea49e12aabc1c2ed
Matias Guijarro <matias.guijarro@esrf.fr>
2014-01-20 15:12:46 +0100

    added backlash handling

--- bliss/common/axis.py
+++ bliss/common/axis.py
@@ -13,6 +13,7 @@ class Axis(object):
     def get(*args, **kwargs):
       pass
 
+
   def __init__(self, name, controller, config):
     self.__name = name
     self.__controller = controller
@@ -20,27 +21,34 @@ class Axis(object):
     self.__settings = Axis.Settings()
     self.__move_task = None
 
+
   @property
   def name(self):
     return self.__name
+
 
   @property
   def controller(self):
     return self.__controller
 
+
   @property
   def config(self):
     return self.__config
+
 
   @property
   def settings(self):
     return self.__settings
 
+
   def measured_position(self):
     return self.__controller.read_position(self, measured=True)
 
+
   def is_moving(self):
     return self.__move_task is not None and not self.__move_task.ready()
+
 
   def position(self):
     if self.is_moving():
@@ -48,16 +56,19 @@ class Axis(object):
     # really read from hw
     return self.__controller.read_position(self)
 
+
   def state(self):
     if self.is_moving():
       return MOVING
     # really read from hw
     return self.__controller.read_state(self)
 
+
   def velocity(self):
     return self.__controller.read_velocity(self)
 
-  def _handle_move(self):
+
+  def _handle_move(self, target_pos, delta, backlash=0):
     def update_settings():
        pos = self.__controller.read_position(self)
        self.settings.set("position", pos)
@@ -74,28 +85,57 @@ class Axis(object):
           if state != MOVING:
             break
           time.sleep(0.02)
-       
+
+        if backlash:
+          # axis has moved to target pos - backlash;
+          # now do the final motion to reach original target
+          final_pos = target_pos + backlash
+          self.__controller.prepare_move(self, final_pos, backlash)
+          self.__controller.start_move(self, final_pos, backlash)
+          self._handle_move(final_pos, backlash)
+    
+
   def prepare_move(self, target_pos):
+    step_size = self.config.get("step_size", float, 1)
+
+    initial_pos      = self.position()
+    # all positions are converted to controller units
+    backlash         = self.config.get("backlash", float, 0) / step_size
+    delta            = (target_pos - initial_pos) / step_size
+    target_pos       = target_pos / step_size
+    
+    if backlash:
+      if cmp(delta, 0) != cmp(backlash, 0):
+        # move and backlash are not in the same direction;
+        # apply backlash correction, the move will happen
+        # in 2 steps
+        target_pos -= backlash
+        delta -= backlash
+      else:
+        # don't do backlash correction
+        backlash = 0
+    
+    self.__controller.prepare_move(self, target_pos, delta)
+
+    return target_pos, delta, backlash
+
+ 
+  def move(self, user_target_pos, wait=True):
     initial_state = self.state()
     if initial_state != READY:
       raise RuntimeError, "motor %s state is %r" % (self.name, initial_state)
 
-    initial_pos      = self.position()
-    delta            = target_pos - initial_pos
-    self.__controller.prepare_move(self, target_pos, delta)
-    return initial_pos, target_pos, delta
- 
-  def move(self, target_pos, wait=True):
-    initial_pos, target_pos, delta = self.prepare_move(target_pos)
+    target_pos, delta, backlash = self.prepare_move(user_target_pos)
 
     self.__controller.start_move(self, target_pos, delta)
     
-    self.__move_task = gevent.spawn(self._handle_move)
+    self.__move_task = gevent.spawn(self._handle_move, target_pos, delta, backlash)
 
     if wait: 
       self.__move_task.get()
     else:
       return self.__move_task
+
 
   def stop(self):
     if self.is_moving():

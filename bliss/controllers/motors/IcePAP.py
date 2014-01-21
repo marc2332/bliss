d5993e6856103248cf967daff99607b8e0ccc65a
Manuel Perez <manuel.perez@esrf.fr>
2014-01-21 11:35:47 +0100

    Add axis motion to IcePAP controller

--- bliss/controllers/motors/IcePAP.py
+++ bliss/controllers/motors/IcePAP.py
@@ -52,10 +52,14 @@ class IcePAP(Controller):
 
   def finalize(self):
     """Controller no more needed"""	  
+    # Remove any group in the IcePAP lib
     try:
       self.libgroup.delete()
     except:
       pass
+
+    # Close IcePAP lib socket/threads
+    #self.libdevice.close()
 
 
   def initialize_axis(self, axis):
@@ -63,7 +67,6 @@ class IcePAP(Controller):
 
     # Get axis config from bliss config
     axis.address  = axis.config.get("address", int)
-    axis.step_size= axis.config.get("step_size", float)
 
     # Create an IcePAP lib axis object
     device        = self.libdevice
@@ -76,132 +79,63 @@ class IcePAP(Controller):
 
 
   def read_position(self, axis):
-    """Returns axis position in user units"""
+    """Returns axis position in motor units"""
 
     # The axis can only be accessed through a group in IcePAP lib
     # Use the default group
     pos_stps = self.libgroup.pos([axis.libaxis])[axis.libaxis]
 
     # Position unit convertion
-    return pos_stps/axis.step_size
+    return pos_stps
 
 
   def read_velocity(self, axis):
     """Returns axis current velocity in user units per seconds"""
+
     """    
+    # TODO
     return self.axis_settings.get(axis, "velocity")
     """    
 
+
   def read_state(self, axis):
     """Returns the current axis state"""
+
+    # The axis can only be accessed through a group in IcePAP lib
+    # Use the default group
+    status = self.libgroup.status([axis.libaxis])[axis.libaxis]
+
+    # Convert status formats
+    if(icepap.lib.status_moving(status)):
+      return MOVING
+
+    # TODO: check that instead of considering it as a default
     return READY
-    """    
-    if self._get_closed_loop_status():
-      if self._get_on_target_status():
-        return READY
-      else:
-        return MOVING
-    else:
-      raise RuntimeError("closed loop disabled")
-    """    
+
 
   def prepare_move(self, axis, target_pos, delta):
-    """Called once before an axis motion"""
+    """Called once before an axis motion, positions in motor units"""
 
 
   def start_move(self, axis, target_pos, delta):
-    """Launch an axis moition, returns immediately"""
-    """    
-    self.sock.write("MOV 1 %g\n"%self._target_pos)
-    """    
+    """Launch an axis moition, returns immediately, positions in motor units"""
+    target_positions = icepap.lib.PosList()
+    target_positions[axis.libaxis] = target_pos
+    self.libgroup.move(target_positions)
+
 
   def stop(self, axis):
     """Stops smoothly an axis motion"""
-    """    
-    # to check : copy of current position into target position ???
-    self.sock.write("STP\n")
-    """    
+    self.libgroup.stop([axis.libaxis])
 
-  def stop(self, axis):
+
+  def abort(self, axis):
     """Stops as fast as possible an axis motion, emergency stop"""
 
 
+  def _set_lib_verbose_level(self, val): 
+    """Change IcePAP library verbose level"""
+    self.libdevice.set_verbose_level(val)
 
-"""
-  def _get_pos(self, measured=False):
-    if measured:
-      _ans = self.sock.write_readline("POS?\n")
-    else:
-      _ans = self.sock.write_readline("MOV?\n")
-
-    # _ans should looks like "1=-8.45709419e+01\n"
-    # "\n" removed by tcp lib.
-    _pos = float(_ans[2:])
-
-    return _pos
-
-  def _get_identifier(self):
-    return self.sock.write_readline("IDN?\n")
-
-  def _get_closed_loop_status(self):
-    _ans = self.sock.write_readline("SVO?\n")
-
-    if _ans == "1=1":
-      return True
-    elif _ans == "1=0":
-      return False
-    else:
-      return -1
-
-  def _get_on_target_status(self):
-    _ans = self.sock.write_readline("ONT?\n")
-
-    if _ans =="":
-      return True
-    elif _ans =="":
-      return False
-    else:
-      return -1
-
-  def _get_error(self):
-    _error_number = self.sock.write_readline("ERR?\n")
-    _error_str = pi_gcs.get_error_str(_error_number)
-
-    return (_error_number, _error_str)
-
-  def _get_infos(self):
-    _infos = [
-      ("identifier                 ", "IDN?\n"),
-      ("com level                  ", "CCL?\n"),
-      ("Real Position              ", "POS?\n"),
-      ("Setpoint Position          ", "MOV?\n"),
-      ("Position low limit         ", "SPA? 1 0x07000000\n"),
-      ("Position High limit        ", "SPA? 1 0x07000001\n"),
-      ("Velocity                   ", "VEL?\n"),
-      ("On target                  ", "ONT?\n"),
-      ("target tolerance           ", "SPA? 1 0X07000900\n"),
-      ("Sensor Offset              ", "SPA? 1 0x02000200\n"),
-      ("Sensor Gain                ", "SPA? 1 0x02000300\n"),
-      ("Motion status              ", "#5\n"),
-      ("Closed loop status         ", "SVO?\n"),
-      ("Auto Zero Calibration ?    ", "ATZ?\n"),
-      ("Low  Voltage Limit         ", "SPA? 1 0x07000A00\n"),
-      ("High Voltage Limit         ", "SPA? 1 0x07000A01\n")
-    ]
-
-    self.sock.flush()
-
-    _txt = ""
-
-    for i in _infos:
-      _txt = _txt + "    %s %s\n"%(i[0],
-                        self.sock.write_readline(i[1]))
-
-    self.sock.write("TAD?\n")
-    _txt = _txt + "    %s   %s \n"%("ADC value of analog input",
-                                    self.sock.raw_read())
-
-    return _txt
-"""
 
 
--- tests/motors/TestIcePAP.py
+++ tests/motors/TestIcePAP.py
@@ -32,10 +32,11 @@ config_xml = """
 <config>
   <controller class="IcePAP" name="test">
     <host value="%s"/>
-    <libdebug value="1"/>
+    <libdebug value="3"/>
     <axis name="mymot">
-      <address value="%s"/>
+      <address   value="%s"/>
       <step_size value="2000"/>
+      <backlash  value="0.01"/>
     </axis>
   </controller>
 </config>
@@ -68,7 +69,7 @@ class TestIcePAPController(unittest.TestCase):
 
   def test_get_position(self):
     mymot = bliss.get_axis("mymot")
-    print "\"mymot\" position :", mymot.position()
+    pos   = mymot.position()
 
   """
   def test_get_id(self):
@@ -76,15 +77,28 @@ class TestIcePAPController(unittest.TestCase):
     print "\"mymot\" ID:", mymot.controller._get_identifier()
   """
 
+  def test_axis_state(self):
+    mymot = bliss.get_axis("mymot")
+    mymot.state()
 
-#     def test_axis_move(self):
-#         mymot = bliss.get_axis("mymot")
-#         self.assertEqual(mymot.state(), "READY")
-#         move_greenlet=mymot.move(10, wait=False)
-#         self.assertEqual(mymot.state(), "MOVING")
-#         move_greenlet.join()
-#         self.assertEqual(mymot.state(), "READY")
+  def test_axis_stop(self):
+    mymot = bliss.get_axis("mymot")
+    mymot.stop()
 
+  def test_axis_move(self):
+    mymot = bliss.get_axis("mymot")
+    pos   = mymot.position()
+    mymot.controller._set_lib_verbose_level(3)
+    mymot.move(pos+0.1)
+
+  def test_axis_move_backlash(self):
+    mymot = bliss.get_axis("mymot")
+    pos   = mymot.position()
+    mymot.move(pos-0.1)
+
+  def test_axis_rmove(self):
+    mymot = bliss.get_axis("mymot")
+    mymot.rmove(0.1)
 
 
 """
@@ -93,7 +107,7 @@ Main entry point
 if __name__ == '__main__':
 
   # Get arguments
-  usage  = "Usage: %prog [options] hostname mot_address"
+  usage  = "Usage: %prog [options] hostname motor_address"
   parser = optparse.OptionParser(usage)
   argv   = sys.argv
   (settings, args) = parser.parse_args(argv)
@@ -111,26 +125,17 @@ if __name__ == '__main__':
   del sys.argv[1:]
 
   # Launch the tests sequence
-  unittest.main()
+  print "\nTesting IcePAP control on system \"%s\"\n"%hostname
+  print "\n".rjust(70,"-")
 
+  # Change the default unittest test sequence order from cmp() to line number
+  loader = unittest.TestLoader()
+  ln = lambda f: getattr(TestIcePAPController, f).\
+		         im_func.func_code.co_firstlineno
+  lncmp = lambda a, b: cmp(ln(a), ln(b))
+  loader.sortTestMethodsUsing = lncmp
 
-'''
-Interactive test :
-
-load_cfg_fromstring("""<config>
-  <controller class="IcePAP" name="testid16">
-    <host value="e753id16ni-mlm"/>
-    <axis name="mymot">
-    </axis>
-  </controller>
-</config>
-""")
-
-a=get_axis("mymot")
-
-print a.controller.sock.write_readline("IDN?\n")
-print a.controller._get_infos()
-
-
-
-'''
+  # NOTE: unittest.main(verbosity=2) not supported under Python 2.6
+  suite  = loader.loadTestsFromTestCase(TestIcePAPController)
+  unittest.TextTestRunner(verbosity=3).run(suite)
+ 

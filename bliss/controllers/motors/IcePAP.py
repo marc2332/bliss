74654600ad2c05140c6f5d3c9be09129e22186b4
Matias Guijarro <matias.guijarro@esrf.fr>
2014-02-25 14:10:18 +0100

    continuing with making code PEP8 compliant

--- bliss/controllers/motors/IcePAP.py
+++ bliss/controllers/motors/IcePAP.py
@@ -4,9 +4,6 @@ Bliss generic library
 from bliss.controllers.motor import Controller
 from bliss.controllers.motor import add_axis_method
 from bliss.common.axis import READY, MOVING, UNKNOWN
-from bliss.common.task_utils import task, error_cleanup, cleanup
-
-
 
 
 """
@@ -15,166 +12,150 @@ IcePAP specific library
 import icepap.lib
 
 
-
 """
 """
-import random
-import math
-import time
-import pdb
+#import pdb
 
 
 class IcePAP(Controller):
-  """Implement IcePAP stepper motor controller access"""
 
-  def __init__(self, name, config, axes):
-    """Contructor"""
-    Controller.__init__(self, name, config, axes)
+    """Implement IcePAP stepper motor controller access"""
 
+    def __init__(self, name, config, axes):
+        """Contructor"""
+        Controller.__init__(self, name, config, axes)
 
-  def initialize(self):
-    """Controller initialization"""
+    def initialize(self):
+        """Controller initialization"""
 
-    # Get controller config from bliss config
-    # Mandatory parameters
-    self.host = self.config.get("host")
+        # Get controller config from bliss config
+        # Mandatory parameters
+        self.host = self.config.get("host")
 
-    # Optional parameters
-    try:
-      self.libdebug = int(self.config.get("libdebug"))
-    except:
-      self.libdebug = 1
+        # Optional parameters
+        try:
+            self.libdebug = int(self.config.get("libdebug"))
+        except:
+            self.libdebug = 1
 
-    # Create an IcePAP lib object to access the MASTER
-    self.libdevice = icepap.lib.System(self.host, "verb=%d"%self.libdebug)
+        # Create an IcePAP lib object to access the MASTER
+        self.libdevice = icepap.lib.System(
+            self.host,
+            "verb=%d" %
+            self.libdebug)
 
-    # Create an IcePAP lib object as default group
-    self.libgroup  = icepap.lib.Group("default")
+        # Create an IcePAP lib object as default group
+        self.libgroup = icepap.lib.Group("default")
 
+    def finalize(self):
+        """Controller no more needed"""
+        #import pdb;pdb.set_trace()
+        # Remove any group in the IcePAP lib
+        try:
+            self.libgroup.delete()
+        except:
+            pass
 
-  def finalize(self):
-    """Controller no more needed"""	  
-    #import pdb;pdb.set_trace()
-    # Remove any group in the IcePAP lib
-    try:
-      self.libgroup.delete()
-    except:
-      pass
+        # Close IcePAP lib socket/threads
+        self.libdevice.close()
 
-    # Close IcePAP lib socket/threads
-    self.libdevice.close()
+    def initialize_axis(self, axis):
+        """Axis initialization"""
 
+        # Get axis config from bliss config
+        axis.address = axis.config.get("address", int)
 
-  def initialize_axis(self, axis):
-    """Axis initialization"""
+        # Create an IcePAP lib axis object
+        device = self.libdevice
+        address = axis.address
+        name = axis.name
+        axis.libaxis = icepap.lib.Axis(device, address, name)
 
-    # Get axis config from bliss config
-    axis.address  = axis.config.get("address", int)
+        # Add the axis to the default IcePAP lib group
+        self.libgroup.add_axis(axis.libaxis)
 
-    # Create an IcePAP lib axis object
-    device        = self.libdevice
-    address       = axis.address
-    name          = axis.name
-    axis.libaxis  = icepap.lib.Axis(device, address, name)
+        # Initialiaze hardware
+        self.libgroup.set_power(icepap.lib.ON, axis.libaxis)
 
-    # Add the axis to the default IcePAP lib group 
-    self.libgroup.add_axis(axis.libaxis)
+        # Add new axis oject methods
+        add_axis_method(axis, self.get_identifier)
 
-    # Initialiaze hardware
-    self.libgroup.set_power(icepap.lib.ON, axis.libaxis)
+    def position(self, axis, new_position=None, measured=False):
+        """Returns axis position in motor units"""
 
-    # Add new axis oject methods
-    add_axis_method(axis, self.get_identifier)
+        # Optionnal new position to set
+        if new_position is not None:
+            l = icepap.lib.PosList()
+            l[axis.libaxis] = new_position
+            self.libgroup.pos(l)
 
+        # Always return the current position
+        pos_stps = self.libgroup.pos(axis.libaxis)
 
+        return pos_stps
 
+    def velocity(self, axis, new_velocity=None):
+        """Returns axis current velocity in user units per seconds"""
 
-  def position(self, axis, new_position=None, measured=False):
-    """Returns axis position in motor units"""
+        # Optionnal new velocity to set
+        if new_velocity is not None:
+            l = icepap.lib.VelList()
+            l[axis.libaxis] = new_velocity
+            self.libgroup.velocity(l)
 
-    # Optionnal new position to set
-    if new_position is not None:
-      l = icepap.lib.PosList()
-      l[axis.libaxis] = new_position
-      self.libgroup.pos(l)
+        # Always return the current velocity
+        return self.libgroup.velocity(axis.libaxis)
 
-    # Always return the current position
-    pos_stps = self.libgroup.pos(axis.libaxis)
+    def acctime(self, axis, new_acctime=None):
+        """Returns axis current acceleratin time in seconds"""
 
-    return pos_stps
+        # Optionnal new acceleration time to set
+        if new_acctime is not None:
+            l = icepap.lib.AcctimeList()
+            l[axis.libaxis] = new_acctime
+            self.libgroup.acctime(l)
 
+        # Always return the current velocity
+        return self.libgroup.acctime(axis.libaxis)
 
-  def velocity(self, axis, new_velocity=None):
-    """Returns axis current velocity in user units per seconds"""
+    def state(self, axis):
+        """Returns the current axis state"""
 
+        # The axis can only be accessed through a group in IcePAP lib
+        # Use the default group
+        status = self.libgroup.status(axis.libaxis)
 
-    # Optionnal new velocity to set
-    if new_velocity is not None:
-      l = icepap.lib.VelList()
-      l[axis.libaxis] = new_velocity
-      self.libgroup.velocity(l)
+        # Convert status formats
+        if(icepap.lib.status_ismoving(status)):
+            return MOVING
+        if(icepap.lib.status_isready(status)):
+            return READY
 
-    # Always return the current velocity
-    return self.libgroup.velocity(axis.libaxis)
+        # Abnormal end
+        return UNKNOWN
 
+    def prepare_move(self, motion):
+        """Called once before an axis motion, positions in motor units"""
+        pass
 
-  def acctime(self, axis, new_acctime=None):
-    """Returns axis current acceleratin time in seconds"""
+    def start_one(self, motion):
+        """Launch an axis moition, returns immediately,
+           positions in motor units"""
+        target_positions = icepap.lib.PosList()
+        target_positions[motion.axis.libaxis] = motion.target_pos
+        self.libgroup.move(target_positions)
 
-    # Optionnal new acceleration time to set
-    if new_acctime is not None:
-      l = icepap.lib.AcctimeList()
-      l[axis.libaxis] = new_acctime
-      self.libgroup.acctime(l)
+    def stop(self, axis):
+        """Stops smoothly an axis motion"""
+        self.libgroup.stop(axis.libaxis)
 
-    # Always return the current velocity
-    return self.libgroup.acctime(axis.libaxis)
+    def abort(self, axis):
+        """Stops as fast as possible an axis motion, emergency stop"""
 
+    def _set_lib_verbose(self, val):
+        """Change IcePAP library verbose level"""
+        self.libdevice.set_verbose(val)
 
-
-  def state(self, axis):
-    """Returns the current axis state"""
-
-    # The axis can only be accessed through a group in IcePAP lib
-    # Use the default group
-    status = self.libgroup.status(axis.libaxis)
-
-    # Convert status formats
-    if(icepap.lib.status_ismoving(status)):
-      return MOVING
-    if(icepap.lib.status_isready(status)):
-      return READY
-
-    # Abnormal end
-    return UNKNOWN
-
-
-  def prepare_move(self, motion):
-    """Called once before an axis motion, positions in motor units"""
-    pass
-
-  def start_one(self, motion):
-    """Launch an axis moition, returns immediately, positions in motor units"""
-    target_positions = icepap.lib.PosList()
-    target_positions[motion.axis.libaxis] = motion.target_pos
-    self.libgroup.move(target_positions)
-
-
-  def stop(self, axis):
-    """Stops smoothly an axis motion"""
-    self.libgroup.stop(axis.libaxis)
-
-
-  def abort(self, axis):
-    """Stops as fast as possible an axis motion, emergency stop"""
-
-
-  def _set_lib_verbose(self, val): 
-    """Change IcePAP library verbose level"""
-    self.libdevice.set_verbose(val)
-
-
-  def get_identifier(self, axis):
-    """Returns the unique string identifier of the specified axis"""
-    return self.libgroup.command("?ID", axis.libaxis)
-
+    def get_identifier(self, axis):
+        """Returns the unique string identifier of the specified axis"""
+        return self.libgroup.command("?ID", axis.libaxis)

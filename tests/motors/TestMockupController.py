fc18434300d8478b1be718b3c334ac9b0d142f68
lpellab <blissadm@esrf.fr>
2014-03-12 11:02:54 +0100

    pep8 2lines

--- tests/motors/TestMockupController.py
+++ tests/motors/TestMockupController.py
@@ -150,7 +150,6 @@ class TestMockupController(unittest.TestCase):
         _meas_pos = -1.2345
         self.assertEqual(roby.measured_position(), _meas_pos)
 
-
     def test_axis_custom_method(self):
         roby = bliss.get_axis("roby")
         self.assertEqual(roby.get_identifier(), roby.name)

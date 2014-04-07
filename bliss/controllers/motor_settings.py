c01311426fb3141d4da11efdfe1c002e84da089b
Matias Guijarro <matias.guijarro@esrf.fr>
2014-04-07 16:43:48 +0200

    added load_from_config() to AxisSettings and ControllerAxisSettings

--- bliss/controllers/motor_settings.py
+++ bliss/controllers/motor_settings.py
@@ -9,7 +9,8 @@ SETTINGS_WRITER_QUEUE = _threading.Queue()
 def write_settings():
     while True:
         axis, setting_name, value = SETTINGS_WRITER_QUEUE.get()
-        event.send(axis, "write_setting", axis.config, setting_name, value, True)
+        event.send(
+            axis, "write_setting", axis.config, setting_name, value, True)
 
 
 class ControllerAxisSettings:
@@ -21,22 +22,43 @@ class ControllerAxisSettings:
             "position": float,
             "state": str}
         self.axis_settings_dict = dict()
- 
+
         global SETTINGS_WRITER_THREAD
         if SETTINGS_WRITER_THREAD is None:
-            SETTINGS_WRITER_THREAD = _threading.start_new_thread(write_settings, ())
+            SETTINGS_WRITER_THREAD = _threading.start_new_thread(
+                write_settings, ())
 
     def add(self, setting_name, convert_func=str):
         self.setting_names.append(setting_name)
         self.convert_funcs[setting_name] = convert_func
 
-    def set(self, axis, setting_name, value, write=True):
-        settings = self.axis_settings_dict.setdefault(
+    def load_from_config(self, axis):
+        for setting_name in self.setting_names:
+            if setting_name in ("state","position"):
+                continue
+            try:
+                setting_value = axis.config.config_dict["settings"].get(setting_name)
+            except KeyError:
+                # no settings in config.
+                return
+            if setting_value is None:
+                continue
+            self._set_setting(axis, setting_name, setting_value)
+
+    def _settings(self, axis):
+        return self.axis_settings_dict.setdefault(
             axis,
             dict(zip(self.setting_names, (None,) * len(self.setting_names))))
+
+    def _set_setting(self, axis, setting_name, value):
+        settings = self._settings(axis)
         convert_func = self.convert_funcs.get(setting_name, str)
         setting_value = convert_func(value)
         settings[setting_name] = setting_value
+        return setting_value 
+
+    def set(self, axis, setting_name, value, write=True):
+        setting_value = self._set_setting(axis, setting_name, value)
 
         if write:
             SETTINGS_WRITER_QUEUE.put((axis, setting_name, setting_value))
@@ -44,7 +66,8 @@ class ControllerAxisSettings:
         event.send(axis, setting_name, setting_value)
 
     def get(self, axis, setting_name):
-        return self.axis_settings_dict[axis][setting_name]
+        settings = self._settings(axis)
+        return settings.get(setting_name)
 
 
 class AxisSettings:
@@ -59,3 +82,7 @@ class AxisSettings:
     def get(self, setting_name):
         return self.__axis.controller.axis_settings.get(
             self.__axis, setting_name)
+
+    def load_from_config(self):
+        return self.__axis.controller.axis_settings.load_from_config(self.__axis)
+

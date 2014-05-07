318c91f1dcf834d4baa37c2f848f90ebc1e42110
blissadm <blissadm@lid00f>
2014-05-07 11:35:59 +0200

    fix bugs : missing %f  and impotr log...

--- bliss/controllers/motors/FlexDC.py
+++ bliss/controllers/motors/FlexDC.py
@@ -1,4 +1,4 @@
-from bliss.controllers.motor import Controller
+from bliss.controllers.motor import Controller; from bliss.common import log
 from bliss.controllers.motor import add_axis_method
 from bliss.common.axis import READY, MOVING
 
@@ -159,13 +159,13 @@ class FlexDC(Controller):
         return _ret
 
     def prepare_move(self, motion):
-        flexdc_debug("prepare_move, motion.target_pos=%f" % motion.target_pos)
+        flexdc_debug("prepare_move, motion.target_pos=%g" % motion.target_pos)
         # Prepare axis movement.
         self._flexdc_query("%sAP=%d" % (motion.axis.channel,
                                         int(motion.target_pos)))
 
     def start_one(self, motion):
-        flexdc_debug("start_one, motion.target_pos=" % motion.target_pos)
+        flexdc_debug("start_one, motion.target_pos=%g" % motion.target_pos)
         # Start prepared movement.
         self._flexdc_query("%sBG" % motion.axis.channel)
 

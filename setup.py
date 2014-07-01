4c142f3119c8b2e9cecf8ca3e824c6bf0ff13e32
Matias Guijarro <matias.guijarro@esrf.fr>
2014-07-01 23:48:36 +0200

    fixed setup.py (added missing subdirectories)

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name="bliss", version="0.1",
       description="ESRF Motion library",
       author="M. Guijarro, M. Perez (ESRF)",
       package_dir={"bliss": "bliss"},
-      packages=["bliss"])  # , "cool.control_objects"])
+      packages=["bliss", 'bliss.controllers.motors', 'bliss.config.motors', 'bliss.comm'])  # , "cool.control_objects"])

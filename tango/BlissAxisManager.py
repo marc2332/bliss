9d87f312850136be1cc03825a15554fa383e5a4d
blissadm@lid269 ID26 <bcu-oncall@esrf.fr>
2014-07-04 17:28:15 +0200

    thread safe access to attributes from proxy object (have to be called like functions)

--- tango/BlissAxisManager.py
+++ tango/BlissAxisManager.py
@@ -763,7 +763,7 @@ def main():
         _axis = TgGevent.get_proxy(bliss.get_axis, axis_name)
 
         # Search for custom commands.
-        _cmd_list = _axis.get_custom_methods_list()
+        _cmd_list = _axis.custom_methods_list()
         print "BlissAxisManager.py - '%s' custom commands:" % axis_name
 
         types_conv_tab = {

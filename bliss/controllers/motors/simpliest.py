beee3d24b73f315e3c0b8f72c7f8b9a1dc360a5d
Cyril Guilloud <cyril.guilloud@esrf.fr>
2014-09-24 16:44:13 +0200

    presentation of calccontreoller + simpliest demo

---
+++ bliss/controllers/motors/simpliest.py
@@ -0,0 +1,14 @@
+from bliss.controllers.motor import CalcController
+import math
+
+class simpliest(CalcController):
+
+    def __init__(self, *args, **kwargs):
+        CalcController.__init__(self, *args, **kwargs)
+
+    def calc_from_real(self, positions_dict):
+        return { "m0": positions_dict["m0"] / 2 }
+
+    def calc_to_real(self, axis_tag, positions_dict):
+        return { "m1": positions_dict["m1"] * 2 }
+

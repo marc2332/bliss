3a9bf37ee90bb30b3dfb8ed38563c2ed23e75e9d
bliss administrator <blissadm@aelita.esrf.fr>
2014-09-29 11:40:52 +0200

    new NF8753 controller + test, initial commit

---
+++ bliss/controllers/motors/NF8753.py
@@ -0,0 +1,125 @@
+"""
+Bliss controller for ethernet NewFocus 87xx series piezo controller.
+A. Beteva, M. Guijarro, ESRF BCU
+"""
+import time
+
+from bliss.controllers.motor import Controller; from bliss.common import log
+from bliss.controllers.motor import add_axis_method
+from bliss.common.axis import READY, MOVING
+from bliss.comm import tcp
+from bliss.common import event
+import gevent.lock
+
+DELAY=0.02 #delay between 2 commands
+
+class NF8753(Controller):
+
+    def __init__(self, name, config, axes):
+        Controller.__init__(self, name, config, axes)
+
+        self.host = self.config.get("host")
+        self._current_selected_channel = None
+        self.lock = gevent.lock.RLock()
+        self.__busy = False
+
+    def initialize(self):
+        self.sock = tcp.Socket(self.host, 23)
+        if '=2' in self._write_read(None, "DRT", raw=True):
+          raise RuntimeError("Uncompatible closed-loop driver detected in daisy chain")
+        
+    def finalize(self):
+        """
+        Closes the controller socket.
+        """
+        self.sock.close()
+        # this controller can't reconnect immediately
+        # after socket is disconnected, so we put a delay here to make sure
+        # socket is really closed on the controller side
+        time.sleep(DELAY)
+
+    def initialize_axis(self, axis):
+        axis.driver = axis.config.get("driver", str)
+        axis.channel = axis.config.get("channel", int)
+        axis.position(axis.settings.get("position") or 0)
+        
+        event.connect(axis, "move_done", self._axis_move_done)
+        self._write_no_reply(axis, "JOF")
+        self._write_no_reply(axis, "MON")
+
+    def read_position(self, axis, measured=False):
+        # position is relative to current client connection,
+        # and in case of 8753 it is just a counter of relative
+        # moves
+        _pos = self._write_read(axis, "POS %s" % axis.driver)
+        return float(_pos)
+
+    def _select_channel(self, axis):
+        change_channel = "CHL %s=%d" % (axis.driver, axis.channel)
+        if change_channel != self._current_selected_channel:
+          self._current_selected_channel = change_channel
+          self._write_no_reply(None, change_channel)
+
+    def _write_no_reply(self, axis, cmd_string):
+        with self.lock:
+          if not cmd_string.endswith('\n'):
+            cmd_string += '\n'
+          if axis is not None:
+            self._select_channel(axis)
+          print 'sending', cmd_string
+          self.sock.write_readline(cmd_string, eol='>') 
+          time.sleep(DELAY)
+
+    def _write_read(self, axis, cmd_string, eol='\r\n', raw=False):
+        with self.lock:
+          if not cmd_string.endswith('\r\n'):
+            cmd_string += '\r\n'
+
+          if axis is not None:
+            self._select_channel(axis)
+ 
+          print 'sending',cmd_string,'waiting for reply...'
+          ans = self.sock.write_readline(cmd_string, eol=eol)
+          time.sleep(DELAY)
+          print 'reply=', ans
+
+          ans = ans.replace(">", "")
+          if raw:
+            return ans
+          else:
+            return ans.split("=")[1].split("\r\n")[0]
+    
+    def read_velocity(self, axis):
+        return int(self._write_read(axis, "VEL %s %d" % (axis.driver, axis.channel)))
+    
+    def set_velocity(self, axis, new_velocity):
+        self._write_no_reply(axis, "VEL %s %d=%d" % (axis.driver, axis.channel, new_velocity))
+        return self.read_velocity(axis)
+
+    def state(self, axis):
+        sta = self._write_read(axis, "STA", eol='\r\n>', raw=True)
+        for line in sta.split("\n"):
+          if line.startswith(axis.driver):
+            status_byte = int(line.split("=")[-1], 16)
+            if status_byte & 0x0000001:
+              return MOVING
+            else:
+              return READY
+
+    def is_busy(self):
+        return self.__busy
+
+    def prepare_move(self, motion):
+        self.__busy = True
+
+    def _axis_move_done(self, done):
+        if done:
+          print ">"*10, "AXIS MOVE DONE"
+          self.__busy = False
+ 
+    def start_one(self, motion):
+        print "in motion start_one for axis", motion.axis.name
+        self._write_no_reply(motion.axis, "ABS %s=%d G" % (motion.axis.driver, motion.target_pos))
+
+    def stop(self, axis):
+        self._write_no_reply(axis, "HAL") 
---
+++ tests/motors/TestNewFocus.py
@@ -0,0 +1,97 @@
+import unittest
+import sys
+import os
+import time
+
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "..")))
+
+import bliss
+from bliss.common.axis import READY,MOVING
+
+config_xml = """
+<config>
+  <controller class="NF8753">
+    <host value="newfocusid30a3"/>
+    <axis name="a1">
+      <driver value="A1"/>
+      <channel value="0"/>
+      <steps_per_unit value="50"/>
+      <velocity value="10" />
+    </axis>
+    <axis name="a2">
+      <driver value="A1"/>
+      <channel value="1"/>
+      <steps_per_unit value="50"/>
+      <velocity value="10" />
+    </axis>
+  </controller>
+</config>
+"""
+
+
+class TestNewFocus(unittest.TestCase):
+
+    def setUp(self):
+        bliss.load_cfg_fromstring(config_xml)
+    """
+    def testVelocity(self):
+        a1 = bliss.get_axis("a1")
+        self.assertEquals(a1.velocity(1), 1)
+        config_velocity = a1.config.get("velocity", int)
+        self.assertEquals(a1.velocity(config_velocity), config_velocity)
+        a2 = bliss.get_axis("a2")
+        config_velocity = a2.config.get("velocity", int)
+        self.assertEquals(a2.velocity(config_velocity), config_velocity)
+    """
+    def testPosition(self):
+        a1 = bliss.get_axis("a1")
+        import pdb;pdb.set_trace()
+        p0 = a1.position()
+        target = p0+0.1
+        a1.move(target)
+        self.assertEquals(a1.position(), target)
+        a1.move(p0)
+        self.assertEquals(a1.position(), p0)  
+
+        a2 = bliss.get_axis("a2")
+	p0 = a2.position()
+	target = p0+0.1
+	a2.move(target)
+	self.assertEquals(a2.position(), target)
+	a2.move(p0)
+	self.assertEquals(a2.position(), p0)  
+    """    
+    def testState(self):
+        a1 = bliss.get_axis("a1")
+        self.assertEquals(a1.state(), READY)
+    
+    def testSimultaneousMove(self):
+        a1 = bliss.get_axis("a1")
+        a2 = bliss.get_axis("a2")
+        p1 = a1.position()
+        print "!"*20, p1       
+        p2 = a2.position()
+        a1.rmove(0.1, wait=False)
+        try:
+          a2.rmove(0.1, wait=False)
+        except:
+          pass
+        a1.wait_move()
+        print "!"*20, a1.position()       
+        a2.wait_move()
+        self.assertEquals(a1.position(), p1+0.1)
+        self.assertEquals(a2.position(), p2+0.1) 
+        #a1.rmove(-0.1, wait=False)
+        #a2.rmove(-0.1, wait=False)
+        #a1.wait_move()
+        #a2.wait_move()
+        #self.assertEquals(a1.position(), p1)
+        #self.assertEquals(a2.position(), p2) 
+    """
+if __name__ == '__main__':
+    unittest.main()

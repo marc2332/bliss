dd478f7416fb2bc542c4394df36029b1b5dc777d
bliss administrator <blissadm@aelita.esrf.fr>
2014-11-18 20:06:14 +0100

    fixed forgotten argument in function

--- bliss/controllers/motor.py
+++ bliss/controllers/motor.py
@@ -156,7 +156,7 @@ class Controller(object):
     def home_state(self, axis):
         raise NotImplementedError
 
-    def limit_search(self, limit):
+    def limit_search(self, axis, limit):
         raise NotImplementedError
 
     def read_position(self, axis, measured=False):

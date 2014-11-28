37fd49ec878121710a2d7e78b2a1bca261429471
Cyril Guilloud <cyril.guilloud@esrf.fr>
2014-11-28 15:48:24 +0100

    template python file to create new controller.

---
+++ bliss/controllers/motors/template.py
@@ -0,0 +1,118 @@
+import time
+import serial
+
+from bliss.controllers.motor import Controller
+from bliss.common import log as elog
+from bliss.controllers.motor import add_axis_method
+from bliss.common.axis import READY, MOVING
+
+
+"""
+Bliss controller for XXX.
+"""
+
+
+class XXX(Controller):
+
+    def __init__(self, name, config, axes):
+        Controller.__init__(self, name, config, axes)
+
+        # self.com = self.config.get("com")
+
+    def initialize(self):
+        """
+        """
+        # opens communication
+
+    def finalize(self):
+        """
+        """
+        # Closes communication
+
+    def initialize_axis(self, axis):
+        """
+        Reads specific config
+        Adds specific methods
+        """
+
+    def read_position(self, axis, measured=False):
+        """
+        Returns position's setpoint or measured position.
+
+        Args:
+            - <axis> : bliss axis.
+            - [<measured>] : boolean : if True, function returns
+              measured position in ???
+        Returns:
+            - <position> : float : axis setpoint in ???.
+        """
+        if measured:
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+
+    def read_velocity(self, axis):
+        """
+        Args:
+            - <axis> : Bliss axis object.
+        Returns:
+            - <velocity> : float
+        """
+
+    def set_velocity(self, axis, new_velocity):
+
+
+    def state(self, axis):
+        _ans = ???
+        if _ans == ???:
+            return MOVING
+        else:
+            return READY
+
+    def prepare_move(self, motion):
+        pass
+
+    def start_one(self, motion):
+        """
+        """
+        _cmd =
+        self.send(motion.axis, _cmd)
+
+    def stop(self, axis):
+        # Halt a scan (not a movement ?)
+        self.send(axis, "STOP")
+
+    def raw_write(self, axis, cmd):
+        self.serial.write(cmd)
+
+    def raw_write_read(self, axis, cmd):
+        self.serial.write(cmd)
+        _ans = self.serial.readlines()
+        return _ans
+
+
+    def get_id(self, axis):
+        """
+        Returns firmware version.
+        """
+        return self.send(axis, "?VER")
+
+    def get_info(self, axis):
+        """
+        Returns information about controller.
+        """
+
+        _txt = ""
+
+        return _txt
+
+
+    """
+    VSCANNER Com
+    """
+    def send(self, axis, cmd):
+        return _ans
+
+    def send_no_ans(self, axis, cmd):
+        pass

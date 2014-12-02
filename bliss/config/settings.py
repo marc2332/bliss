a60417c11d3bef66ee5af2db1ae8274dd7d64884
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2014-12-02 19:34:14 +0100

    fixed settings naming

--- bliss/config/settings.py
+++ bliss/config/settings.py
@@ -24,7 +24,7 @@ def auto_conversion(var):
             pass
     return var
 
-def picke_loads(var) :
+def pickle_loads(var) :
     if var is None:
         return None
     return pickle.loads(var)
@@ -81,76 +81,79 @@ def write_decorator(func) :
     return _write
 
 class SimpleSetting(object) :
-    class Proxy(object) :
-        def __init__(self,name,cnx,
-                     read_type_conversion,write_type_conversion) :
-            self._cnx = weakref.ref(cnx)
-            self._name = name
-            self._read_type_conversion = read_type_conversion
-            self._write_type_conversion = write_type_conversion
+    def __init__(self,name,connection = None,
+                 read_type_conversion = auto_conversion,
+                 write_type_conversion = None) :
+        if connection is None:
+            connection = get_cache()
+        self._cnx = weakref.ref(connection)
+        self._name = name
+        self._read_type_conversion = read_type_conversion
+        self._write_type_conversion = write_type_conversion
 
-        @read_decorator
-        def get(self) :
-            cnx = self._cnx()
-            value = cnx.get(self._name)
+    @read_decorator
+    def get(self) :
+        cnx = self._cnx()
+        value = cnx.get(self._name)
+        return value
+
+    @write_decorator
+    def set(self,value) :
+        cnx = self._cnx()
+        cnx.set(self._name,value)
+
+    def ttl(self,value = -1):
+        return ttl_func(self._cnx(),self._name,value)
+
+    def __add__(self,other) :
+        value = self.get()
+        if isinstance(other,SimpleSetting.Proxy):
+            other = other.get()
+        return value + other
+
+    def __iadd__(self,other):
+        cnx = self._cnx()
+        if cnx is not None:
+            if isinstance(other,int) :
+                if other == 1:
+                    cnx.incr(self._name)
+                else:
+                    cnx.incrby(self._name,other)
+            elif isinstance(other,float) :
+                cnx.incrbyfloat(self._name,other)
+            else:
+                cnx.append(self._name,other)
+            return self
+
+    def __isub__(self,other) :
+        if isinstance(other,basestring):
+            raise TypeError("unsupported operand type(s) for -=: %s" % type(other).__name__)
+        return self.__iadd__(-other)
+
+    def __getitem__(self,ran) :
+        cnx = self._cnx()
+        if cnx is not None:
+            step = None
+            if isinstance(ran,slice):
+                i,j = ran.start,ran.stop
+                step = ran.step
+            elif isinstance(ran,int):
+                i = j = ran
+            else:
+                raise TypeError('indices must be integers')
+
+            value = cnx.getrange(self._name,i,j)
+            if step is not None:
+                value = value[0:-1:step]
             return value
 
-        @write_decorator
-        def set(self,value) :
-            cnx = self._cnx()
-            cnx.set(self._name,value)
 
-        def ttl(self,value = -1):
-            return ttl_func(self._cnx(),self._name,value)
+    def __repr__(self):
+        cnx = self._cnx()
+        value = cnx.get(self._name)
+        return '<SimpleSetting.Proxy name=%s value=%s>' % (self._name,value)
 
-        def __add__(self,other) :
-            value = self.get()
-            if isinstance(other,SimpleSetting.Proxy):
-                other = other.get()
-            return value + other
-
-        def __iadd__(self,other):
-            cnx = self._cnx()
-            if cnx is not None:
-                if isinstance(other,int) :
-                    if other == 1:
-                        cnx.incr(self._name)
-                    else:
-                        cnx.incrby(self._name,other)
-                elif isinstance(other,float) :
-                    cnx.incrbyfloat(self._name,other)
-                else:
-                    cnx.append(self._name,other)
-                return self
-        
-        def __isub__(self,other) :
-            if isinstance(other,basestring):
-                raise TypeError("unsupported operand type(s) for -=: %s" % type(other).__name__)
-            return self.__iadd__(-other)
-            
-        def __getitem__(self,ran) :
-            cnx = self._cnx()
-            if cnx is not None:
-                step = None
-                if isinstance(ran,slice):
-                    i,j = ran.start,ran.stop
-                    step = ran.step
-                elif isinstance(ran,int):
-                    i = j = ran
-                else:
-                    raise TypeError('indices must be integers')
-
-                value = cnx.getrange(self._name,i,j)
-                if step is not None:
-                    value = value[0:-1:step]
-                return value
-                    
-
-        def __repr__(self):
-            cnx = self._cnx()
-            value = cnx.get(self._name)
-            return '<SimpleSetting.Proxy name=%s value=%s>' % (self._name,value)
-
+class SimpleSettingProp(object) :
     def __init__(self,name,connection = None,
                  read_type_conversion = auto_conversion,
                  write_type_conversion = None,
@@ -166,9 +169,9 @@ class SimpleSetting(object) :
             name = obj.name + ':' + self._name
         else:
             name = self._name
-        return SimpleSetting.Proxy(name,self._cnx,
-                                   self._read_type_conversion,
-                                   self._write_type_conversion)
+        return SimpleSetting.(name,self._cnx,
+                              self._read_type_conversion,
+                              self._write_type_conversion)
 
     def __set__(self,obj,value) :
         if isinstance(value,SimpleSetting.Proxy): return
@@ -186,110 +189,111 @@ class SimpleSetting(object) :
             self._cnx.set(name,value)
 
 class QueueSetting(object):
-    class Proxy(object):
-        def __init__(self,name,cnx,
-                     read_type_conversion,
-                     write_type_conversion) :
-            self._cnx = weakref.ref(cnx)
-            self._name = name
-            self._read_type_conversion = read_type_conversion
-            self._write_type_conversion = write_type_conversion
+    def __init__(self,name,connection = None,
+                 read_type_conversion = auto_conversion,
+                 write_type_conversion = None) :
+        if connection is None: connection = get_cache()
+        self._cnx = weakref.ref(connection)
+        self._name = name
+        self._read_type_conversion = read_type_conversion
+        self._write_type_conversion = write_type_conversion
 
-        @read_decorator
-        def get(self,first=0,last=-1) :
-            cnx = self._cnx()
-            if first == last:
-                l = cnx.lindex(self._name,first)
-            else:
-                l = cnx.lrange(self._name,first,last)
-            return l
+    @read_decorator
+    def get(self,first=0,last=-1) :
+        cnx = self._cnx()
+        if first == last:
+            l = cnx.lindex(self._name,first)
+        else:
+            l = cnx.lrange(self._name,first,last)
+        return l
 
-        @write_decorator
-        def append(self,value) :
-            cnx = self._cnx()
-            cnx.rpush(self._name,value)
+    @write_decorator
+    def append(self,value) :
+        cnx = self._cnx()
+        cnx.rpush(self._name,value)
 
-        @write_decorator
-        def prepend(self,value) :
-            cnx = self._cnx()
-            cnx.lpush(self._name,value)
+    @write_decorator
+    def prepend(self,value) :
+        cnx = self._cnx()
+        cnx.lpush(self._name,value)
 
-        @write_decorator_multiple
-        def extend(self,values) :
-            cnx = self._cnx()
+    @write_decorator_multiple
+    def extend(self,values) :
+        cnx = self._cnx()
+        cnx.rpush(self._name,*values)
+
+    @write_decorator
+    def remove(self,value) :
+        cnx = self._cnx()
+        cnx.lrem(self._name,1,value)
+
+    @write_decorator_multiple
+    def set(self,values) :
+        if not isinstance(values,(list,tuple)) and values is not None:
+            raise TypeError('can only be tuple or list')
+
+        cnx = self._cnx()
+        cnx.delete(self._name)
+        if values is not None:
             cnx.rpush(self._name,*values)
 
-        @write_decorator
-        def remove(self,value) :
-            cnx = self._cnx()
-            cnx.lrem(self._name,1,value)
+    @write_decorator
+    def set_item(self,value,pos = 0) :
+        cnx = self._cnx()
+        cnx.lset(self._name,pos,value)
 
-        @write_decorator_multiple
-        def set(self,values) :
-            if not isinstance(values,(list,tuple)) and values is not None:
-                raise TypeError('can only be tuple or list')
+    @read_decorator
+    def pop_front(self) :
+        cnx = self._cnx()
+        value = cnx.lpop(self._name)
+        if self._read_type_conversion:
+            value = self._read_type_conversion(value)
+        return value
 
-            cnx = self._cnx()
-            cnx.delete(self._name)
-            if values is not None:
-                cnx.rpush(self._name,*values)
-        
-        @write_decorator
-        def set_item(self,value,pos = 0) :
-            cnx = self._cnx()
-            cnx.lset(self._name,pos,value)
+    @read_decorator
+    def pop_back(self) :
+        cnx = self._cnx()
+        value = cnx.rpop(self._name)
+        if self._read_type_conversion:
+            value = self._read_type_conversion(value)
+        return value
 
-        @read_decorator
-        def pop_front(self) :
-            cnx = self._cnx()
-            value = cnx.lpop(self._name)
-            if self._read_type_conversion:
-                value = self._read_type_conversion(value)
-            return value
+    def ttl(self,value = -1):
+        return ttl_func(self._cnx(),self._name,value)
 
-        @read_decorator
-        def pop_back(self) :
-            cnx = self._cnx()
-            value = cnx.rpop(self._name)
-            if self._read_type_conversion:
-                value = self._read_type_conversion(value)
-            return value
-        
-        def ttl(self,value = -1):
-            return ttl_func(self._cnx(),self._name,value)
+    def __len__(self) :
+        cnx = self._cnx()
+        return cnx.llen(self._name)
 
-        def __len__(self) :
-            cnx = self._cnx()
-            return cnx.llen(self._name)
+    def __repr__(self) :
+        cnx = self._cnx()
+        value = cnx.lrange(self._name,0,-1)
+        return '<QueueSetting.Proxy name=%s value=%s>' % (self._name,value)
 
-        def __repr__(self) :
-            cnx = self._cnx()
-            value = cnx.lrange(self._name,0,-1)
-            return '<QueueSetting.Proxy name=%s value=%s>' % (self._name,value)
+    def __iadd__(self,other):
+        self.extend(other)
+        return self
 
-        def __iadd__(self,other):
-            self.extend(other)
-            return self
+    def __getitem__(self,ran) :
+        if isinstance(ran,slice):
+            i,j = ran.start,ran.stop
+        elif isinstance(ran,int):
+            i = j = ran
+        else:
+            raise TypeError('indices must be integers')
+        return self.get(first = i,last = j)
 
-        def __getitem__(self,ran) :
-            if isinstance(ran,slice):
-                i,j = ran.start,ran.stop
-            elif isinstance(ran,int):
-                i = j = ran
-            else:
-                raise TypeError('indices must be integers')
-            return self.get(first = i,last = j)
+    def __setitem__(self,ran,value):
+        if isinstance(ran,slice) :
+            for i,v in zip(range(ran.start,ran.stop),value) :
+                self.set_item(v,pos=i)
+        elif isinstance(ran,int):
+            self.set_item(value,pos=ran)
+        else:
+            raise TypeError('indices must be integers')
+        return self
 
-        def __setitem__(self,ran,value):
-            if isinstance(ran,slice) :
-                for i,v in zip(range(ran.start,ran.stop),value) :
-                    self.set_item(v,pos=i)
-            elif isinstance(ran,int):
-                self.set_item(value,pos=ran)
-            else:
-                raise TypeError('indices must be integers')
-            return self
-        
+class QueueSettingProp(object):
     def __init__(self,name,connection = None,
                  read_type_conversion = auto_conversion,
                  write_type_conversion = None,
@@ -306,9 +310,9 @@ class QueueSetting(object):
         else:
             name = self._name
 
-        return QueueSetting.Proxy(name,self._cnx,
-                                  self._read_type_conversion,
-                                  self._write_type_conversion)
+        return QueueSetting(name,self._cnx,
+                            self._read_type_conversion,
+                            self._write_type_conversion)
 
     def __set__(self,obj,values) :
         if isinstance(values,QueueSetting.Proxy): return
@@ -318,9 +322,9 @@ class QueueSetting(object):
         else:
             name = self._name
 
-        proxy = QueueSetting.Proxy(name,self._cnx,
-                                   self._read_type_conversion,
-                                   self._write_type_conversion)
+        proxy = QueueSetting(name,self._cnx,
+                             self._read_type_conversion,
+                             self._write_type_conversion)
         proxy.set(values)
 
 class HashSetting(object):
@@ -492,25 +496,45 @@ class HashSettingProp(object):
                            self._write_type_conversion)
 #helper
 
+def _change_to_obj_marchaling(keys) :
+    read_type_conversion = keys.pop('read_type_conversion',pickle_loads)
+    write_type_conversion = keys.pop('write_type_conversion',pickle.dumps)
+    keys.update({'read_type_conversion':read_type_conversion,
+                 'write_type_conversion':write_type_conversion})
+
 class HashObjSetting(HashSetting) :
-    def __init__(self,name,connection = None,**keys) :
-        HashSetting.__init__(self,name,connection=connection,
-                             read_type_conversion=pickle.loads,
-                             write_type_conversion=pickle.dumps,**keys)
+    def __init__(self,name,**keys) :
+        _change_to_obj_marchaling(keys)
+        HashSetting.__init__(self,name,**keys)
+
+class HashObjSettingProp(HashSettingProp) :
+    def __init__(self,name,**keys):
+        _change_to_obj_marchaling(keys)
+        HashSettingProp.__init__(self,name,**keys)
+
 class QueueObjSetting(QueueSetting) :
-    def __init__(self,name,connection = None,**keys) :
-        QueueSetting.__init__(self,name,connection=connection,
-                              read_type_conversion=pickle.loads,
-                              write_type_conversion=pickle.dumps,**keys)
+    def __init__(self,name,**keys) :
+        _change_to_obj_marchaling(keys)
+        QueueSetting.__init__(self,name,**keys)
+
+class QueueObjSettingProp(QueueSettingProp):
+    def __init__(self,name,**keys) :
+        _change_to_obj_marchaling(keys)
+        QueueSettingProp.__init__(self,name,**keys)
+
 class SimpleObjSetting(SimpleSetting) :
-    def __init__(self,name,connection = None,**keys) :
-        SimpleSetting.__init__(self,name,connection=connection,
-                               read_type_conversion=picke_loads,
-                               write_type_conversion=pickle.dumps,**keys)
+    def __init__(self,name,**keys) :
+        _change_to_obj_marchaling(keys)
+        SimpleSetting.__init__(self,name,**keys)
+
+class SimpleObjSettingProp(SimpleSettingProp):
+    def __init__(self,name,**keys) :
+        _change_to_obj_marchaling(keys)
+        SimpleSettingProp.__init__(self,name,**keys)
 
 class Struct(object):
     def __init__(self,name,**keys) :
-        self._proxy = HashSetting(name,**keys).get_proxy()
+        self._proxy = HashSetting(name,**keys)
 
     def __dir__(self) :
         return self._proxy.keys()
@@ -538,12 +562,12 @@ class Struct(object):
 
 if __name__ == "__main__" :
     class A(object) :
-        x = SimpleSetting('counter')
-        y = SimpleObjSetting('obj')
-        q = QueueSetting('seb')
-        ol = QueueObjSetting('seb-list')
-        h = HashSetting('seb-hash')
-        oh = HashObjSetting('seb-hash-object')
+        x = SimpleSettingProp('counter')
+        y = SimpleObjSettingProp('obj')
+        q = QueueSettingProp('seb')
+        ol = QueueObjSettingProp('seb-list')
+        h = HashSettingProp('seb-hash')
+        oh = HashObjSettingProp('seb-hash-object')
         def __init__(self,name):
             self.name = name
 

65c9a32dc6de7b322461b96005ba057daea62082
bliss administrator <blissadm@bacon.esrf.fr>
2015-01-14 13:23:35 +0100

    fixed setup.py to include shell files

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name="bliss", version="0.1",
       description="Bliss",
       author="M.Guijarro (ESRF)",
       package_dir={"bliss": "bliss"},
-      packages=["khoros", 'khoros.core', 'khoros.js'],
+      packages=["khoros", 'khoros.core', 'khoros.blcomponents'],
+      package_data={'khoros':['shell.html', 'shell.css', "js/*"]},
       scripts = ['bin/bliss_webserver'],) 

92da2ea23aae58ea119e7a1afd4c82c1468bd26e
bliss administrator <blissadm@bacon.esrf.fr>
2015-02-19 14:43:24 +0100

    'create_objects_from_config' now takes config object itself as first arg. references in khoros blcomponents are now resolved to config objects

--- bliss/config/plugins/khoros.py
+++ bliss/config/plugins/khoros.py
@@ -3,10 +3,14 @@ import sys
 from bliss.config import static
 from khoros.core.measurement import CounterBase
 
-def create_objects_from_config_node(item_cfg_node):
+def create_objects_from_config_node(config, item_cfg_node):
     module = __import__('bliss.controllers.%s' % item_cfg_node['class'], fromlist=[None])
     klass = getattr(module, item_cfg_node['class'])
     item_name = item_cfg_node["name"]
+    for name, value in item_cfg_node.iteritems():
+        if isinstance(value, str) and value.startswith("$"):
+            # convert reference to item from config
+            item_cfg_node[name]=config.get(value)  
     o = klass(item_name, item_cfg_node)
 
     if isinstance(o, CounterBase):

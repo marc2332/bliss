29adc145a5bfcd99a97f3cebcf29a7e2f2ab4041
bliss administrator <blissadm@bacon.esrf.fr>
2015-02-25 17:04:16 +0100

    added 'dspace' setting recalculate energy, wavelength positions when dspace setting changes

--- bliss/controllers/motors/energy_wl.py
+++ bliss/controllers/motors/energy_wl.py
@@ -6,7 +6,7 @@ energy: energy calculated axis alias
 wavelength: wavelength calculated axis alias
 dspace: monochromator crystal d-spacing
 """
-from bliss.controllers.motor import CalcController
+from bliss.controllers.motor import CalcController; from bliss.common import event
 import math
 
 
@@ -14,12 +14,20 @@ class energy_wl(CalcController):
     def __init__(self, *args, **kwargs):
         CalcController.__init__(self, *args, **kwargs)
 
-        self.dspace = self.config.get("dspace", float)
+        self.axis_settings.add("dspace", float)
+        
+    def initialize_axis(self, axis):
+        CalcController.initialize_axis(self, axis)
+        event.connect(axis, "dspace", self._calc_from_real)
 
     def calc_from_real(self, positions_dict):
-        lamb = 2*self.dspace*math.sin(math.radians(positions_dict["monoang"]))
+        energy_axis = self._tagged["energy"][0]
+        dspace = energy_axis.settings.get("dspace")
+        lamb = 2*dspace*math.sin(math.radians(positions_dict["monoang"]))
         return {"energy": 12.3984/lamb, "wavelength": lamb}
 
     def calc_to_real(self, axis_tag, positions_dict):
-        monoangle = math.degrees(math.asin(12.3984/(positions_dict["energy"]*2*self.dspace)))
+        energy_axis = self._tagged["energy"][0]
+        dspace = energy_axis.settings.get("dspace")
+        monoangle = math.degrees(math.asin(12.3984/(positions_dict["energy"]*2*dspace)))
         return {"monoang": monoangle}

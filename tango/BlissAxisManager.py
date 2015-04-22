1678dab79a45077d1899d1fa5d360d53f2dc848a
bliss administrator <blissadm@lid269.esrf.fr>
2015-04-22 14:56:36 +0200

    fix commands stepdown new command stop

--- tango/BlissAxisManager.py
+++ tango/BlissAxisManager.py
@@ -473,8 +473,18 @@ class BlissAxis(PyTango.Device_4Impl):
         self.debug_stream("In Abort()")
         self.axis.stop()
 
+    def Stop(self):
+        """ Stop gently the motor
+
+        :param :
+        :type: PyTango.DevVoid
+        :return:
+        :rtype: PyTango.DevVoid """
+        self.debug_stream("In Stop()")
+        self.axis.stop()
+
     def StepUp(self):
-        """ perform a relative motion of ``stepSize`` in the forward
+        """ Performs a relative motion of ``stepSize`` in the forward
          direction.  StepSize is defined as an attribute of the
          device.
 
@@ -482,10 +492,11 @@ class BlissAxis(PyTango.Device_4Impl):
         :type: PyTango.DevVoid
         :return:
         :rtype: PyTango.DevVoid """
-        self.debug_stream("In StepUp()")
+        self.debug_stream("In StepUp(); stepsize=%f" % self.attr_StepSize_read)
+        self.axis.rmove(self.attr_StepSize_read, wait=self.write_position_wait)
 
     def StepDown(self):
-        """ perform a relative motion of ``stepSize`` in the backward
+        """ Performs a relative motion of ``stepSize`` in the backward
          direction.  StepSize is defined as an attribute of the
          device.
 
@@ -493,7 +504,8 @@ class BlissAxis(PyTango.Device_4Impl):
         :type: PyTango.DevVoid
         :return:
         :rtype: PyTango.DevVoid """
-        self.debug_stream("In StepDown()")
+        self.debug_stream("In StepDown(); stepsize=%f" % self.attr_StepSize_read)
+        self.axis.rmove(-self.attr_StepSize_read, wait=self.write_position_wait)
 
     def GetInfo(self):
         """ provide information about the axis.
@@ -575,6 +587,9 @@ class BlissAxisClass(PyTango.DeviceClass):
         [[PyTango.DevVoid, "none"],
          [PyTango.DevVoid, "none"]],
         'Abort':
+        [[PyTango.DevVoid, "none"],
+         [PyTango.DevVoid, "none"]],
+        'Stop':
         [[PyTango.DevVoid, "none"],
          [PyTango.DevVoid, "none"]],
         'StepUp':

31f607f9f8a7298fe4433a50bbe6c27b810a179d
cyril@lid269 <blissadm@lid269.esrf.fr>
2015-06-12 10:10:53 +0200

    setup.cfg made ESRF only.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
-setup(name="bliss", version="0.1",
+setup(name="bliss", version="1.8",
       description="ESRF Motion library",
-      author="M.Guijarro, C.Guilloud, M.Perez (ESRF)",
+      author="M.Guijarro, C.Guilloud, M.Perez (ESRF) 2014-2015",
       package_dir={"bliss": "bliss"},
       packages=["bliss", 'bliss.controllers', 'bliss.controllers.motors', 'bliss.controllers.motors.libicepap',
                 'bliss.controllers.motors.libicepap.deep', 'bliss.config', 'bliss.config.motors',

09f16839f3975f973a285f6359c4f248d61b3d96
Matias Guijarro <matias.guijarro@esrf.fr>
2015-07-31 15:51:32 +0200

    added config channel for beacon backend; when a config property is changed, all peers receive the new configuration; for example, if client A is changing velocity, then doing 'settings_to_config', or if config file is changed and client A does an 'apply_config', then client B will receive the new config without the need to read it from file, so calling '.velocity(from_config=True)' from B would return the right value

--- bliss/config/motors/beacon_backend.py
+++ bliss/config/motors/beacon_backend.py
@@ -193,6 +193,13 @@ class StaticConfig(object):
 
     def __init__(self, config_dict):
         self.config_dict = config_dict
+        try:
+            config_chan_name = "config.%s" % config_dict['name']
+        except KeyError:
+            # can't have config channel is there is no name
+            self.config_channel = None
+        else:
+            self.config_channel = channels.Channel(config_chan_name, dict(config_dict), callback=self._config_changed)
 
     def get(self, property_name, converter=str, default=None):
         """Get static property
@@ -222,6 +229,7 @@ class StaticConfig(object):
    
     def save(self):
         self.config_dict.save()
+        self._update_channel() 
 
     def reload(self):
         cfg = static.get_config()
@@ -229,8 +237,16 @@ class StaticConfig(object):
         # a big task and it can be left as simple as it is, if needed
         # we could selectively reload only parts of the config (e.g one
         # single object yml file)
-        # TODO: a channel to inform all clients that config has changed!!!
-        # config keys should be sent to all listeners
         cfg.reload()
         self.config_dict = cfg.get_config(self.config_dict['name'])
+        self._update_channel()
+
+    def _update_channel(self):
+        if self.config_channel is not None:
+            # inform all clients that config has changed
+            self.config_channel.value = dict(self.config_dict)
+
+    def _config_changed(self, config_dict):
+        for key, value in config_dict.iteritems():
+            self.config_dict[key]=value
 

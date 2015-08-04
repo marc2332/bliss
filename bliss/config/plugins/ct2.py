dab7323ba59d736d9b15c013015dfa900e8ca482
coutinho <coutinho@esrf.fr>
2015-08-04 10:16:30 +0200

    More complete implementation

--- bliss/config/plugins/ct2.py
+++ bliss/config/plugins/ct2.py
@@ -3,6 +3,14 @@ from __future__ import absolute_import
 import os
 import shlex
 
+try:
+    from collections import OrderedDict
+except ImportError:
+    try:
+        from ordereddict import OrderedDict
+    except ImportError:
+        OrderedDict = dict
+
 import flask.json
 
 from jinja2 import Environment, FileSystemLoader
@@ -14,73 +22,77 @@ __this_path = os.path.dirname(os.path.realpath(__file__))
 import ct2
 
 GENERAL_PARAMS_SEQ = (
-#      name                          type    writable   default value         label                     group         description
-    ("name",                      ('unicode', True,  None,                  "Name",                     None,        "card name")),
-    ("class",                     ('str',     True,  None,                  "Type",                     None,        "card type")),
-    ("address",                   ('str',     True,  None,                  "Address",                  None,        "card address (ex: /dev/p201)")),
-    ("clock",                     ('Clock',   True,  ct2.Clock.CLK_100_MHz, "Clock",                    None,        "card clock")),
-    ("dma interrupt",             ('bool',    True,  False,                 "DMA interrupt",            "interrupt", "End of DMA transfer will trigger interrupt")),
-    ("fifo half full interrupt",  ('bool',    True,  False,                 "FIFO half full interrupt", "interrupt", "FIFO half full will trigger interrupt")),
-    ("error interrupt",           ('bool',    True,  False,                 "Error interrupt",          "interrupt", "FIFO transfer error or too close DMA triggers will trigger interrupt")),
+#      name                          type   possible writable   default value         label                     group         description
+#                                            values
+    ("name",                      ('unicode', None, True,  None,                  "Name",                     None,        "card name")),
+    ("class",                     ('str',     None, True,  None,                  "Type",                     None,        "card type")),
+    ("address",                   ('str',     None, True,  None,                  "Address",                  None,        "card address (ex: /dev/p201)")),
+    ("clock",                     ('Clock',   None, True,  ct2.Clock.CLK_100_MHz, "Clock",                    None,        "counters external clock")),
+    ("dma interrupt",             ('bool',    None, True,  False,                 "DMA interrupt",            "interrupt", "End of DMA transfer will trigger interrupt")),
+    ("fifo half full interrupt",  ('bool',    None, True,  False,                 "FIFO half full interrupt", "interrupt", "FIFO half full will trigger interrupt")),
+    ("error interrupt",           ('bool',    None, True,  False,                 "Error interrupt",          "interrupt", "FIFO transfer error or too close DMA triggers will trigger interrupt")),
 )
 
-GENERAL_PARAMS_DICT = {}
+GENERAL_PARAMS = OrderedDict()
 for k, v in GENERAL_PARAMS_SEQ:
-    GENERAL_PARAMS_DICT[k] = dict(name=k, type=v[0], access=v[1], default=v[2],
-                                  label=v[3], group=v[4], description=v[5])
+    GENERAL_PARAMS[k] = dict(name=k, type=v[0], values=v[1], access=v[2], default=v[3],
+                             label=v[4], group=v[5], description=v[6])
 
 COUNTER_PARAMS_SEQ = (
-#      name             type            writable      default value           label         group       description
-    ("address",         ('int',            False, None,                        "#",      None,        "counter ID")),
-#    ("name",            ('unicode',        True,  "",                          "Name",         None,        "counter name")),
-    ("clock source",    ('CtClockSrc',     True,  ct2.CtClockSrc.CLK_1_25_KHz, "Clock",        "source",    "counter clock source")),
-    ("gate source",     ('CtGateSrc',      True,  ct2.CtGateSrc.GATE_CMPT,     "Gate",         "source",    "counter gate source")),
-    ("start source",    ('CtHardStartSrc', True,  ct2.CtHardStartSrc.SOFTWARE, "Start",        "source",    "hardware start source")),
-    ("stop source",     ('CtHardStopSrc',  True,  ct2.CtHardStopSrc.SOFTWARE,  "Stop",         "source",    "hardware stop source")),
-    ("latch sources",   ('list<int>',      True,  [],                          "Latch(es)",    "source",    "counter to latch(es) on counter signals hardware stop, software stop and software disable (comma separated)")),
-    ("reset",           ('bool',           True,  False,                       "Reset",        None,        "reset from hardware or software stop")),
-    ("stop",            ('bool',           True,  False,                       "Stop",         None,        "stop from hardware stop")),
-    ("interrupt",       ('bool',           True,  False,                       "Interrupt",    "interrupt", "counter N stop triggers interrupt")),
-    ("software enable", ('bool',           True,  False,                       "Soft. enable", None,        "software enable/disable")),
-    ("comparator",      ('int',            True,  0,                           "Comp.",   None,        "counter N comparator value")),
+#      name             type           possible writable      default value           label         group       description
+#                                        values
+    ("address",         ('int',            None,        False, None,                        "#",      None,        "counter ID")),
+#    ("name",            ('unicode',        None,        True,  "",                          "Name",         None,        "counter name")),
+    ("clock source",    ('CtClockSrc',     None,        True,  ct2.CtClockSrc.CLK_1_25_KHz, "Clock",        "source",    "counter clock source")),
+    ("gate source",     ('CtGateSrc',      None,        True,  ct2.CtGateSrc.GATE_CMPT,     "Gate",         "source",    "counter gate source")),
+    ("start source",    ('CtHardStartSrc', None,        True,  ct2.CtHardStartSrc.SOFTWARE, "Start",        "source",    "hardware start source")),
+    ("stop source",     ('CtHardStopSrc',  None,        True,  ct2.CtHardStopSrc.SOFTWARE,  "Stop",         "source",    "hardware stop source")),
+    ("latch sources",   ('list int',       range(1,13), True,  [],                          "Latch(es)",    "source",    "counter to latch(es) on counter signals hardware stop, software stop and software disable (comma separated)")),
+    ("reset",           ('bool',           None,        True,  False,                       "Reset",        None,        "reset from hardware or software stop")),
+    ("stop",            ('bool',           None,        True,  False,                       "Stop",         None,        "stop from hardware stop")),
+    ("interrupt",       ('bool',           None,        True,  False,                       "Interrupt",    "interrupt", "counter N stop triggers interrupt")),
+    ("software enable", ('bool',           None,        True,  False,                       "Soft. enable", None,        "software enable/disable")),
+    ("comparator",      ('int',            None,        True,  0,                           "Comp.",   None,        "counter N comparator value")),
 )
 
-COUNTER_PARAMS_DICT = {}
+COUNTER_PARAMS = OrderedDict()
 for k, v in COUNTER_PARAMS_SEQ:
-    COUNTER_PARAMS_DICT[k] = dict(name=k, type=v[0], access=v[1], default=v[2],
-                                  label=v[3], group=v[4], description=v[5])
+    COUNTER_PARAMS[k] = dict(name=k, type=v[0], values=v[1], access=v[2], default=v[3],
+                             label=v[4], group=v[5], description=v[6])
 
 IN_CHANNEL_PARAMS_SEQ = (
-#      name             type            writable      default value           label         group       description
-    ("address",         ('int',          False, None,                        "#",        None,     "channel ID")),
-#    ("name",            ('unicode',      True,  "",                          "Name",           None,     "channel name")),
-    ("level",           ('Level',        True,  ct2.Level.DISABLE,           "Level",          None,     "channel level (TTL, NIM, Both or None)")),
-    ("50 ohm",          ('bool',         True,  False,                       "50 &#8486;",     None,     "50 &#8486; adapter")),
-    ("readback",        ('bool',         True,  False,                       "Readback",       None,     "readback")),
-    ("interrupt",       ('list<str>',    True,  [],                          "Edge interrupt", None,     "edge interrupt: rising, falling or both (comma separated)")),
+#      name             type          possible writable      default value           label         group       description
+#                                       values
+    ("address",         ('int',          None,                  False, None,                        "#",        None,     "channel ID")),
+#    ("name",            ('unicode',      None,                  True,  "",                          "Name",           None,     "channel name")),
+    ("level",           ('Level',        None,                  True,  ct2.Level.DISABLE,           "Level",          None,     "channel level (TTL, NIM, Both or None)")),
+    ("50 ohm",          ('bool',         None,                  True,  False,                       "50 &#8486;",     None,     "50 &#8486; adapter")),
+    ("readback",        ('bool',         None,                  True,  False,                       "Readback",       None,     "readback")),
+    ("interrupt",       ('list str',     ["rising", "falling"], True,  [],                          "Edge interrupt", None,     "edge interrupt: rising, falling or both (comma separated)")),
 )
 
-IN_CHANNEL_PARAMS_DICT = {}
+IN_CHANNEL_PARAMS = OrderedDict()
 for k, v in IN_CHANNEL_PARAMS_SEQ:
-    IN_CHANNEL_PARAMS_DICT[k] = dict(name=k, type=v[0], access=v[1], default=v[2],
-                                  label=v[3], group=v[4], description=v[5])
+    IN_CHANNEL_PARAMS[k] = dict(name=k, type=v[0], values=v[1], access=v[2], default=v[3],
+                                label=v[4], group=v[5], description=v[6])
 
 OUT_CHANNEL_PARAMS_SEQ = (
-#      name             type            writable      default value           label         group       description
-    ("address",         ('int',          False, None,                        "#",        None,     "channel ID")),
-#    ("name",            ('unicode',      True,  "",                          "Name",           None,     "channel name")),
-    ("software level",  ('bool',         True,  False,                      "Sw. enable",      None,     "software enable")),
-    ("level",           ('bool',         True,  False,                      "Level",           None,     "level")),
-    ("source",          ('OutputSrc',    True,  ct2.OutputSrc.SOFTWARE,     "Source",          "source", "output source")),
-    ("filter enable",   ('bool',         True,  False,                      "Filter enable",   "filter", "output filter enable/disable")),
-    ("filter clock",    ('FilterClock',  True,  ct2.FilterClock.CLK_100_MHz,"Filter clock",    "filter", "output filter clock")),
-    ("filter polarity", ('bool',         True,  False,                      "Filter polarity", "filter", "output filter polarity (0 or 1)")),
+#      name             type         possible writable      default value           label         group       description
+#                                      values
+    ("address",         ('int',          None, False, None,                        "#",        None,     "channel ID")),
+#    ("name",            ('unicode',      None, True,  "",                          "Name",           None,     "channel name")),
+    ("software level",  ('bool',         None, True,  False,                      "Sw. enable",      None,     "software enable")),
+    ("level",           ('bool',         None, True,  False,                      "Level",           None,     "level")),
+    ("source",          ('OutputSrc',    None, True,  ct2.OutputSrc.SOFTWARE,     "Source",          "source", "output source")),
+    ("filter enable",   ('bool',         None, True,  False,                      "Filter enable",   "filter", "output filter enable/disable")),
+    ("filter clock",    ('FilterClock',  None, True,  ct2.FilterClock.CLK_100_MHz,"Filter clock",    "filter", "output filter clock")),
+    ("filter polarity", ('bool',         None, True,  False,                      "Filter polarity", "filter", "output filter polarity (0 or 1)")),
 )
 
-OUT_CHANNEL_PARAMS_DICT = {}
+OUT_CHANNEL_PARAMS = OrderedDict()
 for k, v in OUT_CHANNEL_PARAMS_SEQ:
-    OUT_CHANNEL_PARAMS_DICT[k] = dict(name=k, type=v[0], access=v[1], default=v[2],
-                                  label=v[3], group=v[4], description=v[5])
+    OUT_CHANNEL_PARAMS[k] = dict(name=k, type=v[0], values=v[1],  access=v[2], default=v[3],
+                                 label=v[4], group=v[5], description=v[6])
 
 def get_jinja2():
     global __environment
@@ -132,14 +144,14 @@ def get_card_html(cfg):
             ch['output']["address"] = addr
             out_channels[addr] = ch['output']
 
-    general_params = dict([(k, cfg[k]) for k in GENERAL_PARAMS_DICT if k in cfg])
+    general_params = dict([(k, cfg[k]) for k in GENERAL_PARAMS if k in cfg])
 
     params = dict(
         ct2=ct2,
-        GENERAL_PARAMS_SEQ=GENERAL_PARAMS_SEQ,
-        COUNTER_PARAMS_SEQ=COUNTER_PARAMS_SEQ,
-        IN_CHANNEL_PARAMS_SEQ=IN_CHANNEL_PARAMS_SEQ,
-        OUT_CHANNEL_PARAMS_SEQ=OUT_CHANNEL_PARAMS_SEQ,
+        GENERAL_PARAMS=GENERAL_PARAMS,
+        COUNTER_PARAMS=COUNTER_PARAMS,
+        IN_CHANNEL_PARAMS=IN_CHANNEL_PARAMS,
+        OUT_CHANNEL_PARAMS=OUT_CHANNEL_PARAMS,
         params=general_params,
         counters=counters,
         in_channels=in_channels,
@@ -149,43 +161,34 @@ def get_card_html(cfg):
     return html_template.render(**params)
 
 
-def __value_to_config(value, dictio):
+
     if value is None:
         value = dictio['default']
     if value is None:
         return
-    type_str = dictio['type']
-    if type_str == 'list<int>':
-        try:
-            return map(int, value.split(","))
-        except:
-            return ()
-    elif type_str == 'list<str>':
-        try:
-            ret = shlex.shlex(value)
-            ret.whitespace += ","
-            return list(ret)
-        except:
-            return ()
-    elif type_str == 'bool':
-        if value in ('on', 'true'):
-            value = True
-        elif value in ('off', 'false'):
-            value = False
-        return bool(value)
-    elif type_str == 'int':
-        return int(value)
-    elif type_str == 'float':
-        return float(value)
-    elif type_str == 'str':
-        return str(value)
-    elif type_str == 'unicode':
-        return unicode(value)
+
+def __value_to_config(form, key, dictio):
+    type_str, default = dictio['type'], dictio['default']
+    kwargs = {}
+    if type_str.startswith('list'):
+        meth = form.getlist
+        type_str = type_str.split()[1]
     else:
-        if not isinstance(value, ct2.enum.Enum):
-            value = getattr(ct2, type_str)[value]
-        return value.name
-    return value
+        meth = form.get
+        kwargs["default"] = dictio['default']
+
+    try:
+        # usual bool, int, float, str, unicode
+        # bool works because form returns "on" if True or no value at all if False
+        kwargs["type"] = __builtins__[type_str]
+    except KeyError:
+        pass
+        # could be a ct2 enum...
+        #try:
+        #    kwargs["type"] = getattr(ct2, type_str)
+        #except AttributeError:
+        #    conv = None
+    return meth(key, **kwargs)
 
 
 def __get_label_from_enum(en_value):
@@ -206,8 +209,8 @@ def empty_channel(addr):
 
 def card_edit(cfg, request):
     if request.method == "POST":
-        form = dict([(k,v) for k,v in request.form.items()])
-        orig_card_name = form.pop("__original_name__")
+        form = request.form
+        orig_card_name = form.get("__original_name__")
         card_name = form["name"]
         card_type = form.get("class")
         card = getattr(ct2, card_type)
@@ -217,40 +220,41 @@ def card_edit(cfg, request):
             result["type"] = "danger"
             return flask.json.dumps(result)
         card_cfg = cfg.get_config(orig_card_name)
-
+        
+        print (form.getlist("bla"))
         cfg, counters, channels = {}, {}, {}
         # handle generic card parameters
-        for p_name, p_info in GENERAL_PARAMS_DICT.items():
-            cfg[p_name] = __value_to_config(form.get(p_name), p_info)
+        for p_name, p_info in GENERAL_PARAMS.items():
+            cfg[p_name] = __value_to_config(form, p_name, p_info)
 
         # handle counter parameters
-        for p_name, p_info in COUNTER_PARAMS_DICT.items():
+        for p_name, p_info in COUNTER_PARAMS.items():
             if p_name == "address": continue
             for counter in card.COUNTERS:
                 ct = counters.setdefault(counter, empty_counter(counter))
                 ct_p_name = "ct {0} {1}".format(counter, p_name)
-                ct[p_name] = __value_to_config(form.get(ct_p_name), p_info)
+                ct[p_name] = __value_to_config(form, ct_p_name, p_info)
 
         # handle input channels parameters
-        for p_name, p_info in IN_CHANNEL_PARAMS_DICT.items():
+        for p_name, p_info in IN_CHANNEL_PARAMS.items():
             if p_name == "address": continue
             for channel in card.INPUT_CHANNELS:
                 ch = channels.setdefault(channel, empty_channel(channel))['input']
                 ch_p_name = "inch {0} {1}".format(channel, p_name)
-                ch[p_name] = __value_to_config(form.get(ch_p_name), p_info)
+                ch[p_name] = __value_to_config(form, ch_p_name, p_info)
 
         # handle output channels parameters
-        for p_name, p_info in OUT_CHANNEL_PARAMS_DICT.items():
+        for p_name, p_info in OUT_CHANNEL_PARAMS.items():
             if p_name == "address": continue
             for channel in card.OUTPUT_CHANNELS:
                 ch = channels.setdefault(channel, empty_channel(channel))['output']
                 ch_p_name = "outch {0} {1}".format(channel, p_name)
-                ch[p_name] = __value_to_config(form.get(ch_p_name), p_info)
+                ch[p_name] = __value_to_config(form, ch_p_name, p_info)
 
         cfg['counters'] = [counters[ct] for ct in sorted(counters)]
         cfg['channels'] = [channels[ch] for ch in sorted(channels)]
 
-        card_cfg.update((k, v) for k, v in cfg.items())
+        card_cfg.update([(k, v) for k, v in cfg.items()])
         card_cfg.save()
 
         result["message"] = "'%s' configuration applied!" % card_name

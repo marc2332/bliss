434fab3b5abec166bb6fd7607f489f9df6a8a2bf
bliss administrator <blissadm@aelita.esrf.fr>
2015-08-14 18:27:24 +0200

    do monkey patching of standard Python modules, except threading

--- bliss/__init__.py
+++ bliss/__init__.py
@@ -1,3 +1,4 @@
+from gevent import monkey; monkey.patch_all(thread=False)
 from bliss.common.continuous_scan import Scan
 from bliss.common.continuous_scan import AcquisitionChain
 from bliss.common.continuous_scan import AcquisitionDevice

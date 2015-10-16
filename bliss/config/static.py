4466a6c6218046ad9205ac5d9fc4133b9d4d7efc
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2015-10-16 13:37:16 +0200

    fixed merge

--- bliss/config/static.py
+++ bliss/config/static.py
@@ -148,15 +148,15 @@ class Node(NodeDict):
                 if child_list:
                     return_dict[key] = child_list
             else:
-                return plugin
-
-        def get_node_filename(self):
-            config = self._config()
-            filename = config._node2file.get(self)
-            if filename is not None:
-                return self, filename
-            elif self._parent is not None:
-                return self._parent.get_node_filename()
+                return_dict[key] = values
+        return return_dict
+  
+    def _get_save_list(self,l,filename):
+        return_list = []
+        for v in l:
+            if isinstance(v,Node) :
+                if v.filename != filename: break
+                return_list.append(self._get_save_dict(v,filename))
             else:
                 return_list.append(v)
         return return_list

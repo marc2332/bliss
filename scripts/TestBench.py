b24e51f6e04daca8ba079f429918b9cabd2cc915
cyril@lid269 <blissadm@lid269.esrf.fr>
2015-12-10 15:03:47 +0100

    simplification

--- scripts/TestBench.py
+++ scripts/TestBench.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python
 
-# Simple python script to test bliss
+# Simple python script to test bliss and beacon
 #
 # needs a ". blissrc"
 
-import time
-import PyTango
-from bliss.common import log
-import bliss
-
-log.level(log.DEBUG)
-log.level(log.INFO)
-
-from bliss.config import settings
-from bliss.config.motors import set_backend
-set_backend("beacon")
-
-ra = bliss.get_axis("ra")
-svg = bliss.get_axis("svg")
-
-
-# ra.apply_config()
-# svg.apply_config()
+from bliss.config import static
+cfg = static.get_config()
+ra = cfg.get("ra")
+print "ra velocity : %g" % ra.velocity()
 
 

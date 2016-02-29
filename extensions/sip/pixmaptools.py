d5f5bd55e33db4b11022c00c623907a49c4da33c
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2016-02-29 11:05:42 +0100

    setup: extension module can now be compiled and install.
    sip extension modules can now be managed in the main setup.
    cython module extension was integrated the same way of sip but not TESTED.
    
    sip extension was tested with old pixmaptools.

---
+++ extensions/sip/pixmaptools.py
@@ -0,0 +1,119 @@
+import os
+from distutils.core import setup, Extension
+import numpy
+
+try:
+    import PyQt4.pyqtconfig as pyqtconfig_qt4
+except ImportError:
+    pyqtconfig_qt4 = None
+
+try:
+    import pyqtconfig
+except ImportError:
+    pyqtconfig = None
+
+try:
+    import sipconfig
+    # get sip version
+    str_version = sipconfig.version_to_string(sipconfig.Configuration().sip_version)
+    versions = [int(x) for x in str_version.split('.')]
+    major,minor = versions[0],versions[1]
+except ImportError:
+    sipconfig = None
+
+base_path = os.path.join('bliss','data','routines','pixmaptools')
+
+def get_options():
+    return [
+        ("with-pixmaptools-qt4",None,"compile/install pixmaptools for qt4"),
+        ("with-pixmaptools-qt3",None,"compile/install pixmaptools for qt3"),
+    ]
+
+def get_sip_extension(extra_option):
+    modules = []
+    qt4 = extra_option.get("with_pixmaptools_qt4",False)
+    if qt4:
+        if pyqtconfig_qt4 is None:
+            raise RuntimeError("PyQt4 dev is not install can't build pixmaptools extension")
+
+        # Get the PyQt configuration information.
+        config = pyqtconfig_qt4.Configuration()
+        qt_sip_flags = config.pyqt_sip_flags
+
+        #Local include
+        include_dirs = [os.path.dirname(os.path.realpath(__file__))]
+        #Extra include for numpy
+        include_dirs += [numpy.get_include()]
+        #Extra include for pyqt
+        include_dirs += [config.qt_inc_dir]
+        include_dirs += [os.path.join(config.qt_inc_dir,x) for x in ['QtCore','QtGui']]
+        include_dirs.append(base_path)
+
+        extra_compile_args = pyqtconfig_qt4._default_macros['CXXFLAGS'].split()
+        extra_compile_args += pyqtconfig_qt4._default_macros['CXXFLAGS_THREAD'].split()
+        extra_compile_args += pyqtconfig_qt4._default_macros['CXXFLAGS_WARN_ON'].split()
+
+        extra_link_args = pyqtconfig_qt4._default_macros['LFLAGS'].split()
+
+        library_dirs = pyqtconfig_qt4._default_macros['LIBDIR_QT'].split()
+        extra_libs = ['QtCore','QtGui']
+        sources = ['pixmaptools_io.cpp','pixmaptools_lut.cpp','pixmaptools_stat.cpp']
+        if major == 4 and minor < 12:
+            sources.append('pixmaptools_before_4_12.sip')
+        else:
+            sources.append('pixmaptools.sip')
+        ext = Extension("bliss.data.routines.pixmaptools.qt4",
+                        sources = [os.path.join(base_path,x) for x in sources],
+                        include_dirs = include_dirs,
+                        extra_compile_args=extra_compile_args,
+                        extra_link_args = extra_link_args,
+                        library_dirs = library_dirs,
+                        libraries = extra_libs,
+                        language = 'c++',
+                    )
+
+        sip_flags =  ' '.join(('-I', config.pyqt_sip_dir,qt_sip_flags))
+        modules.append((ext,sip_flags))
+    
+    qt3 = extra_option.get("with_pixmaptools_qt3",False)
+    if qt3:
+        if pyqtconfig is None:
+             raise RuntimeError("PyQt3 dev is not install can't build pixmaptools extension")
+        # Get the PyQt configuration information.
+        config = pyqtconfig.Configuration()
+        qt_sip_flags = config.pyqt_qt_sip_flags
+
+        #Local include
+        include_dirs = [os.path.dirname(os.path.realpath(__file__))]
+        #Extra include for numpy
+        include_dirs += [numpy.get_include()]
+        #Extra include for pyqt
+        include_dirs += [config.qt_inc_dir]
+        include_dirs.append(os.path.join(base_path,'qt3'))
+
+        extra_compile_args = pyqtconfig._default_macros['CXXFLAGS'].split()
+        extra_compile_args += pyqtconfig._default_macros['CXXFLAGS_THREAD'].split()
+        extra_compile_args += pyqtconfig._default_macros['CXXFLAGS_WARN_ON'].split()
+
+        extra_link_args = pyqtconfig._default_macros['LFLAGS'].split()
+
+        library_dirs = pyqtconfig._default_macros['LIBDIR_QT'].split()
+        extra_libs = ['qt-mt']
+        sources = ['pixmaptools_io.cpp','pixmaptools_lut.cpp','pixmaptools_stat.cpp']
+        if major == 4 and minor < 12:
+            sources.append('pixmaptools_qt3_before_4_12.sip')
+        else:
+            sources.append('pixmaptools_qt3.sip')
+
+        ext = Extension("bliss.data.routines.pixmaptools.qt3",
+                        sources = [os.path.join(base_path,'qt3',x) for x in sources],
+                        include_dirs = include_dirs,
+                        extra_compile_args=extra_compile_args,
+                        extra_link_args = extra_link_args,
+                        library_dirs = library_dirs,
+                        libraries = extra_libs,
+                        language = 'c++',
+                        )
+        sip_flags = ' '.join(('-I', config.pyqt_sip_dir,qt_sip_flags))
+        modules.append((ext,sip_flags))
+    return modules
--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-import os
+import os, sys
+import argparse
 from distutils.core import setup
+from distutils.command.build import build
+from distutils.command.install import install
+from distutils.unixccompiler import UnixCCompiler
+
+try:
+    import sipdistutils
+    from extensions import sip as sip_modules
+except ImportError:
+    sipdistutils = None
 
 def get_packages_path():
     packages_path = ['bliss']
@@ -9,6 +19,87 @@ def get_packages_path():
 
     return [dirname.replace(os.path.sep, '.') for dirname in packages_path]
 
+def get_options():
+    user_options = []
+    if sipdistutils is not None:
+        for module_name in sip_modules.__all__:
+            try:
+                m = __import__('extensions.sip.%s' % (module_name),None,None,'extensions.sip.%s' % (module_name))
+            except ImportError:
+                print "Warning optional sip module %s can't be compile, dependency not satisfied." % module_name
+                continue
+            else:
+                try:
+                    func = getattr(m,'get_options')
+                except AttributeError:
+                    continue
+                else:
+                    user_options.extend(func())
+    return user_options
+
+def get_sip_extension(extra_option) :
+    sip_extension_modules = []
+    if sipdistutils is not None:
+        for module_name in sip_modules.__all__:
+            try:
+                m = __import__('extensions.sip.%s' % (module_name),None,None,'extensions.sip.%s' % (module_name))
+            except ImportError:
+                continue
+            else:
+                try:
+                    func = getattr(m,'get_sip_extension')
+                except AttributeError:
+                    continue
+                else:
+                    sip_extension_modules.extend(func(extra_option))
+    return sip_extension_modules
+
+extra_option = {}
+def _initialize_options(self):
+    from distutils.fancy_getopt import longopt_xlate
+    for option,_,_ in get_options():
+        option = option.translate(longopt_xlate)
+        if option[-1] == "=":
+            option = option[:-1]
+        self.__dict__[option] = None
+        extra_option[option] = None
+
+def _finalize_options(self):
+    for key in extra_option:
+        extra_option[key] = self.__dict__[key]
+
+class BlissBuild(build):
+    user_options = build.user_options + get_options()
+    boolean_options = build.boolean_options + [x[0] for x in get_options() if x[0][:-1] != '=']
+
+    def __init__(self,*args,**keys):
+        build.__init__(self,*args,**keys)
+        self._args = {}
+
+    def initialize_options(self):
+        build.initialize_options(self)
+        _initialize_options(self)
+
+    def finalize_options(self):
+        build.finalize_options(self)
+        _finalize_options(self)
+
+class BlissInstall(install):
+    user_options = install.user_options + get_options()
+    boolean_options = install.boolean_options + [x[0] for x in get_options() if x[0][:-1] != '=']
+
+    def __init__(self,*args,**keys):
+        install.__init__(self,*args,**keys)
+        self._args = {}
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        _initialize_options(self)
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        _finalize_options(self)
+   
 setup(name="bliss", version="0.1",
       description="BeamLine Instrumentation Support Software",
       author="BCU (ESRF)",
@@ -22,4 +113,61 @@ setup(name="bliss", version="0.1",
                                              "js/*.*",
                                              "res/*.*"],
                     'bliss.shell.web':['*.html', 'css/*.css', "js/*.js"]},
-      scripts=["bin/beacon-server", "bin/bliss", 'bin/bliss_webserver'])
+      scripts=["bin/beacon-server", "bin/bliss", 'bin/bliss_webserver'],
+      cmdclass = {'build': BlissBuild,
+                  'install': BlissInstall,},
+)
+
+#remove user option from command argument
+final_argv = []
+long_options = set((x[0] for x in get_options()))
+short_option = set((x[1] for x in get_options() if x[1] is not None))
+for arg in sys.argv:
+    pos = arg.find('=')
+    opt = arg[:pos+1] if pos > -1 else arg
+    #long options
+    if opt.startswith('--'):
+        if opt[2:] in long_options:
+            continue
+    elif opt.startswith('-'):
+        if opt[1:] in short_option:
+            continue
+
+    final_argv.append(arg)
+
+sys.argv = final_argv
+#Sip extension modules
+all_sip_extension_modules = get_sip_extension(extra_option)
+
+if all_sip_extension_modules:
+    #merge module by sip_flags
+    modules_by_sipflags = {}
+    for extentions, sip_flags in all_sip_extension_modules:
+        m_list = modules_by_sipflags.get(sip_flags,[])
+        if isinstance(extentions,list):
+            m_list.extend(extentions)
+        else:
+            m_list.append(extentions)
+        modules_by_sipflags[sip_flags] = m_list
+
+    for sip_flags,modules in modules_by_sipflags.iteritems():
+        class build_ext(sipdistutils.build_ext):
+
+            def initialize_options (self):
+                sipdistutils.build_ext.initialize_options(self)
+                self.sip_opts = ' '.join(('-g','-e',sip_flags))
+
+            def build_extensions(self):
+                if isinstance(self.compiler, UnixCCompiler):
+                    compiler_pars = self.compiler.compiler_so
+                    while '-Wstrict-prototypes' in compiler_pars:
+                        del compiler_pars[compiler_pars.index('-Wstrict-prototypes')]
+                sipdistutils.build_ext.build_extensions(self)
+
+        setup(
+            name = 'bliss.siptools',
+            version = '1.0',
+            ext_modules= modules,
+
+            cmdclass = {'build_ext': build_ext},
+        )

98a49f781710bcd6d2cd0e15a6dea695ebfcfc87
blissadm@id30_massif3 <blissadm@id30_massif3>
2016-05-04 10:01:06 +0200

    moved `setup` and related functions code to bliss/shell/__init__.py

--- bliss/__init__.py
+++ bliss/__init__.py
@@ -3,128 +3,13 @@ from __future__ import division
 from gevent import monkey
 monkey.patch_all(thread=False)
 
-from bliss.controllers.motor import Controller, CalcController
-from bliss.common.task_utils import *
 from bliss.config.motors import load_cfg, load_cfg_fromstring, get_axis, get_encoder
 from bliss.controllers.motor_group import Group
 from bliss.common.scans import *
-from bliss.common.task_utils import task, cleanup, error_cleanup
-from bliss import setup_globals
 from bliss.common.continuous_scan import Scan
 from bliss.common.continuous_scan import AcquisitionChain
 from bliss.common.continuous_scan import AcquisitionDevice
 from bliss.common.continuous_scan import AcquisitionMaster
 from bliss.common.axis import Axis
 
-try:
-    from bliss.config import static
-except ImportError:
-    sys.excepthook(*sys.exc_info())
-import functools
-import os
-import sys
-
-try:
-    from tabulate import tabulate
-except ImportError:
-    pass
-
-def setup(setup_file=None, env_dict=None, config_objects_names_list=None, verbose=True):
-    if env_dict: 
-        setup_file = env_dict.get("SETUP_FILE") if setup_file is None else setup_file
-
-    if setup_file is not None:
-        if env_dict is not None:
-            env_dict["SETUP_FILE"] = setup_file
-
-        setup_file_path = os.path.abspath(os.path.expanduser(setup_file))
-
-        if os.path.isfile(setup_file_path):
-            setup_file_dir = os.path.dirname(setup_file_path)
-            if not setup_file_dir in sys.path:
-                sys.path.insert(0, setup_file_dir)
-
-            if verbose:
-                print "Reading setup file '%s`" % setup_file_path
-
-            setattr(setup_globals, "SETUP_FILE", setup_file_path)
-
-            if env_dict is None:
-                # does Python run in interactive mode?
-                import __main__ as main
-                if not hasattr(main, '__file__'):
-                    # interactive interpreter 
-                    env_dict = main.__dict__
-                else:
-                    env_dict = globals()
-
-            _load_config(env_dict, config_objects_names_list, verbose) 
-
-            env_dict['load_script']=functools.partial(_load_script, env_dict)
-            env_dict['wa']=functools.partial(_wa, env_dict)
-
-            try:
-                execfile(setup_file_path, env_dict) 
-            finally:
-                for obj_name, obj in env_dict.iteritems():
-                    setattr(setup_globals, obj_name, obj) 
-
-            if verbose:
-                print "Done."
-            return True
-    raise RuntimeError("No setup file.")
-
-def _load_config(env_dict, names_list=None, verbose=True):
-    try:
-        cfg = static.get_config()
-    except:
-        sys.excepthook(*sys.exc_info())
-        return        
-
-    cfg.reload()
- 
-    if names_list is None:
-        names_list = cfg.names_list
-    for item_name in names_list:
-        if verbose:
-            print "Initializing '%s`" % item_name
-        try:
-            o = cfg.get(item_name)
-        except:
-            sys.excepthook(*sys.exc_info())
-        else:
-            env_dict[item_name] = o
-            setattr(setup_globals, item_name, o)
-            del o
-
-def _load_script(env_dict, script_module_name, path=None):
-    if path is None:
-        setup_file_path = setup_globals.SETUP_FILE
-        path = os.path.join(os.path.dirname(setup_file_path), "scripts")
-
-    if not os.path.isdir(path):
-        raise RuntimeError("Scripts directory '%s` does not exist." % path)
-    if not path in sys.path:
-        sys.path.insert(0, path)
-
-    if script_module_name in sys.modules:
-        reload_module = True
-    else:
-        reload_module = False
-    try:
-        script_module = __import__(script_module_name, globals(), {}, [])
-    except Exception:
-        sys.excepthook(*sys.exc_info())
-    else:
-        if reload_module:
-            reload(script_module)
-        for k, v in script_module.__dict__.iteritems():
-            env_dict[k] = v
-
-def _wa(env_dict):
-  motors = []
-  for name, obj in env_dict.iteritems():
-    if isinstance(obj, Axis):
-        motors.append([obj.name, obj.position(), obj.offset])
-  print str(tabulate(motors, headers=["Axis name", "Position", "Offset"]))
 
--- bliss/shell/__init__.py
+++ bliss/shell/__init__.py
@@ -1,10 +1,26 @@
 import os
 import yaml
 import sys
+import functools
+
+from bliss import setup_globals
+from bliss.common.axis import Axis
+
+try:
+    from bliss.config import static
+except ImportError:
+    sys.excepthook(*sys.exc_info())
+
+try:
+    from tabulate import tabulate
+except ImportError:
+    pass
+
 
 SHELL_CONFIG_FILE = None
 SYNOPTIC = dict()
 SETUP = dict()
+
 
 def set_shell_config_file(cfg_file):
     global SHELL_CONFIG_FILE
@@ -26,6 +42,7 @@ def set_setup_file(session_id, setup_file, config_objects_names):
         config_objects_names = config_objects_names.split()
     SETUP[session_id] = dict(file=os.path.abspath(os.path.expanduser(setup_file)), config_objects=config_objects_names)
 
+
 def read_config(config_file=None):
     if config_file is None:
         config_file = SHELL_CONFIG_FILE
@@ -33,12 +50,18 @@ def read_config(config_file=None):
         return
     with file(config_file, "r") as f:
         cfg = yaml.load(f.read())
+        default_session = None
 
         for session_id in cfg.iterkeys():
+            if default_session is None:
+                default_session = session_id
+
             setup_file = cfg[session_id]["setup-file"]
             if not os.path.isabs(setup_file):
                 setup_file = os.path.join(os.path.dirname(os.path.abspath(config_file)), setup_file)
+
             set_setup_file(str(session_id), setup_file, cfg[session_id].get("config_objects"))
+
             try:
                 synoptic_file = cfg[session_id]["synoptic"]["svg-file"]
             except KeyError:
@@ -47,4 +70,108 @@ def read_config(config_file=None):
                 if not os.path.isabs(synoptic_file):
                     synoptic_file = os.path.join(os.path.dirname(os.path.abspath(config_file)), synoptic_file)
                 set_synoptic_file(str(session_id), synoptic_file, cfg[session_id]["synoptic"]["elements"])
-    return SETUP, SYNOPTIC
+
+    return SETUP, SYNOPTIC, default_session
+
+
+def setup(setup_file=None, env_dict=None, config_objects_names_list=None, verbose=True):
+    if env_dict: 
+        setup_file = env_dict.get("SETUP_FILE") if setup_file is None else setup_file
+
+    if setup_file is not None:
+        if env_dict is not None:
+            env_dict["SETUP_FILE"] = setup_file
+
+        setup_file_path = os.path.abspath(os.path.expanduser(setup_file))
+
+        if os.path.isfile(setup_file_path):
+            setup_file_dir = os.path.dirname(setup_file_path)
+            if not setup_file_dir in sys.path:
+                sys.path.insert(0, setup_file_dir)
+
+            if verbose:
+                print "Reading setup file '%s`" % setup_file_path
+
+            setattr(setup_globals, "SETUP_FILE", setup_file_path)
+
+            if env_dict is None:
+                # does Python run in interactive mode?
+                import __main__ as main
+                if not hasattr(main, '__file__'):
+                    # interactive interpreter 
+                    env_dict = main.__dict__
+                else:
+                    env_dict = globals()
+
+            _load_config(env_dict, config_objects_names_list, verbose) 
+
+            env_dict['load_script']=functools.partial(_load_script, env_dict)
+            env_dict['wa']=functools.partial(_wa, env_dict)
+
+            try:
+                execfile(setup_file_path, env_dict) 
+            finally:
+                for obj_name, obj in env_dict.iteritems():
+                    setattr(setup_globals, obj_name, obj) 
+
+            if verbose:
+                print "Done."
+            return True
+    raise RuntimeError("No setup file.")
+
+
+def _load_config(env_dict, names_list=None, verbose=True):
+    try:
+        cfg = static.get_config()
+    except:
+        sys.excepthook(*sys.exc_info())
+        return        
+
+    cfg.reload()
+ 
+    if names_list is None:
+        names_list = cfg.names_list
+    for item_name in names_list:
+        if verbose:
+            print "Initializing '%s`" % item_name
+        try:
+            o = cfg.get(item_name)
+        except:
+            sys.excepthook(*sys.exc_info())
+        else:
+            env_dict[item_name] = o
+            setattr(setup_globals, item_name, o)
+            del o
+
+
+def _load_script(env_dict, script_module_name, path=None):
+    if path is None:
+        setup_file_path = setup_globals.SETUP_FILE
+        path = os.path.join(os.path.dirname(setup_file_path), "scripts")
+
+    if not os.path.isdir(path):
+        raise RuntimeError("Scripts directory '%s` does not exist." % path)
+    if not path in sys.path:
+        sys.path.insert(0, path)
+
+    if script_module_name in sys.modules:
+        reload_module = True
+    else:
+        reload_module = False
+    try:
+        script_module = __import__(script_module_name, globals(), {}, [])
+    except Exception:
+        sys.excepthook(*sys.exc_info())
+    else:
+        if reload_module:
+            reload(script_module)
+        for k, v in script_module.__dict__.iteritems():
+            env_dict[k] = v
+
+
+def _wa(env_dict):
+  motors = []
+  for name, obj in env_dict.iteritems():
+    if isinstance(obj, Axis):
+        motors.append([obj.name, obj.position(), obj.offset])
+  print str(tabulate(motors, headers=["Axis name", "Position", "Offset"]))

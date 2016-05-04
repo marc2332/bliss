7231a475ee3a27a59c586fdeeae90535447e37a3
blissadm@id30_massif3 <blissadm@id30_massif3>
2016-05-04 10:02:19 +0200

    added TODO for later

--- bliss/__init__.py
+++ bliss/__init__.py
@@ -3,8 +3,10 @@ from __future__ import division
 from gevent import monkey
 monkey.patch_all(thread=False)
 
+## TODO: remove those exported functions ; need changes in motor tests
 from bliss.config.motors import load_cfg, load_cfg_fromstring, get_axis, get_encoder
 from bliss.controllers.motor_group import Group
+##
 from bliss.common.scans import *
 from bliss.common.continuous_scan import Scan
 from bliss.common.continuous_scan import AcquisitionChain

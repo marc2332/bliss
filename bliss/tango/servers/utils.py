913156ddd269d165818efd65342c01f70450e479
Jose Tiago Macara Coutinho <tiago.coutinho@esrf.fr>
2016-06-02 18:41:36 +0200

    bliss tango: prepare bliss to discover other device classes

---
+++ bliss/tango/servers/utils.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+'''
+tango server utilities
+'''
+
+import os
+import sys
+
+import PyTango
+
+
+def get_server_info(argv=None):
+    '''
+    Returns a tuple with three elements: server type, server instance, server name
+
+    Example: ('Bliss', 'sixc', 'Bliss/sixc')
+    '''
+
+    argv = argv or sys.argv
+
+    file_name = os.path.basename(argv[0])
+    server_type = os.path.splitext(file_name)[0]
+    instance_name = argv[1]
+    server_instance = '/'.join((server_type, instance_name))
+    return server_type, instance_name, server_instance
+
+
+def get_devices_from_server(argv=None, db=None):
+    '''
+    Returns the devices already registered. dict< str(tango class name): list(device names) >
+    '''
+    argv = argv or sys.argv
+    db = db or PyTango.Database()
+
+    # get sub devices
+    _, _, personal_name = get_server_info(argv)
+    result = list(db.get_device_class_list(personal_name))
+
+    # dict<dev_name: tango_class_name>
+    dev_dict = dict(zip(result[::2], result[1::2]))
+
+    class_dict = {}
+    for dev, class_name in dev_dict.items():
+        devs = class_dict.setdefault(class_name, [])
+        devs.append(dev)
+
+    class_dict.pop('DServer', None)
+
+    return class_dict

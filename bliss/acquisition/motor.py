5a9a5aa516193980c17b43690de8cf472f935fc2
beteva/bacon <beteva@esrf.fr>
2016-06-15 13:06:04 +0200

    motor.py: fixed typos (old 'emotion' imports)

--- bliss/acquisition/motor.py
+++ bliss/acquisition/motor.py
@@ -7,9 +7,9 @@
 
 from __future__ import absolute_import
 from bliss.common.continuous_scan import AcquisitionMaster
-import bliss
 from bliss.common import axis
-from bliss.common.event import connect, disconnect
+from bliss.common import event
+import bliss
 import numpy
 import gevent
 import sys
@@ -36,11 +36,11 @@ class MotorMaster(AcquisitionMaster):
         start = self._calculate_undershoot(self.start_pos)
         self.movable.move(start)
 
-    def start(self, polling_time=bliss.common.axis.DEFAULT_POLLING_TIME):
+    def start(self, polling_time=axis.DEFAULT_POLLING_TIME):
         self.initial_velocity = self.movable.velocity()
         self.movable.velocity(self.velocity) 
         end = self._calculate_undershoot(self.end_pos,end=True)
-        connect(self.movable, "move_done", self.move_done)
+        event.connect(self.movable, "move_done", self.move_done)
         self.movable.move(end, polling_time=polling_time)
 
     def stop(self):
@@ -49,7 +49,7 @@ class MotorMaster(AcquisitionMaster):
     def move_done(self, done):
         if done:
             self.movable.velocity(self.initial_velocity)
-            disconnect(self.movable, "move_done", self.move_done)
+            event.disconnect(self.movable, "move_done", self.move_done)    
 
 class SoftwarePositionTriggerMaster(MotorMaster):
     def __init__(self, axis, start, end, npoints=1, **kwargs):
@@ -59,7 +59,7 @@ class SoftwarePositionTriggerMaster(MotorMaster):
     def start(self):
         self.exception = None
         self.index = 0
-        connect(self.movable, "position", self.position_changed)
+        event.connect(self.movable, "position", self.position_changed)
         MotorMaster.start(self, 1E-6)
         if self.exception:
             raise self.exception[0], self.exception[1], self.exception[2]
@@ -78,12 +78,12 @@ class SoftwarePositionTriggerMaster(MotorMaster):
           try:
               self.trigger_slaves()
           except Exception:
-              disconnect(self.movable, "position", self.position_changed)
+              event.disconnect(self.movable, "position", self.position_changed)
               self.movable.stop(wait=False)
               self.exception = sys.exc_info()
         
     def move_done(self, done):
         if done:
-            disconnect(self.movable, "position", self.position_changed)
+            event.disconnect(self.movable, "position", self.position_changed)
         MotorMaster.move_done(self, done) 
        

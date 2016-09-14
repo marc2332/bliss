f1d577c5df45d80f32e1fc53e44fcdaadc009c8f
bliss administrator <blissadm@lid287.esrf.fr>
2016-09-14 14:37:54 +0200

    added copyright notice

--- bliss/controllers/motors/leica.py
+++ bliss/controllers/motors/leica.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) 2016 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+
 from bliss.controllers.motor import Controller
 from bliss.controllers import _leica_usb as leica_usb
 from bliss.common.axis import AxisState

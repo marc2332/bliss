8a3da288c5856b54681a6182c93b1ecdf5b8ae21
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2016-09-16 13:59:21 +0200

    aerotech: first version of A3200 over Taco server

---
+++ bliss/controllers/motors/Aerotech_Taco.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) 2016 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+
+from bliss.controllers.motors.TacoMaxe import TacoMaxe
+
+class Aerotech_Taco(TacoMaxe):
+    """
+    Specialisation of TacoMaxe with special command
+    to control aerotech controller A3200
+    """
+    
+    ENCODER_DIVIDER_PARAMETER = 209
+    
+    def __init__(self, name, config, axes, encoders):
+        TacoMaxe.__init__(self, name, config, axes, encoders)
+        self._jog_axis = None
+        
+    def initialize_hardware(self):
+        #enable encoder output
+        self.device.DevWriteProg("ENCODER OUT X ON 7,8")
+        self.device.DevExecProg(0)
+
+    def initialize_hardware_axis(self, axis):
+        encoder_divider = axis.config.get("encoder_divider",int)
+        self.device.DevSetParam((axis.channel,
+                                 self.ENCODER_DIVIDER_PARAMETER,
+                                 0,encoder_divider))
+        axis.encoder_divider = encoder_divider
+
+    def start_jog(self, axis, velocity, direction):
+        self.device.DevSetContinuous((axis.channel,
+                                      1,
+                                      direction,
+                                      velocity,
+                                      axis.myacceleration))
+        self._job_axis = axis
+
+    def stop_jog(self, axis):
+        self.device.DevSetContinuous((axis.channel,
+                                      0,
+                                      0,
+                                      0,
+                                      0))

6b50a4c1fd80451ba1c3f0429565c8ef650186f8
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2016-09-21 14:10:11 +0200

    config: clean configuration node after creating the object.
    This may cause some problem if we try to save the modify node.

--- bliss/config/plugins/bliss.py
+++ bliss/config/plugins/bliss.py
@@ -29,13 +29,20 @@ def create_objects_from_config_node(config, item_cfg_node):
 
     item_name = item_cfg_node["name"]
     referenced_objects = dict()
+    item_cfg_node_2_clean = set()
+
     for name, value in item_cfg_node.iteritems():
         if isinstance(value, str) and value.startswith("$"):
             # convert reference to item from config
-            item_cfg_node[name]=config.get(value)
+            obj = config.get(value)
+            item_cfg_node[name]=obj
+            item_cfg_node_2_clean.add(name)
             referenced_objects[name]=item_cfg_node[name]
 
     o = klass(item_name, item_cfg_node)
+
+    for name in item_cfg_node_2_clean:
+        item_cfg_node.pop(name)
 
     for name, object in referenced_objects.iteritems():
         if hasattr(o, name):

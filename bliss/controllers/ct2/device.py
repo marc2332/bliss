b62a2a5486676ad58641c959d391026a909fb42b
Jose Tiago Macara Coutinho <coutinhotiago@gamil.com>
2016-12-07 17:17:19 +0100

    ct2: add wait_acq

--- bliss/controllers/ct2/device.py
+++ bliss/controllers/ct2/device.py
@@ -216,7 +216,7 @@ class CT2Device(BaseCT2Device):
         self.__card = card
         self.__card_config = card_config
         self.__acq_mode = acq_mode
-        self.__out_config = dict(out_config or {}) 
+        self.__out_config = dict(out_config or {})
         self.__acq_status = AcqStatus.Ready
         self.__acq_expo_time = 1.0
         self.__acq_point_period = None
@@ -505,10 +505,14 @@ class CT2Device(BaseCT2Device):
         else:
             raise NotImplementedError
 
+    def __on_acq_loop_finished(self, event_loop):
+        self.__event_loop = None
+
     def start_acq(self):
         self.__acq_status = AcqStatus.Running
         try:
             self.__event_loop = gevent.spawn(self.run_acq_loop)
+            self.__event_loop.link(self.__on_acq_loop_finished)
 
             if self.acq_mode in self.StdModes:
                 counters = (self.internal_point_nb_counter,)
@@ -537,6 +541,10 @@ class CT2Device(BaseCT2Device):
         gevent.wait([self.__event_loop])
         self.__event_loop = None
         self._send_status(self.__acq_status)
+
+    def wait_acq(self):
+        if self.__event_loop:
+            gevent.wait([self.__event_loop])
 
     def trigger_latch(self, counters):
         self.card.trigger_counters_software_latch(counters)

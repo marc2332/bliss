44efd930e26f387f4bc5953f4360969a825c9bed
bliss administrator <blissadm@sybil.esrf.fr>
2017-04-20 10:42:47 +0200

    tango_keithley: added .autozero()

--- bliss/controllers/tango_keithley.py
+++ bliss/controllers/tango_keithley.py
@@ -31,3 +31,9 @@ class tango_keithley(CounterBase):
         else:
             self.__control.autorange = autorange_on
 
+    def autozero(self, autozero_on=None):
+        if autozero_on is None:
+            return self.__control.autozero
+        else:
+            self.__control.autozero = autozero_on
+

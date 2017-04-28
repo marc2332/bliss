2eae59f9ff9de74a6ee97216852045737a3a26f7
Matias Guijarro <guijarro@id02det1.esrf.fr>
2017-04-28 15:35:13 +0200

    controllers/motor.py: removed comments

--- bliss/controllers/motor.py
+++ bliss/controllers/motor.py
@@ -376,6 +376,7 @@ class CalcController(Controller):
         for tagged_axis_name, dial_pos in new_positions.iteritems():
             axis = self._tagged[tagged_axis_name][0]
             if axis in self.pseudos:
+                #dial_pos = position / axis.steps_per_unit
                 user_pos = axis.dial2user(dial_pos)
                 axis.settings.set("dial_position", dial_pos, write=motion_control)
                 axis.settings.set("position", user_pos, write=False)
@@ -389,7 +390,6 @@ class CalcController(Controller):
 
     def _real_move_done(self, done):
         if done:
-            #print 'MOVE DONE'
             for axis in self.pseudos:
                 if axis.encoder:
                     # check position and raise RuntimeError if encoder

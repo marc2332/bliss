051c942f6ec600a4cb6f36a8af9ece6f0c242cee
blissadm@id30_massif3 <blissadm@id30_massif3>
2017-07-11 15:28:22 +0200

    common/axis.py: allow multi-line states

--- bliss/common/axis.py
+++ bliss/common/axis.py
@@ -1238,7 +1238,7 @@ class AxisState(object):
         # (copy constructor)
         if '(' in state:
             full_states = [s.strip() for s in state.split('|')]
-            p = re.compile('^([A-Z0-9]+)\s\((.+)\)$')
+            p = re.compile('^([A-Z0-9]+)\s\((.+)\)', re.DOTALL)
             for full_state in full_states:
                 m = p.match(full_state)
                 state = m.group(1)

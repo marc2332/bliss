eb918c7d4a3d38f683b9b0c16f767fa830b31f2b
Jose Tiago Coutinho Macara <tiago.coutinho@esrf.fr>
2017-08-11 09:39:33 +0200

    scan: better scan/ct output

--- bliss/shell/__init__.py
+++ bliss/shell/__init__.py
@@ -10,6 +10,7 @@
 import os
 import yaml
 import sys
+import time
 import logging
 import functools
 from six import print_
@@ -58,6 +59,24 @@ def initialize(*session_names):
 
     return user_ns,sessions
 
+
+def _obj_unit(name):
+    config = static.get_config()
+    if isinstance(name, (str, unicode)):
+        try:
+            obj = config.get(name)
+        except:
+            return ''
+    else:
+        obj = name
+        name = obj.name
+    try:
+        return obj.unit or ''
+    except AttributeError:
+        obj_config = config.get_config(name)
+        return obj_config.get('unit', '')
+    
+
 class ScanListener:
     '''listen to scan events and compose output'''
 
@@ -67,6 +86,8 @@ class ScanListener:
              "{title}\n\n" + \
              "{column_header}"
 
+    DEFAULT_WIDTH = 12
+
     def __init__(self):
         event.connect(scan, 'scan_new', self.__on_scan_new)
         event.connect(scan, 'scan_data', self.__on_scan_data)
@@ -74,43 +95,65 @@ class ScanListener:
 
     def __on_scan_new(self, scan_info, filename, motor_names, nb_points, counter_names):
         scan_info = dict(scan_info)
-        if scan_info['type'] == 'ct':
-            return
         if isinstance(motor_names, str):
             motor_names = [motor_names]
-        col_names = motor_names + counter_names
-        point_nb_col_len = len(str(nb_points-1)) + 1 if nb_points else 6
-        col_lens = map(len, col_names)
-        col_templs = ["{{0:>{0}}}".format(min(col_len, 8)) for col_len in col_lens]
-        col_names.insert(0, '#')
-        col_templs.insert(0, "{{0:>{0}}}".format(point_nb_col_len))
-        col_header = "  ".join([col_templs[i].format(m) for i, m in enumerate(col_names)])
+        col_labels = ['#']
+        for motor_name in motor_names:
+            # replace time_stamp with elapsed_time
+            if motor_name == 'timestamp':
+                motor_name = 'dt'
+                unit = 's'
+            else:
+                unit = _obj_unit(motor_name)
+            motor_label = motor_name
+            if unit:
+                motor_label += '({0})'.format(unit)
+            col_labels.append(motor_label)
+        for counter_name in counter_names:
+            counter_label = counter_name
+            unit = _obj_unit(counter_name)
+            if unit:
+                counter_label += '({0})'.format(unit)
+            col_labels.append(counter_label)
+        
+        self.col_labels = col_labels
+
         if not scan_info['save']:
             scan_info['root_path'] = '<no file>'
-        header = self.HEADER.format(column_header=col_header,**scan_info)
-        self.col_templs = ["{{0:>{0}g}}".format(min(col_len, 8)) for col_len in col_lens]
-        self.col_templs.insert(0, "{{0:>{0}g}}".format(point_nb_col_len))
+
+        if scan_info['type'] == 'ct':
+            return
+
+        col_lens = map(lambda x: max(len(x), self.DEFAULT_WIDTH), col_labels)
+        h_templ = ["{{0:>{width}}}".format(width=col_len)
+                   for col_len in col_lens]
+        header = "  ".join([templ.format(label)
+                            for templ, label in zip(h_templ, col_labels)])
+        header = self.HEADER.format(column_header=header, **scan_info)
+        self.col_templ = ["{{0: >{width}}}".format(width=col_len) 
+                          for col_len in col_lens]
         self._point_nb = 0
         print_(header)
 
     def __on_scan_data(self, scan_info, values):
+        elapsed_time = time.time() - scan_info['start_time_stamp']
+        values = [elapsed_time] + values[1:]
         if scan_info['type'] == 'ct':
             # ct is actually a timescan(npoints=1).
-            names, values = scan_info['counter_names'], values
-            # First value is elapsed time since timescan started. We don't need it here
-            values = values[1:]
+            
             norm_values = numpy.array(values) / scan_info['count_time']
-            col_len = max(map(len, names)) + 2
-            template = '{{0:>{0}}} = {{1: 10g}} ({{2: 10g}}/s)'.format(col_len)
-            lines = "\n".join([template.format(name, v, nv)
-                               for name, v, nv in zip(names, values, norm_values)])
+            col_len = max(map(len, self.col_labels)) + 2
+            template = '{{label:>{0}}} = {{value: >12}} ({{norm: 12}}/s)'.format(col_len)
+            lines = "\n".join([template.format(label=label, value=v, norm=nv)
+                               for label, v, nv in zip(self.col_labels[1:], 
+                                                      values, norm_values)])
             end_time_str = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Y")
             msg = '\n{0}\n\n{1}'.format(end_time_str, lines)
             print_(msg)
         else:
-            values = [self._point_nb] + values
+            values.insert(0, self._point_nb)
             self._point_nb += 1
-            line = "  ".join([self.col_templs[i].format(v) for i, v in enumerate(values)])
+            line = "  ".join([self.col_templ[i].format(v) for i, v in enumerate(values)])
             print_(line)
 
     def __on_scan_end(self, scan_info):

6fde1c0f7bf7fb71a5eec6a1c21e974a27aec8b4
Vincent Michel <vincent.michel@esrf.fr>
2017-10-18 13:04:26 +0200

    Provide pytango enumeration as a reference

--- bliss/common/tango.py
+++ bliss/common/tango.py
@@ -1,30 +1,65 @@
-from mock import Mock
-import sys
+"""Compatibility module for pytango."""
+
+from enum import IntEnum
+
+__all__ = ['AttrQuality', 'EventType', 'DevState',
+           'AttributeProxy', 'DeviceProxy']
+
+
+class AttrQuality(IntEnum):
+    ATTR_VALID = 0
+    ATTR_INVALID = 1
+    ATTR_ALARM = 2
+    ATTR_CHANGING = 3
+    ATTR_WARNING = 4
+
+
+class EventType(IntEnum):
+    CHANGE_EVENT = 0
+    QUALITY_EVENT = 1
+    PERIODIC_EVENT = 2
+    ARCHIVE_EVENT = 3
+    USER_EVENT = 4
+    ATTR_CONF_EVENT = 5
+    DATA_READY_EVENT = 6
+    INTERFACE_CHANGE_EVENT = 7
+    PIPE_EVENT = 8
+
+
+class DevState(IntEnum):
+    ON = 0
+    OFF = 1
+    CLOSE = 2
+    OPEN = 3
+    INSERT = 4
+    EXTRACT = 5
+    MOVING = 6
+    STANDBY = 7
+    FAULT = 8
+    INIT = 9
+    RUNNING = 10
+    ALARM = 11
+    DISABLE = 12
+    UNKNOWN = 13
+
 
 def DeviceProxy(*args, **kwargs):
-    raise RuntimeError("Tango is not imported. Hint: is tango Python module installed ?")
+    raise RuntimeError(
+        "Tango is not imported. Hint: is tango Python module installed ?")
+
 
 def AttributeProxy(*args, **kwargs):
-    raise RuntimeError("Tango is not imported. Hint: is tango Python module installed ?")
+    raise RuntimeError(
+        "Tango is not imported. Hint: is tango Python module installed ?")
+
 
 try:
-    from tango.gevent import DeviceProxy
+    from tango import AttrQuality, EventType, DevState
+    from tango.gevent import DeviceProxy, AttributeProxy
 except ImportError:
+    # PyTango < 9 imports
     try:
-        from PyTango.gevent import DeviceProxy
+        from PyTango import AttrQuality, EventType, DevState
+        from PyTango.gevent import DeviceProxy, AttributeProxy
     except ImportError:
-        PyTango = Mock()
-        PyTango.DeviceProxy = DeviceProxy
-        PyTango.AttributeProxy = AttributeProxy
-        sys.modules['PyTango'] = PyTango
-    else:
-        from PyTango import AttributeProxy
-        import PyTango
-else:
-    from tango.gevent import AttributeProxy
-    import tango as PyTango
-
-AttrQuality = PyTango.AttrQuality
-DevState = PyTango.DevState
-EventType = PyTango.EventType
-
+        pass

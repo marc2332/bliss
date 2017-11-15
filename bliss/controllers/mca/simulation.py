7f8be750f27b50be2a83e652696ebeeb711488ff
Vincent Michel <vincent.michel@esrf.fr>
2017-11-15 17:40:42 +0100

    Add multiple acquisitions to simulated mca

--- bliss/controllers/mca/simulation.py
+++ bliss/controllers/mca/simulation.py
@@ -14,6 +14,7 @@ class SimulatedMCA(BaseMCA):
     _prepare_time = 0.5
     _cleanup_time = 0.5
     _gate_end = 0.5
+    _mapping_modulo = 2
 
     # Initialization
 
@@ -54,7 +55,7 @@ class SimulatedMCA(BaseMCA):
 
     @property
     def supported_trigger_modes(self):
-        return TriggerMode.SOFTWARE, TriggerMode.GATE
+        return TriggerMode.SOFTWARE, TriggerMode.GATE, TriggerMode.EXTERNAL
 
     def set_trigger_mode(self, mode):
         if mode is None:
@@ -73,7 +74,7 @@ class SimulatedMCA(BaseMCA):
     def block_size(self):
         return self._block_size or 100
 
-    def set_block_size(elf, value=None):
+    def set_block_size(self, value=None):
         self._block_size = value
 
     # Acquisition control
@@ -82,6 +83,7 @@ class SimulatedMCA(BaseMCA):
         if not self._running:
             gevent.sleep(self._prepare_time)
             self._t0 = time.time()
+            self._count = -1
         self._running = True
 
     def stop_acquisition(self):
@@ -103,25 +105,53 @@ class SimulatedMCA(BaseMCA):
     # Get data
 
     def get_acquisition_data(self):
-        data = dict((i, numpy.zeros(1024)) for i in range(self.elements))
-        events = int(0.9 * 10000 * self.delta / 2)
-        for _ in range(events):
-            loc = numpy.random.normal(512, 64)
-            for i in range(self.elements):
-                e = int(numpy.random.normal(loc, 64))
-                data[i][e] += 1
-        return data
+        a, b = self._generate_pixel(self.delta)
+        return a
 
     def get_acquisition_statistics(self):
-        realtime = self.delta
+        a, b = self._generate_pixel(self.delta)
+        return b
+
+    def poll_data(self):
+        self._count += 1
+        current = self._count // self._mapping_modulo
+        # Nothing happened
+        if self._count % self._mapping_modulo != 0:
+            return current, {}, {}
+        # New buffer
+        if current and current % self.block_size == 0:
+            a, b = self._generate_pixels(current-self.block_size, current)
+            return current, a, b
+        # Finished
+        if current == self.acquisition_number:
+            start = (current // self.block_size) * self.block_size
+            a, b = self._generate_pixels(start, current)
+            return current, a, b
+        # A new pixel has been generated
+        return current, {}, {}
+
+    # Data generation
+
+    def _generate_pixels(self, start, stop):
+        da, db = {}, {}
+        for i in range(start, stop):
+            da[i], db[i] = self._generate_pixel(0.2 * self._mapping_modulo)
+        return da, db
+
+    def _generate_pixel(self, delta):
+        realtime = delta
         livetime = realtime * 0.9
         triggers = int(livetime * 10000)
         events = triggers // 2
         icr = triggers / realtime
         ocr = events / livetime
         deadtime = 1 - ocr / icr
-        stats = Stats(realtime, livetime, triggers, events, icr, ocr, deadtime)
-        return dict((i, stats) for i in range(self.elements))
-
-    def poll_data(self):
-        raise RuntimeError
+        st = Stats(realtime, livetime, triggers, events, icr, ocr, deadtime)
+        stats = dict((i, st) for i in range(self.elements))
+        data = dict((i, numpy.zeros(1024)) for i in range(self.elements))
+        for _ in range(events):
+            loc = numpy.random.normal(512, 64)
+            for i in range(self.elements):
+                e = int(numpy.random.normal(loc, 64))
+                data[i][e] += 1
+        return data, stats
--- tests/mca/test_simulation.py
+++ tests/mca/test_simulation.py
@@ -24,3 +24,19 @@ def test_simulated_mca_external_acquisition(beacon, mocker):
     a, b = s.run_external_acquisition()
     assert len(a) == len(b) == 4
     assert b[0].realtime == 0.5
+
+
+def test_simulated_mca_multiple_acquisitions(beacon, mocker):
+    beacon.reload()
+    times = [10., 14.][::-1]
+    m1 = mocker.patch('gevent.sleep')
+    m2 = mocker.patch('time.time')
+    m2.side_effect = times.pop
+    s = beacon.get('simu1')
+
+    a, b = s.run_multiple_acquisitions(10, block_size=3)
+    assert len(a) == len(b) == 10
+    for i in range(10):
+        assert len(a[i]) == len(b[i]) == 4
+        for j in range(4):
+            assert b[i][j].realtime == 0.4

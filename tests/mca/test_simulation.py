2dcf4d6ae8a9dee250ded3b880523042c6ce9a54
Vincent Michel <vincent.michel@esrf.fr>
2017-11-17 17:18:49 +0100

    Update mca tests

--- tests/mca/test_simulation.py
+++ tests/mca/test_simulation.py
@@ -1,40 +1,44 @@
 
 
-def test_simulated_mca_single_acquisition(beacon, mocker):
+def test_simulated_mca_software_acquisition(beacon, mocker):
     beacon.reload()
-    times = [10., 11.][::-1]
+    times = range(30)[::-1]
     m1 = mocker.patch('gevent.sleep')
     m2 = mocker.patch('time.time')
     m2.side_effect = times.pop
     s = beacon.get('simu1')
 
-    a, b = s.run_single_acquisition(1.)
-    assert len(a) == len(b) == 4
-    assert b[0].realtime == 1.
+    a, b = s.run_software_acquisition(10, 1.)
+    for i in range(10):
+        assert len(a[i]) == len(b[i]) == 4
+        for j in range(4):
+            assert b[i][j].realtime == 1.
 
 
-def test_simulated_mca_external_acquisition(beacon, mocker):
+def test_simulated_mca_gate_acquisition(beacon, mocker):
     beacon.reload()
-    times = [10., 10.2, 10.4, 10.6, 10.8, 11.][::-1]
+    times = range(30)[::-1]
     m1 = mocker.patch('gevent.sleep')
     m2 = mocker.patch('time.time')
     m2.side_effect = times.pop
     s = beacon.get('simu1')
 
-    a, b = s.run_external_acquisition()
-    assert len(a) == len(b) == 4
-    assert b[0].realtime == 0.5
+    a, b = s.run_gate_acquisition(10)
+    for i in range(10):
+        assert len(a[i]) == len(b[i]) == 4
+        for j in range(4):
+            assert b[i][j].realtime == .5
 
 
 def test_simulated_mca_multiple_acquisitions(beacon, mocker):
     beacon.reload()
-    times = [10., 14.][::-1]
+    times = range(30)[::-1]
     m1 = mocker.patch('gevent.sleep')
     m2 = mocker.patch('time.time')
     m2.side_effect = times.pop
     s = beacon.get('simu1')
 
-    a, b = s.run_multiple_acquisitions(10, block_size=3)
+    a, b = s.run_synchronized_acquisition(10, block_size=3)
     assert len(a) == len(b) == 10
     for i in range(10):
         assert len(a[i]) == len(b[i]) == 4

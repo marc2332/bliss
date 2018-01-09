c0fc969c8260d0dec277811054bde60d72786df8
Vincent Michel <vincent.michel@esrf.fr>
2018-01-09 20:25:42 +0100

    Use a python subprocess instead of Popen to fire up the beacon server

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,31 +5,33 @@
 # Copyright (c) 2016 Beamline Control Unit, ESRF
 # Distributed under the GNU LGPLv3. See LICENSE for more info.
 
-import pytest
-import subprocess
 import os
 import time
+import multiprocessing
+
+import redis
+import pytest
 
 from bliss.config import static
 from bliss.config.conductor import client
 from bliss.config.conductor import connection
-import redis
 
 BLISS = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-BEACON = ['python', '-m', 'bliss.config.conductor.server']
 BEACON_DB_PATH = os.path.join(BLISS, 'tests', 'test_configuration')
 BEACON_PORT = 7655
 
 
 @pytest.fixture(scope="session")
 def beacon():
+    from bliss.config.conductor import server
     args = [
         '--port=%d' % BEACON_PORT,
         '--redis_port=7654',
         '--redis_socket=/tmp/redis_test.sock',
         '--db_path='+BEACON_DB_PATH,
         '--posix_queue=0']
-    p = subprocess.Popen(BEACON + args)
+    proc = multiprocessing.Process(target=server.main, args=(args,))
+    proc.start()
     time.sleep(0.5)  # wait for beacon to be really started
     redis_db = redis.Redis(port=7654)
     redis_db.flushdb()
@@ -37,4 +39,4 @@ def beacon():
     client._default_connection = beacon_connection
     cfg = static.get_config()
     yield cfg
-    p.terminate()
+    proc.terminate()

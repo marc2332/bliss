e2e746db758fa765716758b73dda70bbcc0253b9
CyrilGuilloud@pcsht <cyril.guilloud@esrf.fr>
2018-01-19 17:29:02 +0100

    * update  __init__() : 6 params to **kwargs to match new API. * fix import of file names (now lower cases) * pep8

--- bliss/controllers/motors/aerotech_taco.py
+++ bliss/controllers/motors/aerotech_taco.py
@@ -2,35 +2,35 @@
 #
 # This file is part of the bliss project
 #
-# Copyright (c) 2016 Beamline Control Unit, ESRF
+# Copyright (c) 2016-2018 Beamline Control Unit, ESRF
 # Distributed under the GNU LGPLv3. See LICENSE for more info.
 
 from bliss.controllers.motors.TacoMaxe import TacoMaxe
+
 
 class Aerotech_Taco(TacoMaxe):
     """
     Specialisation of TacoMaxe with special command
     to control aerotech controller A3200
     """
-    
     ENCODER_DIVIDER_PARAMETER = 209
-    
-    def __init__(self, name, config, axes, encoders):
-        TacoMaxe.__init__(self, name, config, axes, encoders)
-        
+
+    def __init__(self, *args, **kwargs):
+        TacoMaxe.__init__(self, *args, **kwargs)
+
     def initialize_hardware(self):
-        #enable encoder output
+        # enables encoder output
         self.device.DevWriteProg("ENCODER OUT X ON 7,8")
         self.device.DevExecProg(0)
 
     def initialize_hardware_axis(self, axis):
         if axis.encoder:
-            encoder_divider = axis.encoder.config.get("divider",int)
+            encoder_divider = axis.encoder.config.get("divider", int)
             axis.encoder.axis = axis
             axis.encoder.divider = encoder_divider
             self.device.DevSetParam((axis.channel,
                                      self.ENCODER_DIVIDER_PARAMETER,
-                                     0,encoder_divider))
+                                     0, encoder_divider))
 
     def start_jog(self, axis, velocity, direction):
         acceleration = axis.acceleration()
@@ -47,8 +47,8 @@ class Aerotech_Taco(TacoMaxe):
                                       0,
                                       0))
 
-    def initialize_encoder(self,encoder):
+    def initialize_encoder(self, encoder):
         pass                    # noting to do
 
-    def read_encoder(self,encoder):
+    def read_encoder(self, encoder):
         return self.read_position(encoder.axis) / encoder.divider

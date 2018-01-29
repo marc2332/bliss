667e4aef8514e725173d8bfcafb3bb45ee398fab
Vincent Michel <vincent.michel@esrf.fr>
2018-01-29 18:24:00 +0100

    Enhance PEPU tests

--- tests/controllers_sw/test_pepu.py
+++ tests/controllers_sw/test_pepu.py
@@ -7,16 +7,63 @@ Run with:
 
 """
 
+# Imports
+
+from functools import partial
+from contextlib import contextmanager
+
 import mock
 import pytest
 
 from bliss.controllers.pepu import PEPU, Signal, Trigger
 
 
+# Helpers
+
+@contextmanager
+def pepu_assert_command(pepu, write, read):
+    conn = pepu.conn
+    conn._write.reset_mock()
+    conn._readline.reset_mock()
+    conn._readline.return_value = read
+    try:
+        yield mock
+        conn._write.assert_called_once_with(write+'\n')
+        conn._readline.assert_called_once()
+    finally:
+        conn._write.reset_mock()
+        conn._readline.return_value = None
+        conn._readline.reset_mock()
+
+
+@contextmanager
+def pepu_assert_block(pepu, n, name='TEST'):
+    conn = pepu.conn
+    cmd = '?*DSTREAM %s READ %d' % (name, n)
+    with pepu.assert_command(cmd, '?*DSTREAM'):
+        header = b'\x01\x00\xa5\xa5'
+        header += b'\x00\x00\x00\x00'
+        header += b'\x00\x00\x00\x00'
+        binary = b'\x80\x01\x00\x00\x00\x00\x00\x00'
+        binary += b'\x80\xfe\xff\xff\xff\xff\x00\x00'
+        values = [header, binary * n]
+        conn._read.side_effect = lambda *args, **kwargs: values.pop(0)
+        try:
+            yield mock
+            assert pepu.conn._read.call_count == 2
+        finally:
+            conn._read.side_effect = None
+            conn._read.reset_mock()
+
+
+# Fixtures
+
 @pytest.fixture
 def pepu():
     with mock.patch('bliss.controllers.pepu.get_comm') as get_comm:
         pepu = PEPU('test', {'tcp': {'url': 'pepudcm2'}})
+        pepu.assert_block = partial(pepu_assert_block, pepu)
+        pepu.assert_command = partial(pepu_assert_command, pepu)
         get_comm.assert_called_once_with(
             {'tcp': {'url': 'command://pepudcm2:5000'}}, 'tcp', eol='\n')
         try:
@@ -29,49 +76,54 @@ def pepu():
             pepu.conn.close()
 
 
+# Tests
+
 def test_simple_connection(pepu):
-    pepu.conn._readline.return_value = 'PEPU'
-    assert pepu.app_name == 'PEPU'
+    with pepu.assert_command('?APPNAME', 'PEPU'):
+        assert pepu.app_name == 'PEPU'
 
-    pepu.conn._readline.return_value = '00.01'
-    assert pepu.version == '00.01'
+    with pepu.assert_command('?VERSION', '00.01'):
+        assert pepu.version == '00.01'
 
-    pepu.conn._readline.return_value = "3.4"
-    assert pepu.up_time == 3.4
+    with pepu.assert_command('?UPTIME', '3.4'):
+        assert pepu.up_time == 3.4
 
-    pepu.conn._readline.return_value = 'DANCE BLABLABLA'
-    assert pepu.sys_info.startswith('DANCE')
+    with pepu.assert_command('?SYSINFO', 'DANCE BLABLABLA'):
+        assert pepu.sys_info.startswith('DANCE')
 
-    pepu.conn._readline.return_value = 'UPTIME: BLI\nUNAME: BLA'
-    uptime, uname = pepu.dance_info.splitlines()
-    assert uptime.startswith('UPTIME')
-    assert uname.startswith('UNAME')
+    with pepu.assert_command('?DINFO', 'UPTIME: BLI\nUNAME: BLA'):
+        uptime, uname = pepu.dance_info.splitlines()
+        assert uptime.startswith('UPTIME')
+        assert uname.startswith('UNAME')
 
-    pepu.conn._readline.return_value = '# %APPNAME% PEPU\n BLABLABLA'
-    assert pepu.config.startswith('# %APPNAME% PEPU')
+    with pepu.assert_command('?DCONFIG', '# %APPNAME% PEPU\n BLABLABLA'):
+        assert pepu.config.startswith('# %APPNAME% PEPU')
 
 
 @pytest.mark.parametrize("channel_id", range(1, 7))
 def test_read_in_channels(pepu, channel_id):
-    pepu.conn._readline.return_value = '-1.2'
-    channel = pepu.in_channels[channel_id]
-    assert channel.value == -1.2
+    cmd = '?CHVAL IN%d' % channel_id
+    with pepu.assert_command(cmd, '-1.2'):
+        channel = pepu.in_channels[channel_id]
+        assert channel.value == -1.2
 
 
 @pytest.mark.parametrize("channel_id", [7, 8])
 def test_read_out_channels(pepu, channel_id):
-    pepu.conn._readline.return_value = '-1'
-    channel = pepu.out_channels[channel_id]
-    value = channel.value
+    cmd = '?CHVAL OUT%d' % channel_id
+    with pepu.assert_command(cmd, '-1'):
+        channel = pepu.out_channels[channel_id]
+        value = channel.value
     pytest.xfail()
     assert value in (1.5, -1.5)
 
 
 @pytest.mark.parametrize("channel_id", [1, 2])
 def test_read_calc_channels(pepu, channel_id):
-    pepu.conn._readline.return_value = '-1'
-    channel = pepu.calc_channels[channel_id]
-    value = channel.value
+    cmd = '?CHVAL CALC%d' % channel_id
+    with pepu.assert_command(cmd, '-1'):
+        channel = pepu.calc_channels[channel_id]
+        value = channel.value
     pytest.xfail()
     assert value in (1.5, -1.5)
 
@@ -80,16 +132,6 @@ def test_read_calc_channels(pepu, channel_id):
 @pytest.mark.parametrize("blocks", [1, 2, 10])
 @pytest.mark.parametrize("block_size", [1, 2, 10])
 def test_streams_acquisition(pepu, acquisitions, blocks, block_size):
-
-    def mock_block(mock, n):
-        mock._readline.return_value = '?*DSTREAM'
-        header = b'\x01\x00\xa5\xa5'
-        header += b'\x00\x00\x00\x00'
-        header += b'\x00\x00\x00\x00'
-        binary = b'\x80\x01\x00\x00\x00\x00\x00\x00'
-        binary += b'\x80\xfe\xff\xff\xff\xff\x00\x00'
-        values = [header, binary * n]
-        mock._read.side_effect = lambda *args, **kwargs: values.pop(0)
 
     # Create stream
     stream = pepu.create_stream(
@@ -100,38 +142,47 @@ def test_streams_acquisition(pepu, acquisitions, blocks, block_size):
         overwrite=True)
 
     # Mocking
-    pepu.conn._readline.return_value = (
+    command = '?DSTREAM TEST'
+    return_value = (
         'TEST OFF GLOBAL TRIG SOFT SOFT FSAMPL 10HZ NSAMPL {} SRC CALC1 CALC2'
         .format(blocks * block_size))
 
     # General testing
+
     assert stream.name == 'TEST'
-    assert stream.trigger == Trigger(Signal.SOFT, Signal.SOFT)
-    assert stream.frequency == 10
-    assert stream.nb_points == blocks * block_size
-    assert stream.sources == ['CALC1', 'CALC2']
-    assert not stream.active
+    with pepu.assert_command(command, return_value):
+        assert stream.trigger == Trigger(Signal.SOFT, Signal.SOFT)
+    with pepu.assert_command(command, return_value):
+        assert stream.frequency == 10
+    with pepu.assert_command(command, return_value):
+        assert stream.nb_points == blocks * block_size
+    with pepu.assert_command(command, return_value):
+        assert stream.sources == ['CALC1', 'CALC2']
+    with pepu.assert_command(command, return_value):
+        assert not stream.active
 
     # Loop over acquisitions
     for _ in range(acquisitions):
         pepu.conn._readline.return_value = ''
-        stream.start()
+
+        with pepu.assert_command('#DSTREAM TEST APPLY', 'ACK IS NOT CHECKED'):
+            stream.start()
 
         # Loop over blocks
         for _ in range(blocks):
 
             # Loop over points
             for _ in range(block_size):
-                pepu.conn._readline.return_value = ''
-                pepu.software_trigger()
+                with pepu.assert_command('#STRIG', 'ACK IS NOT CHECKED'):
+                    pepu.software_trigger()
 
             # Read nb points
-            pepu.conn._readline.return_value = str(block_size)
-            assert stream.nb_points_ready == block_size
+            with pepu.assert_command('?DSTREAM TEST NSAMPL', str(block_size)):
+                assert stream.nb_points_ready == block_size
 
             # Read data
-            mock_block(pepu.conn, block_size)
-            data = stream.read(n=block_size)
+            with pepu.assert_block(block_size):
+                data = stream.read(n=block_size)
 
             # Test
             expected = [[1.5, -1.5]] * block_size

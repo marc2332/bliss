f5c0d351c26ee6182f7547450c6c45ab16559276
Vincent Michel <vincent.michel@esrf.fr>
2018-02-07 18:07:23 +0100

    Update PEPU acquisition device

--- bliss/scanning/acquisition/pepu.py
+++ bliss/scanning/acquisition/pepu.py
@@ -14,23 +14,53 @@
 from collections import namedtuple
 
 from ..chain import AcquisitionDevice, AcquisitionChannel
-from ...controllers.pepu import Trigger, Signal, StreamInfo
+from ...controllers.pepu import Trigger, Signal
 
 
-class PEPUAcquisitionDevice(AcquisitionDevice):
+class PepuAcquisitionDevice(AcquisitionDevice):
 
-    def __init__(self, pepu, npoints, frequency=1000,
-                 start=Signal.SOFT, trigger=Signal.FREQ,
+    SOFT = Signal.SOFT
+    FREQ = Signal.FREQ
+    DI1 = Signal.DI1
+    DI2 = Signal.DI2
+
+    def __init__(self, pepu, npoints,
+                 start=Signal.SOFT, trigger=Signal.SOFT,
+                 frequency=None,
                  prepare_once=True, start_once=True,
                  counters=()):
+
+        # Checking
+
         assert start_once
         assert prepare_once
-        if trigger == Signal.SOFT:
-            trigger_type = self.SOFTWARE
-        else:
-            trigger_type = self.HARDWARE
 
-        super(PEPUAcquisitionDevice, self).__init__(
+        if trigger not in (Signal.SOFT, Signal.FREQ, Signal.DI1, Signal.DI2):
+            raise ValueError(
+                '{!r} is not a valid trigger'.format(trigger))
+
+        if start not in (Signal.SOFT, Signal.DI1, Signal.DI2):
+            raise ValueError(
+                '{!r} is not a valid start trigger'.format(trigger))
+
+        if trigger in (Signal.FREQ,) and frequency is None:
+            raise ValueError(
+                'Frequency has to be provided for FREQ trigger')
+
+        if trigger in (Signal.FREQ,) and frequency < 1000:
+            raise ValueError(
+                'Frequency should be greater than or equal to 1000 Hz')
+
+        if trigger not in (Signal.FREQ,) and frequency is not None:
+            raise ValueError(
+                'Frequency does not make sense without a FREQ trigger')
+
+        trigger_type = \
+            self.SOFTWARE if trigger == Signal.SOFT else self.HARDWARE
+
+        # Initialize
+
+        super(PepuAcquisitionDevice, self).__init__(
             pepu, pepu.name,
             npoints=npoints,
             trigger_type=trigger_type,
@@ -38,37 +68,59 @@ class PEPUAcquisitionDevice(AcquisitionDevice):
             start_once=start_once)
 
         self.pepu = pepu
+        self.stream = None
         self.frequency = frequency
-        self.trig = Trigger(start, trigger)
         self.counters = list(counters)
+        self.trig = Trigger(start, trigger)
+
+    # Counter management
+
+    def add_counter(self, counter):
+        self.counters.append(counter)
+        counter.register_device(self)
+
+    def add_counters(self, counters):
+        for counter in counters:
+            self.add_counter(counter)
+
+    def publish(self, data):
+        for counter in self.counters:
+            counter.feed_point(data)
+
+    # Standard methods
 
     def prepare(self):
+        """Prepare the acquisition."""
         sources = [counter.name for counter in self.counters]
-        self.stream = self.pepu.create_stream(self.name,
-                                              trigger=self.trig,
-                                              frequency=self.frequency,
-                                              nb_points=self.npoints,
-                                              sources=sources,
-                                              overwrite=True)
+        self.stream = self.pepu.create_stream(
+            self.name, trigger=self.trig, frequency=self.frequency,
+            nb_points=self.npoints, sources=sources, overwrite=True)
 
     def start(self):
+        """Start the acquisition."""
         self.stream.start()
-        if self.trig.start == Signal.SOFT:
-            self.pepu.software_trigger()
 
     def stop(self):
+        """Stop the acquisition."""
         self.stream.stop()
 
     def trigger(self):
         """Send a software trigger."""
-        print 'trigger'
+        if self.trig.start == Signal.SOFT:
+            self.pepu.software_trigger()
+
+    def wait_ready(self):
+        """Block until finished."""
+        return
 
     def reading(self):
         """Spawn by the chain."""
-        return self.stream.read()
+        for data in self.stream.idata():
+            if data:
+                self.publish(data)
 
 
-class PEPUCounter(object):
+class PepuCounter(object):
 
     def __init__(self, channel):
         self.channel = channel
@@ -84,7 +136,7 @@ class PEPUCounter(object):
 
     @property
     def shape(self):
-        return (self.acquisition_controller.nsamples,)
+        return ()
 
     def register_device(self, device):
         assert device.pepu == self.channel.pepu
@@ -102,6 +154,6 @@ class PEPUCounter(object):
 def pepu_counters(pepu):
     """Provide a convenient access to the PEPU counters."""
     channels = pepu.in_channels.values() + pepu.calc_channels.values()
-    counters = map(PEPUCounter, channels)
+    counters = map(PepuCounter, channels)
     names = (channel.name for channel in channels)
-    return namedtuple('PEPUCounters', names)(*counters)
+    return namedtuple('PepuCounters', names)(*counters)

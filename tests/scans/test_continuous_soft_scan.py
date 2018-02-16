c8e06c2dbc571c76cf4bee5e3fae7dc29df18eca
Vincent Michel <vincent.michel@esrf.fr>
2018-02-16 12:46:19 +0100

    Update test for continuous soft scan

--- tests/scans/test_continuous_soft_scan.py
+++ tests/scans/test_continuous_soft_scan.py
@@ -5,9 +5,12 @@
 # Copyright (c) 2016 Beamline Control Unit, ESRF
 # Distributed under the GNU LGPLv3. See LICENSE for more info.
 
+import time
+
 import pytest
 import gevent
 
+from bliss.common import event
 from bliss.common import scans
 from bliss.scanning.scan import Scan
 from bliss.scanning.chain import AcquisitionChain
@@ -18,26 +21,34 @@ from bliss.scanning.acquisition.motor import SoftwarePositionTriggerMaster
 class DebugMotorMockupAcquisitionDevice(AcquisitionDevice):
     def __init__(self, name, motor_mockup):
         super(DebugMotorMockupAcquisitionDevice, self).__init__(
-            motor_mockup, name)
+          motor_mockup, name)
         self.motor_mockup = motor_mockup
-        self.channels.append(
-            AcquisitionChannel(self.name, float, ()))
+        self.channels.append(AcquisitionChannel(name + '_pos', float, ()))
+        self.channels.append(AcquisitionChannel(name + '_time', float, ()))
+
+    def set_time_ref(self, state):
+        if state == 'MOVING':
+            self.time_ref = time.time()
+
+    def prepare(self):
+        pass
+
+    def start(self):
+        event.connect(
+            self.motor_mockup, 'internal_state', self.set_time_ref)
+
+    def stop(self):
+        event.disconnect(
+            self.motor_mockup, 'internal_state', self.set_time_ref)
 
     def trigger(self):
         controller = self.motor_mockup.controller
         motion = controller._axis_moves[self.motor_mockup]['motion']
         steps = motion.trajectory.position()
         value = steps / float(self.motor_mockup.steps_per_unit)
-        self.channels.update({self.name: value})
-
-    def prepare(self):
-        pass
-
-    def start(self):
-        pass
-
-    def stop(self):
-        pass
+        self.channels.update({
+            self.name + '_pos': value,
+            self.name + '_time': time.time() - self.time_ref})
 
 
 def test_software_position_trigger_master(beacon):
@@ -54,4 +65,6 @@ def test_software_position_trigger_master(beacon):
     data = scans.get_data(s)
     # Typical position error is +0.025 in position unit
     # That's because of redis + gevent delays (~2.5 ms)
-    assert data['roby'] == pytest.approx(data['debug'], abs=0.05)
+    expected_triggers = [0.01, 0.03, 0.05, 0.07, 0.09]
+    assert data['roby'] == pytest.approx(data['debug_pos'], abs=0.05)
+    assert data['debug_time'] == pytest.approx(expected_triggers, abs=0.005)

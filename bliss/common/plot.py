16f4792fbc3985a7e4f8ede02789e8aedcfb6075
Vincent Michel <vincent.michel@esrf.fr>
2018-02-26 16:55:07 +0100

    Refactor flint (again)

--- bliss/common/plot.py
+++ bliss/common/plot.py
@@ -6,7 +6,6 @@ import os
 import sys
 import uuid
 import numpy
-import itertools
 import subprocess
 
 import zerorpc
@@ -20,23 +19,57 @@ from bliss.config.conductor.client import get_default_connection
 
 msgpack_numpy.patch()
 FLINT_PROCESS = None
-FLINT_OBJECT = None
 
+
+# Connection helpers
+
+def get_beacon_config():
+    beacon = get_default_connection()
+    return '{}:{}'.format(beacon._host, beacon._port)
+
+
+def get_flint_process():
+    global FLINT_PROCESS
+    if FLINT_PROCESS is not None and FLINT_PROCESS.poll() is None:
+        return FLINT_PROCESS.pid
+    env = dict(os.environ)
+    env['BEACON_HOST'] = get_beacon_config()
+    args = [sys.executable, '-m', 'bliss.flint']
+    FLINT_PROCESS = subprocess.Popen(args, env=env, close_fds=True)
+    return FLINT_PROCESS.pid
+
+
+def get_flint():
+    # Make sure flint is running
+    pid = get_flint_process()
+    # Get redis connection
+    beacon = get_default_connection()
+    redis = beacon.get_redis_connection()
+    # Current URL
+    key = "flint:%s" % pid
+    url = redis.brpoplpush(key, key, timeout=3000)
+    # Return flint proxy
+    proxy = zerorpc.Client(url)
+    proxy._pid = pid
+    return proxy
+
+
+# Plotting
 
 class Plot(object):
 
-    # Count the plots
-    _id_generator = itertools.count(1)
-
-    def __init__(self, name=None):
-        index = next(self._id_generator)
-        self._name = name or "Plot {}".format(index)
-        self._flint = get_flint()
-        self._flint.add_window(self._name)
-        self.setWindowTitle(self._name)
-
-    def _submit(self, method, *args, **kwargs):
-        return self._flint.run_method(self._name, method, *args, **kwargs)
+    def __init__(self, name=None, existing_id=None, flint=None):
+        if flint is None:
+            self._flint = get_flint()
+        # Create plot window
+        if existing_id is None:
+            self._plot_id = self._flint.add_window()
+        else:
+            self._plot_id = existing_id
+        # Set plot title
+        self._name = name or "Plot {}".format(self._plot_id)
+        if existing_id is None or name is not None:
+            self.setWindowTitle(self._name)
 
     def __getattr__(self, key):
 
@@ -46,49 +79,42 @@ class Plot(object):
         wrapper.__name__ = key
         return wrapper
 
+    def __repr__(self):
+        return '{}(plot_id={}, flint_pid={})'.format(
+            self.__class__.__name__, self.plot_id, self.flint_pid)
 
-def get_beacon_config():
-    beacon = get_default_connection()
-    return '{}:{}'.format(beacon._host, beacon._port)
+    def _submit(self, method, *args, **kwargs):
+        return self._flint.run_method(self.plot_id, method, args, kwargs)
+
+    # Properties
+
+    @property
+    def flint_pid(self):
+        return self._flint._pid
+
+    @property
+    def plot_id(self):
+        return self._plot_id
+
+    # Plot helpers
+
+    def plot(self, data):
+        if data.ndim == 1:
+            xs = numpy.arange(len(data))
+            if data.dtype.fields is None:
+                self._submit('addCurve', xs, data)
+            else:
+                for field in data.dtype.fields:
+                    self._submit('addCurve', xs, data[field], legend=field)
+            return
+        raise NotImplementedError
+
+    def close(self):
+        self._flint.remove_window(self.plot_id)
 
 
-def get_flint():
-    beacon = get_default_connection()
-    redis = beacon.get_redis_connection()
-    session_id = uuid.uuid1()
-    key = "flint:%s" % session_id
-
-    global FLINT_PROCESS
-    if FLINT_PROCESS is None:
-        env = dict(os.environ)
-        env['BEACON_HOST'] = get_beacon_config()
-        FLINT_PROCESS = subprocess.Popen(
-            [sys.executable, '-m',
-             'bliss.flint',
-             '-s', str(session_id)],
-            env=env)
-
-    global FLINT_OBJECT
-    if FLINT_OBJECT is None:
-        url = redis.brpoplpush(key, key)
-        FLINT_OBJECT = zerorpc.Client(url)
-
-    return FLINT_OBJECT
-
-
-def plot(data, name=None):
+def plot(data=None, name=None):
     plot = Plot(name=name)
-    add_data_to_plot(plot, data)
+    if data is not None:
+        plot.plot(data)
     return plot
-
-
-def add_data_to_plot(plot, data):
-    if data.ndim == 1:
-        xs = numpy.arange(len(data))
-        if data.dtype.fields is None:
-            plot.addCurve(xs, data)
-        else:
-            for field in data.dtype.fields:
-                plot.addCurve(xs, data[field], legend=field)
-        return
-    raise NotImplementedError
--- bliss/flint/flint.py
+++ bliss/flint/flint.py
@@ -4,20 +4,13 @@
 #
 # Copyright (c) 2016 Beamline Control Unit, ESRF
 # Distributed under the GNU LGPLv3. See LICENSE for more info.
-"""
-Usage: flint (-s | --session) <sid>
-       flint (-h | --help)
-Options:
-    -s, --session                 Follow data from session.
-    -h, --help                    Show this screen.
-"""
 
 # Imports
-
 import os
-import docopt
+import sys
 import tempfile
 import warnings
+import itertools
 import contextlib
 
 import gevent
@@ -47,21 +40,16 @@ pyqtRemoveInputHook()
 Thread = gevent.monkey.get_original('threading', 'Thread')
 Event = gevent.monkey.get_original('threading', 'Event')
 
-PLOTS = dict()
-GEVENT_PLOTS = dict()
-FLINT_CHANNEL = None
-
 
 # Gevent functions
 
 @contextlib.contextmanager
 def safe_rpc_server(obj):
-    with tempfile.NamedTemporaryFile() as f:
+    with tempfile.NamedTemporaryFile(delete=False) as f:
         url = 'ipc://{}'.format(f.name)
         server = zerorpc.Server(obj)
         try:
             server.bind(url)
-            os.chmod(f.name, 0o700)
             task = gevent.spawn(server.run)
             yield task, url
         finally:
@@ -78,11 +66,11 @@ def maintain_value(key, value):
     try:
         yield
     finally:
-        redis.rpop(key)
+        redis.lrem(key, value)
 
 
-def background_task(flint, session_id, stop):
-    key = "flint:%s" % session_id
+def background_task(flint, stop):
+    key = "flint:%s" % os.getpid()
     stop = concurrent_to_gevent(stop)
     with safe_rpc_server(flint) as (task, url):
         with maintain_value(key, url):
@@ -93,38 +81,37 @@ def background_task(flint, session_id, stop):
 
 class Flint:
 
+    _id_generator = itertools.count()
     _submit = staticmethod(submit_to_qt_application)
 
     def __init__(self, main_window, mdi_area):
         self.mdi_area = mdi_area
         self.main_window = main_window
-        self.window_dict = {}
-        self.window_dict[0] = main_window
+        self.main_index = next(self._id_generator)
+        self.window_dict = {self.main_index: self.main_window}
 
-    def run_method(self, key, method, *args, **kwargs):
+    def run_method(self, key, method, args, kwargs):
         window = self.window_dict[key]
         method = getattr(window, method)
         return self._submit(method, *args, **kwargs)
 
-    def add_window(self, key):
+    def add_window(self):
+        wid = next(self._id_generator)
         window = self._submit(PlotWindow, self.mdi_area)
         self._submit(self.mdi_area.addSubWindow, window)
         self._submit(window.show)
-        self.window_dict[key] = window
+        self.window_dict[wid] = window
+        return wid
+
+    def remove_window(self, wid):
+        window = self.window_dict.pop(wid)
+        self._submit(self.mdi_area.removeSubWindow, window)
 
 
 # Main execution
 
 def main():
-    try:
-        # Parse arguments, use file docstring as a parameter definition
-        arguments = docopt.docopt(__doc__)
-        session_id = arguments['<sid>']
-    except docopt.DocoptExit as e:
-        print(e.message)
-        return
-
-    qapp = qt.QApplication([])
+    qapp = qt.QApplication(sys.argv)
     win = qt.QMainWindow()
     mdi_area = qt.QMdiArea(win)
     win.setCentralWidget(mdi_area)
@@ -134,7 +121,7 @@ def main():
     flint = Flint(win, mdi_area)
     thread = Thread(
         target=background_task,
-        args=(flint, session_id, stop))
+        args=(flint, stop))
     thread.start()
     try:
         qapp.exec_()

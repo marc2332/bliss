6fe8ded9e073c818f70479d1c4a9821d16198816
Vincent Michel <vincent.michel@esrf.fr>
2018-03-05 15:08:46 +0100

    Add flint test session

---
+++ tests/test_configuration/sessions/test_flint.py
@@ -0,0 +1,25 @@
+# Imports
+import numpy
+import scipy.misc
+from bliss.common.plot import *
+
+# Test data
+
+colored_image = scipy.misc.face()
+grey_image = colored_image[:, :, 0]
+
+x_data = numpy.linspace(0, 10, 1000)
+cos_data = numpy.cos(x_data)
+sin_data = numpy.sin(x_data)
+
+
+sin_cos_dict = {
+    'x': x_data,
+    'cos': cos_data,
+    'sin': sin_data}
+
+sin_cos_struct = numpy.array(
+    (x_data, cos_data, sin_data),
+    dtype=[('x', float, x_data.shape),
+           ('cos', float, cos_data.shape),
+           ('sin', float, sin_data.shape)])

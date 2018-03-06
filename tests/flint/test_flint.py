aac59d07f644472e58081e1a53e6a2ba3191c7bc
Vincent Michel <vincent.michel@esrf.fr>
2018-03-06 18:03:32 +0100

    Update flint tests

--- tests/flint/test_flint.py
+++ tests/flint/test_flint.py
@@ -1,6 +1,7 @@
 """Testing Flint."""
 
 import os
+import numpy
 import subprocess
 from distutils.spawn import find_executable
 
@@ -41,10 +42,35 @@ def flint(xvfb, beacon):
         yield flint_pid
     finally:
         plot.FLINT_PROCESS.kill()
-        plot.FLINT_PROCESS.wait(1.)
+        plot.FLINT_PROCESS.wait(timeout=1.)
+
+
+@pytest.fixture
+def flint_session(beacon, flint):
+    env_dict = dict()
+    session = beacon.get("flint")
+    session.setup(env_dict)
+    try:
+        yield env_dict
+    finally:
+        pass
 
 
 def test_empty_plot(flint):
+    p = plot.plot()
+    assert 'flint_pid={}'.format(flint) in repr(p)
+    assert p.qt.windowTitle() == 'Plot {}'.format(p._plot_id)
+
     p = plot.plot(name='Some name')
     assert 'flint_pid={}'.format(flint) in repr(p)
     assert p.qt.windowTitle() == 'Some name'
+
+
+def test_simple_plot(flint_session):
+    sin = flint_session['sin_data']
+    p = plot.plot(sin)
+    assert 'CurvePlot' in repr(p)
+    data = p.get_data()
+    assert data == {
+        'default': sin,
+        'x': range(len(sin))}

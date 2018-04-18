8010ee9b7b70cf581d517c620233a09939d54710
Matias Guijarro <guijarro@esrf.fr>
2018-04-18 14:53:57 +0200

    Added link to API doc

---
+++ doc/docs/redirect.js
@@ -0,0 +1,12 @@
+var anchors = Array.from(document.getElementsByClassName("md-nav__link"));
+anchors.forEach(function(anchor) {
+	var sep = anchor.innerText.indexOf("|");
+	if (sep != -1) {
+		anchor.href = anchor.innerText.substring(sep + 1, anchor.innerText.length);
+		anchor.innerText = anchor.innerText.substring(0,sep);
+	}
+	if (anchor.innerText.indexOf("\uD83D\uDDD7") != -1) { // &#x1F5D7; = "new window" char, see http://www.fileformat.info/info/unicode/char/1f5d7/index.htm
+		anchor.target = "_blank";
+	}
+});
+

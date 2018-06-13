c777c9ee0f153978dd3e5973fa942fa78d15247a
Matias Guijarro <guijarro@esrf.fr>
2018-06-13 22:02:33 +0200

    Null writer: fix issue #372
    Having no writer at all (writer set to None for Scan object) make BLISS
    skipping some calls, in particular writer preparation ; in this case,
    ".prepare_saving()" on acq masters is *not* called.
    
    In the case of Lima master, it does not set mode to MANUAL, which make
    Lima checking directory write access.
    
    This commit creates a Null writer, that does nothing, to be used in
    Scans instead of passing None as a writer to fix this issue.

---
+++ bliss/scanning/writer/null.py
@@ -0,0 +1,44 @@
+from ..scan import FileWriter, \
+    AcquisitionMasterEventReceiver, AcquisitionDeviceEventReceiver
+
+
+def _on_event(obj, event_dict, signal, sender):
+    return
+
+class NullMasterEventReceiver(AcquisitionMasterEventReceiver):
+    def __init__(self, *args, **kwargs):
+        AcquisitionMasterEventReceiver.__init__(self, *args, **kwargs)
+
+    def on_event(self, event_dict=None, signal=None, sender=None):
+        return _on_event(self, event_dict, signal, sender)
+
+
+class NullDeviceEventReceiver(AcquisitionDeviceEventReceiver):
+    def __init__(self, *args, **kwargs):
+        AcquisitionDeviceEventReceiver.__init__(self, *args, **kwargs)
+
+    def on_event(self, event_dict=None, signal=None, sender=None):
+        return _on_event(self, event_dict, signal, sender)
+
+
+class Writer(FileWriter):
+    def __init__(self, *args, **keys):
+        FileWriter.__init__(self, '',
+                            master_event_receiver=NullMasterEventReceiver,
+                            device_event_receiver=NullDeviceEventReceiver,
+                            **keys)
+
+    def create_path(self, scan_recorder):
+        return scan_recorder.path
+
+    def new_file(self, scan_file_dir, scan_recorder):
+        return
+
+    def new_master(self, master, scan):
+        return
+
+    def close(self):
+        return
+
+    def get_scan_entries(self):
+        return []

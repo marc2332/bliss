marc2332/bliss

commit 62446d593d7db482dbfd40e7c9a9e56bbc19c500

conductor/connection.py: treat host passed to Connection constructor the same as BEACON_HOST
Compatibility with bliss_webserver

bliss/config/conductor/connection.py
@@ -110,13 +110,12 @@ class Connection(object):
         self._socket = None
         if host is None:
             beacon_host = os.environ.get("BEACON_HOST")
-            if beacon_host is not None and ':' in beacon_host:
-                host, port = beacon_host.split(":")
-            else:
-                host = beacon_host
+	else:
+            beacon_host = host
+        if beacon_host is not None and ':' in beacon_host:
+            host, port = beacon_host.split(":")
         if port is None:
             port = os.environ.get("BEACON_PORT")
-
         if port is not None:
             try:
                 port = int(port)

commit 8306cf6290f500d24211e1a400754d3783b71ec7

config/conductor/connection.py: fix bug introduced by commit 62446d59

bliss/config/conductor/connection.py
@@ -109,11 +109,9 @@ class Connection(object):
     def __init__(self,host=None,port=None):
         self._socket = None
         if host is None:
-            beacon_host = os.environ.get("BEACON_HOST")
-	else:
-            beacon_host = host
-        if beacon_host is not None and ':' in beacon_host:
-            host, port = beacon_host.split(":")
+            host = os.environ.get("BEACON_HOST")
+        if host is not None and ':' in host:
+            host, port = host.split(":")
         if port is None:
             port = os.environ.get("BEACON_PORT")
         if port is not None:
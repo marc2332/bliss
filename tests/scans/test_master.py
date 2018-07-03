84075f433d8312b66cbe8367decd63973ca4bbe1
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2018-07-03 14:33:14 +0200

    chain: only top master should stop the chain.

--- tests/scans/test_master.py
+++ tests/scans/test_master.py
@@ -1,7 +1,8 @@
 from bliss.scanning.scan import Scan
 from bliss.scanning.chain import AcquisitionMaster, AcquisitionChain
 from bliss.scanning.chain import AcquisitionDevice, AcquisitionChannel
-
+from bliss.scanning.acquisition import timer
+from bliss.scanning.acquisition.lima import LimaAcquisitionMaster
 
 class DummyMaster(AcquisitionMaster):
 
@@ -36,6 +37,26 @@ class DummyDevice(AcquisitionDevice):
     def stop(self):
         pass
 
+class DummySlave(AcquisitionDevice):
+
+    def __init__(self, *args, **kwargs):
+        super(DummySlave, self).__init__(*args, **kwargs)
+        self.channels.append(AcquisitionChannel('nb', float, ()))
+        self.nb_trigger = 0
+        
+    def prepare(self):
+        pass
+
+    def start(self):
+        pass
+
+    def trigger(self):
+        self.channels.update({'nb': self.nb_trigger})
+        self.nb_trigger += 1
+
+    def stop(self):
+        pass
+
 
 def test_dummy_scan_without_external_channel(beacon):
     # Get controllers
@@ -64,3 +85,19 @@ def test_dummy_scan_with_external_channel(beacon):
     # Checks
     assert scan.get_data()['pi'] == [3.14]
     assert scan.get_data()['to'] == [6.28]
+
+def test_stopiter_with_top_master(beacon, lima_simulator):
+    chain = AcquisitionChain()
+    master = timer.SoftwareTimerMaster(0.1,npoints=2)
+    lima_sim = beacon.get("lima_simulator")
+    lima_master = LimaAcquisitionMaster(lima_sim,acq_nb_frames=1,
+                                        saving_directory="/tmp",
+                                        acq_expo_time=0.1,save_flag=False)
+    chain.add(master,lima_master)
+
+    device = DummySlave(None, 'device', npoints=1)
+    chain.add(lima_master,device)
+    
+    scan = Scan(chain, 'test', None)
+    scan.run()
+    assert device.nb_trigger == 2

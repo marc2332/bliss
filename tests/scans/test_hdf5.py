5a764164ea60f9af82eef9487ef519d3ecc5d556
Vincent Michel <vincent.michel@esrf.fr>
2018-07-20 10:07:47 +0200

    Remove capsys fixture form test_hdf5_file_items

--- tests/scans/test_hdf5.py
+++ tests/scans/test_hdf5.py
@@ -17,15 +17,22 @@ import datetime
 import os
 import h5py
 
-def print_items(name, obj):
-    print name
-    for key, val in obj.attrs.iteritems():
-        print "    %s: %s" % (key, val)
 
-def h5dump(scan_file):
+def _h5dump(scan_file):
+    # Get items
     items = []
     with h5py.File(scan_file, "r") as f:
-        f.visititems(print_items)
+        f.visititems(lambda *args: items.append(args))
+        # Yield lines
+        for name, obj in items:
+            yield name
+            for key, val in obj.attrs.iteritems():
+                yield "    %s: %s" % (key, val)
+
+
+def h5dump(scan_file):
+    return '\n'.join(_h5dump(scan_file))
+
 
 ascan_dump="""{ascan}
     NX_class: NXentry
@@ -44,12 +51,13 @@ ascan_dump="""{ascan}
 {ascan}/title
 """
 
+
 def test_hdf5_metadata(beacon):
     session = beacon.get("test_session")
     session.setup()
- 
+
     all_motors = dict([(name, pos) for name, pos, _ in get_axes_positions_iter(on_error='ERR') if pos!='ERR'])
-    
+
     roby = beacon.get("roby")
     diode = beacon.get("diode")
 
@@ -69,48 +77,47 @@ def test_hdf5_metadata(beacon):
             assert all_motors.pop(name) == pos.value
         assert len(all_motors) == 0
 
-def test_hdf5_file_items(beacon, capsys):
-    with capsys.disabled():
-        session = beacon.get('test_session')
-        session.setup()
 
-        roby = beacon.get("roby")
-        diode = beacon.get("diode")
-        simu1 = beacon.get("simu1")
+def test_hdf5_file_items(beacon):
+    session = beacon.get('test_session')
+    session.setup()
 
-        s = scans.ascan(roby, 0, 5, 5, 0.001, diode, simu1.counters.spectrum_det0, save=True, return_scan=True)
+    roby = beacon.get("roby")
+    diode = beacon.get("diode")
+    simu1 = beacon.get("simu1")
 
-        scan_file = os.path.join(s.path, "data.h5")
-    
-    h5dump(scan_file)
-    scan_dump, _ = capsys.readouterr()
+    s = scans.ascan(roby, 0, 5, 5, 0.001, diode, simu1.counters.spectrum_det0, save=True, return_scan=True)
 
-    with capsys.disabled():
-        ref_ascan_dump = ascan_dump.split('\n')
+    scan_file = os.path.join(s.path, "data.h5")
 
-        i = 0
-        in_positioner = False
-        in_scan = False
-        group_name = None
-        for l in scan_dump.split('\n'):
-            if l.startswith(' '):
-                if in_positioner:
-                    continue
-            else:
-                in_scan = l == s.name or l.startswith(s.name+'/')
-            if in_scan:
-                if l.startswith(s.name+'/measurement/group_'):
-                    group_name = l.replace(s.name+'/measurement/','').split('/')[0].replace('_master', '')
-            else:
-                continue 
-            if 'positioner' in l:
-                in_positioner = True
+    scan_dump = h5dump(scan_file)
+
+    ref_ascan_dump = ascan_dump.split('\n')
+
+    i = 0
+    in_positioner = False
+    in_scan = False
+    group_name = None
+    for l in scan_dump.split('\n'):
+        if l.startswith(' '):
+            if in_positioner:
                 continue
-            else:
-                in_positioner = False
-            assert l == ref_ascan_dump[i].format(ascan=s.name, group_name=group_name)
-            i += 1
+        else:
+            in_scan = l == s.name or l.startswith(s.name+'/')
+        if in_scan:
+            if l.startswith(s.name+'/measurement/group_'):
+                group_name = l.replace(s.name+'/measurement/','').split('/')[0].replace('_master', '')
+        else:
+            continue
+        if 'positioner' in l:
+            in_positioner = True
+            continue
+        else:
+            in_positioner = False
+        assert l == ref_ascan_dump[i].format(ascan=s.name, group_name=group_name)
+        i += 1
 
-        f = h5py.File(scan_file)
-        assert f[f[s.name]['measurement'][group_name+"_master"]['timer_master'].value] == f[s.name]['measurement']['timer_master']
-
+    f = h5py.File(scan_file)
+    assert (
+        f[f[s.name]['measurement'][group_name+"_master"]['timer_master'].value]
+        == f[s.name]['measurement']['timer_master'])

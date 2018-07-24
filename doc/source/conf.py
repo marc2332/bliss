07b9d3a46511f53fe208d20b6fc8984a08d304e3
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2018-07-24 08:57:03 +0200

    doc: remove wavedrom dependency

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,7 +38,6 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
     'sphinx.ext.graphviz',
-    'sphinxcontrib.wavedrom',  # wavedrom diagrams
 ]
 
 # Add any paths that contain templates here, relative to this directory.

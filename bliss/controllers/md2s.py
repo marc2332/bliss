31500f5364e73ef999e74c9b004362f961eefe8f
Antonia Beteva <beteva@esrf.fr>
2019-01-11 14:10:32 +0100

    Remove unnecessary methods. Make python3 compatible.

--- bliss/controllers/md2s.py
+++ bliss/controllers/md2s.py
@@ -14,15 +14,20 @@ yml configuration example:
    exporter_addr: 'microdiff29new:9001'
 """
 
+from __future__ import absolute_import
 import time
-from bliss.common.task import task
-from bliss.common.utils import grouped
-from bliss.comm.Exporter import *
 import functools
+from bliss.common.utils import grouped
+from bliss.comm.Exporter import Exporter
 
 
-class MD2S(object):
+class MD2S:
+    """ Due to the underlying control, only one action can be executed at a
+    time. Therefore each action should be waiting the end of the execution.
+    """
+
     def __init__(self, name, config):
+        print(name)
         self.phases = {
             "Centring": 1,
             "BeamLocation": 2,
@@ -30,19 +35,19 @@ class MD2S(object):
             "Transfer": 4,
         }
         self.timeout = 3  # s by default
-        nn, port = config.get("exporter_addr").split(":")
-        self._exporter = Exporter(nn, int(port))
+        nam, port = config.get("exporter_addr").split(":")
+        self._exporter = Exporter(nam, int(port))
 
         fshutter = config.get("fshutter")
         if fshutter:
             fshutter.set_external_control(
                 functools.partial(
-                    self._exporter.writeProperty, "FastShutterIsOpen", "true"
+                    self._exporter.write_property, "FastShutterIsOpen", "true"
                 ),
                 functools.partial(
-                    self._exporter.writeProperty, "FastShutterIsOpen", "false"
+                    self._exporter.write_property, "FastShutterIsOpen", "false"
                 ),
-                lambda: self._exporter.readProperty("FastShutterIsOpen") == "true",
+                lambda: self._exporter.read_roperty("FastShutterIsOpen") == "true",
             )
 
     def get_hwstate(self):
@@ -51,8 +56,8 @@ class MD2S(object):
              state(str): the hardware state or Ready (if not implemented)
         """
         try:
-            return self._exporter.readProperty("HardwareState")
-        except Exception:
+            return self._exporter.read_property("HardwareState")
+        except BaseException:
             return "Ready"
 
     def get_swstate(self):
@@ -60,7 +65,7 @@ class MD2S(object):
            Returns:
              state(str): the software state
         """
-        return self._exporter.readProperty("State")
+        return self._exporter.read_property("State")
 
     def _ready(self):
         if self.get_hwstate() == "Ready" and self.get_swstate() == "Ready":
@@ -83,7 +88,7 @@ class MD2S(object):
              phase(str): the current phase (Centring, BeamLocation,
                             DataCollection or Transfer)
         """
-        return self._exporter.readProperty("CurrentPhase")
+        return self._exporter.read_property("CurrentPhase")
 
     def set_phase(self, phase, wait=False, timeout=40):
         """Set new phase. Wait until finished.
@@ -105,14 +110,14 @@ class MD2S(object):
              values(list): pixel per mm on Y and Z axis
         """
         # the value depends on the zoom
-        px_mm_y = 1. / self._exporter.readProperty("CoaxCamScaleX")
-        px_mm_z = 1. / self._exporter.readProperty("CoaxCamScaleY")
+        px_mm_y = 1. / self._exporter.read_property("CoaxCamScaleX")
+        px_mm_z = 1. / self._exporter.read_property("CoaxCamScaleY")
         return [px_mm_y, px_mm_z]
 
     def move_motors(self, *args):
         """Simultaneous move for all the listed motors.
            Args:
-             motor,position(list): motor_object0, position0, motor_object1, ...
+             motor, position(list): motor_object0, position0, motor_object1, ...
         """
         par = ""
         for axis, target in grouped(args, 2):
@@ -120,56 +125,40 @@ class MD2S(object):
         self._exporter.execute("startSimultaneousMoveMotors", par)
         self._wait_ready(20)
 
-    @task
-    def _simultaneous_move(self, *args):
-        axis_list = []
-        for axis, target in grouped(args, 2):
-            axis_list.append(axis)
-            axis.move(target, wait=False)
-        return [axis.wait_move() for axis in axis_list]
-
-    @task
-    def _simultaneous_rmove(self, *args):
-        axis_list = []
-        for axis, target in grouped(args, 2):
-            axis_list.append(axis)
-            axis.rmove(target, wait=False)
-        return [axis.wait_move() for axis in axis_list]
-
     def msopen(self):
         """Open fast shutter"""
-        self._exporter.writeProperty("FastShutterIsOpen", "true")
+        self._exporter.write_property("FastShutterIsOpen", "true")
 
     def msclose(self):
         """Close fast shutter"""
-        self._exporter.writeProperty("FastShutterIsOpen", "false")
+        self._exporter.write_property("FastShutterIsOpen", "false")
 
     def fldetin(self):
         """Move the fluorescense detector close to the beam position"""
-        self._exporter.writeProperty("FluoDetectorIsBack", "false")
+        self._exporter.write_property("FluoDetectorIsBack", "false")
 
     def fldetout(self):
         """Move the fluorescense detector far from the beam position"""
-        self._exporter.writeProperty("FluoDetectorIsBack", "true")
+        self._exporter.write_property("FluoDetectorIsBack", "true")
 
     def fldetstate(self):
         """Read the fluorescense detector position
            Returns:
              position(bool): close to the beam - True/False
         """
-        self._exporter.readProperty("FluoDetectorIsBack")
+        self._exporter.read_property("FluoDetectorIsBack")
 
     def flight(self, state=None):
         """Switch the front light on/off or read the state
            Args:
              state(bool): on (True), off (False) or None (read only)
            Returns:
-             state(bool): on (True)or off (False) - when no args
+             state(bool): on (True)or off (False)
         """
         if state:
-            self._exporter.writeProperty("FrontLightIsOn", state)
-        else:
-            return self._exporter.readProperty("FrontLightIsOn")
+            self._exporter.write_property("FrontLightIsOn", state)
+            return state
+        return self._exporter.read_property("FrontLightIsOn")
 
     def blight(self, state=None):
         """Switch the back light on/off or read the state
@@ -179,9 +168,9 @@ class MD2S(object):
              state(bool): on (True)or off (False) - when no args
         """
         if state:
-            self._exporter.writeProperty("BackLightIsOn", state)
-        else:
-            return self._exporter.readProperty("BackLightIsOn")
+            self._exporter.write_property("BackLightIsOn", state)
+            return state
+        return self._exporter.read_property("BackLightIsOn")
 
     def cryo(self, state=None):
         """Set the cryostat close to/far from the sample or read the position
@@ -191,9 +180,9 @@ class MD2S(object):
              state(bool): close to(True) or far from(False) - when no args
         """
         if state:
-            self._exporter.writeProperty("CryoIsBack", state)
-        else:
-            return self._exporter.readProperty("CryoIsBack")
+            self._exporter.write_property("CryoIsBack", state)
+            return state
+        return self._exporter.read_property("CryoIsBack")
 
     def microdiff_init(self, wait=True):
         """Do the homing of all the motors
@@ -205,6 +194,10 @@ class MD2S(object):
             self._wait_ready(60)
 
     def diffractometer_init(self, wait=True):
+        """Do the homing of all the motors
+           Args:
+             wait(bool): Wait until finished (True/False)
+        """
         self.microdiff_init(wait)
 
     def phi_init(self, wait=True):
@@ -238,34 +231,34 @@ class MD2S(object):
         if wait:
             self._wait_ready(10)
 
-    def prepare(self, what, **kwargs):
-        """Do actions to prepare the MD2S to use in different procedures like
-           centrebeam or alignment. Wait until actions done.
+    def prepare(self, do_what, **kwargs):
+        """Do actions to prepare the MD2S to be used in different procedures
+           like centrebeam or alignment. Wait until actions done.
            Args:
-             what(string): which is action to follow (see_beam, data_collect)
+             do_what(string): which is action to follow (see_beam, data_collect)
            Keyword Args:
              zoom_level(int): where to move the zoom to
            Returns:
              zoom_level(int): position before the execition of the action,
                               if relevant
         """
-        if what == "data_collect":
+        if do_what == "data_collect":
             self.set_phase("DataCollection", wait=True, timeout=100)
-            if "zoom_level" in kwargs:
-                self._exporter.writeProperty(
-                    "CoaxialCameraZoomValue", kwargs["zoom_level"]
-                )
+            ret_value = kwargs.get("zoom_level", 0)
+            if ret_value:
+                self._exporter.write_property("CoaxialCameraZoomValue", ret_value)
                 self._wait_ready(20)
 
-        if what == "see_beam":
+        if do_what == "see_beam":
             zoom_level = kwargs.get("zoom_level", 5)
             self.set_phase("BeamLocation", wait=True, timeout=100)
-            self._exporter.writeProperty("CapillaryPosition", "OFF")
+            self._exporter.write_property("CapillaryPosition", "OFF")
             self._wait_ready(20)
-            self._exporter.writeProperty("AperturePosition", "OFF")
+            self._exporter.write_property("AperturePosition", "OFF")
             self._wait_ready(20)
             # get the current zoom position and move zoom to zoom_level
-            curr_zoom = self._exporter.readProperty("CoaxialCameraZoomValue")
-            self._exporter.writeProperty("CoaxialCameraZoomValue", zoom_level)
+            ret_value = self._exporter.read_property("CoaxialCameraZoomValue")
+            self._exporter.write_property("CoaxialCameraZoomValue", zoom_level)
             self._wait_ready(20)
-            return curr_zoom
+
+        return ret_value

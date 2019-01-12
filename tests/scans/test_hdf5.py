4790844e911e6c9032b6c5c0a48877fe37155271
Matias Guijarro <guijarro@esrf.fr>
2019-01-12 18:10:20 +0100

    scan: added support for multiple top-masters
    For file writer, and HDF5 in particular, scans with multiple top-masters
    are handled as individual scans with a '.X' suffix appended to the
    scan number, where X represents the subscan number: 1,2,...
    
    By default any top master will stop the scan when its acquisition is
    finished. The 'terminator' property can be set to False to indicate
    the scan will not be finished when this top master ends.

--- tests/scans/test_hdf5.py
+++ tests/scans/test_hdf5.py
@@ -10,6 +10,10 @@ from bliss import setup_globals
 from bliss.common import scans
 from bliss.common.axis import Axis
 from bliss.common.utils import get_axes_positions_iter
+from bliss.scanning.scan import Scan, ScanSaving
+from bliss.scanning.chain import AcquisitionChain
+from bliss.scanning.acquisition import timer
+from bliss.scanning.acquisition.lima import LimaAcquisitionMaster
 import numpy
 import h5py
 import time
@@ -133,3 +137,33 @@ def test_hdf5_values(beacon, session):
     f = h5py.File(scan_file)
     dataset = f[s.node.name]["measurement"]["diode:diode"]
     assert list(dataset) == list(data)
+
+
+def test_subscan_in_hdf5(beacon, lima_simulator, dummy_acq_master, dummy_acq_device):
+    chain = AcquisitionChain()
+    master1 = timer.SoftwareTimerMaster(0.1, npoints=2, name="timer1")
+    dummy1 = dummy_acq_device.get(None, "dummy1", npoints=1)
+    master2 = timer.SoftwareTimerMaster(0.001, npoints=50, name="timer2")
+    lima_sim = beacon.get("lima_simulator")
+    lima_master = LimaAcquisitionMaster(lima_sim, acq_nb_frames=1, acq_expo_time=0.001)
+    dummy2 = dummy_acq_device.get(None, "dummy2", npoints=1)
+    chain.add(lima_master, dummy2)
+    chain.add(master2, lima_master)
+    chain.add(master1, dummy1)
+    master1.terminator = False
+
+    scan_saving = ScanSaving("test")
+    scan = Scan(chain, "test", scan_saving=scan_saving)
+    scan.run()
+
+    scan_file = scan.writer.filename
+    f = h5py.File(scan_file)
+
+    scan_number, scan_name = scan.node.name.split("_", maxsplit=1)
+    scan_index = 1
+    subscan_name = f"{scan_number}{'.%d_' % scan_index}{scan_name}"
+
+    assert f[scan.node.name]["measurement"]["timer1:elapsed_time"]
+    assert f[subscan_name]["measurement"]["timer2:elapsed_time"]
+    assert f[scan.node.name]["measurement"]["dummy1:nb"]
+    assert f[subscan_name]["measurement"]["dummy2:nb"]
--- tests/scans/test_master.py
+++ tests/scans/test_master.py
@@ -1,70 +1,23 @@
 import numpy
 import gevent
 from bliss.scanning.scan import Scan
-from bliss.scanning.chain import AcquisitionMaster, AcquisitionChain
-from bliss.scanning.chain import AcquisitionDevice, AcquisitionChannel
+from bliss.scanning.chain import AcquisitionChain, AcquisitionChannel
 from bliss.scanning.acquisition import timer
 from bliss.scanning.acquisition.lima import LimaAcquisitionMaster
 from bliss.scanning.acquisition.motor import LinearStepTriggerMaster, MotorMaster
+from bliss.scanning.acquisition.counter import SamplingCounterAcquisitionDevice
+import pytest
+import gevent
+import numpy
 
 
-class DummyMaster(AcquisitionMaster):
-    def prepare(self):
-        pass
-
-    def start(self):
-        pass
-
-    def trigger(self):
-        pass
-
-    def stop(self):
-        pass
-
-
-class DummyDevice(AcquisitionDevice):
-    def __init__(self, *args, **kwargs):
-        super(DummyDevice, self).__init__(*args, **kwargs)
-        self.channels.append(AcquisitionChannel("pi", float, ()))
-
-    def prepare(self):
-        pass
-
-    def start(self):
-        self.channels.update({"pi": 3.14})
-
-    def trigger(self):
-        pass
-
-    def stop(self):
-        pass
-
-
-class DummySlave(AcquisitionDevice):
-    def __init__(self, *args, **kwargs):
-        super(DummySlave, self).__init__(*args, **kwargs)
-        self.channels.append(AcquisitionChannel("nb", float, ()))
-        self.nb_trigger = 0
-
-    def prepare(self):
-        pass
-
-    def start(self):
-        pass
-
-    def trigger(self):
-        self.channels.update({"nb": self.nb_trigger})
-        self.nb_trigger += 1
-
-    def stop(self):
-        pass
-
-
-def test_dummy_scan_without_external_channel(beacon):
+def test_dummy_scan_without_external_channel(
+    beacon, dummy_acq_master, dummy_acq_device
+):
     # Get controllers
     chain = AcquisitionChain()
-    master = DummyMaster(None, "master", npoints=1)
-    device = DummyDevice(None, "device", npoints=1)
+    master = dummy_acq_master.get(None, "master", npoints=1)
+    device = dummy_acq_device.get(None, "device", npoints=1)
     chain.add(master, device)
     # Run scan
     scan = Scan(chain, "test", save=False)
@@ -73,11 +26,11 @@ def test_dummy_scan_without_external_channel(beacon):
     assert scan.get_data()["pi"] == [3.14]
 
 
-def test_dummy_scan_with_external_channel(beacon):
+def test_dummy_scan_with_external_channel(beacon, dummy_acq_master, dummy_acq_device):
     # Get controllers
     chain = AcquisitionChain()
-    master = DummyMaster(None, "master", npoints=1)
-    device = DummyDevice(None, "device", npoints=1)
+    master = dummy_acq_master.get(None, "master", npoints=1)
+    device = dummy_acq_device.get(None, "device", npoints=1)
     chain.add(master, device)
     # Add external channel
     master.add_external_channel(device, "pi", "to", lambda x: 2 * x)
@@ -91,14 +44,14 @@ def test_dummy_scan_with_external_channel(beacon):
     assert scan.get_data()["to_int"] == [6]
 
 
-def test_stopiter_with_top_master(beacon, lima_simulator):
+def test_stopiter_with_top_master(beacon, lima_simulator, dummy_acq_device):
     chain = AcquisitionChain()
     master = timer.SoftwareTimerMaster(0.1, npoints=2)
     lima_sim = beacon.get("lima_simulator")
     lima_master = LimaAcquisitionMaster(lima_sim, acq_nb_frames=1, acq_expo_time=0.1)
     chain.add(master, lima_master)
 
-    device = DummySlave(None, "device", npoints=1)
+    device = dummy_acq_device.get(None, "device", npoints=1)
     chain.add(lima_master, device)
 
     scan = Scan(chain, "test")
@@ -132,3 +85,60 @@ def test_attach_channel(beacon):
     data = scan.get_data()
     assert len(data["m1"]) == 6
     assert len(data["m0"]) == len(data["m1"])
+
+
+def test_add_multiple_top_masters_same_name(beacon, dummy_acq_master, dummy_acq_device):
+    chain = AcquisitionChain()
+    master1 = dummy_acq_master.get(None, "master")
+    master2 = dummy_acq_master.get(None, "master")
+    master3 = dummy_acq_master.get(None, "master")
+    master4 = dummy_acq_master.get(None, "master4")
+    dummy_device = dummy_acq_device.get(None, "dummy_device")
+
+    assert chain.add(master1, master3) is None
+    with pytest.raises(RuntimeError) as e_info:
+        assert chain.add(master2, dummy_device)
+    assert "duplicated" in str(e_info.value)
+    assert chain.add(master4, dummy_device) is None
+
+
+def test_multiple_top_masters(beacon, lima_simulator, dummy_acq_device):
+    chain = AcquisitionChain()
+    master1 = timer.SoftwareTimerMaster(0.1, npoints=2, name="timer1")
+    diode_sim = beacon.get("diode")
+    diode_device = SamplingCounterAcquisitionDevice(diode_sim, 0.1)
+    master2 = timer.SoftwareTimerMaster(0.001, npoints=50, name="timer2")
+    lima_sim = beacon.get("lima_simulator")
+    lima_master = LimaAcquisitionMaster(lima_sim, acq_nb_frames=1, acq_expo_time=0.001)
+    # note: dummy device has 2 channels: pi and nb
+    dummy_device = dummy_acq_device.get(None, "dummy_device", npoints=1)
+    chain.add(lima_master, dummy_device)
+    chain.add(master2, lima_master)
+    chain.add(master1, diode_device)
+    master1.terminator = False
+
+    scan = Scan(chain, "test", save=False)
+    scan.run()
+
+    assert dummy_device.nb_trigger == 50
+    scan_data = scan.get_data()
+    assert isinstance(scan_data["elapsed_time"], numpy.ndarray)
+    assert isinstance(scan_data["timer2:elapsed_time"], numpy.ndarray)
+    assert isinstance(scan_data["pi"], numpy.ndarray)
+    assert isinstance(scan_data["nb"], numpy.ndarray)
+    assert isinstance(scan_data["diode"], numpy.ndarray)
+    assert len(scan_data["elapsed_time"]) == 2
+    assert len(scan_data["timer2:elapsed_time"]) == 50
+
+
+def test_master_synchro(beacon, dummy_acq_master, dummy_acq_device):
+    chain = AcquisitionChain(parallel_prepare=True)
+    master = dummy_acq_master.get(None, "master", npoints=1)
+    device1 = dummy_acq_device.get(None, "device1", npoints=1, sleep_time=0.1)
+    device2 = dummy_acq_device.get(None, "device2", npoints=1, sleep_time=0.2)
+    chain.add(master, device1)
+    chain.add(master, device2)
+    scan = Scan(chain, "test", save=False)
+    scan.run()
+    assert master.child_started == 2
+    assert master.child_prepared == 2

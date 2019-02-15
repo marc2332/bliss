137c4bb5e8f7acebc447b966ec4fa4b68e9deaa5
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2019-02-15 16:57:26 +0100

    gevent: fixed the kill mask patch for gevent 1.3

--- bliss/common/greenlet_utils.py
+++ bliss/common/greenlet_utils.py
@@ -18,7 +18,7 @@ class KillMask:
         self.__kill_counter = masked_kill_nb
 
     def __enter__(self):
-        self.__func, self.__exception, self.__waiter = None, None, None
+        self.__exception = None
         MASKED_GREENLETS.setdefault(self.__greenlet, set()).add(self)
 
     def __exit__(self, exc_type, value, traceback):
@@ -26,27 +26,17 @@ class KillMask:
         if MASKED_GREENLETS[self.__greenlet]:
             return
         MASKED_GREENLETS.pop(self.__greenlet)
-        if self.__func:
-            self.__greenlet.parent.loop.run_callback(
-                self.__func, self.__greenlet, self.__exception, self.__waiter
-            )
-            gevent.sleep(0)
-        elif self.__exception is not None:
-            gevent.get_hub().loop.run_callback(self.__greenlet.throw, self.__exception)
+        if self.__exception is not None:
+            raise self.__exception
 
-    def set_kill(self, func, exception, waiter):
+    def set_throw(self, exception):
         if self.__kill_counter:
-            self.__func = func
             self.__exception = exception
-            self.__waiter = waiter
         else:  # reach 0
-            self.__func, self.__exception, self.__waiter = None, None, None
+            self.__exception = None
         cnt = self.__kill_counter
         self.__kill_counter -= 1
         return not cnt
-
-    def set_hub_kill(self, exception):
-        self.__exception = exception
 
 
 def protect_from_kill(fu):
@@ -66,39 +56,19 @@ def protect_from_one_kill(fu):
 
 
 # gevent.greenlet module patch
+class Greenlet(greenlet.Greenlet):
+    def throw(self, exception):
+        if isinstance(exception, gevent.timeout.Timeout):
+            return super().throw(exception)
 
-saved_kill = greenlet._kill
-
-
-def _patched_kill(greenlet, exception, waiter):
-    masks = MASKED_GREENLETS.get(greenlet)
-    if masks:
-        for m in masks:
-            if m.set_kill(saved_kill, exception, waiter):
-                saved_kill(greenlet, exception, waiter)
-
-    else:
-        saved_kill(greenlet, exception, waiter)
-
-
-# Alter the true scope of the greenlet module.
-# With gevent >= 1.3, the module would be typically be 'gevent._greenlet',
-# with its globals copied to 'gevent.greenlet'. That means patching
-# 'greenlet._kill' doesn't work since the methods of the 'Greenlet' class
-# are looking for '_kill' in the 'gevent._greenlet' namespace.
-sys.modules[greenlet.__name__]._kill = _patched_kill
+        masks = MASKED_GREENLETS.get(self)
+        if masks:
+            for m in list(masks):
+                if m.set_throw(exception):
+                    super().throw(exception)
+        else:
+            super().throw(exception)
 
 
-# gevent.hub module patch
-
-
-def _hub_patched_kill(greenlet, exception):
-    masks = MASKED_GREENLETS.get(greenlet)
-    if masks:
-        for m in masks:
-            m.set_hub_kill(exception)
-    elif not greenlet.dead:
-        gevent.get_hub().loop.run_callback(greenlet.throw, exception)
-
-
-hub.kill = _hub_patched_kill
+gevent.spawn = Greenlet.spawn
+gevent.spawn_later = Greenlet.spawn_later
--- tests/common/test_greenlet_utils.py
+++ tests/common/test_greenlet_utils.py
@@ -1,4 +1,5 @@
 
+import sys
 import gevent
 
 from bliss.common.greenlet_utils import protect_from_kill, protect_from_one_kill
@@ -48,3 +49,22 @@ def test_protect_from_one_kill():
     event4.set()
     task.join()
     assert not event5.is_set()
+
+
+def test_protect_from_kill_all():
+    event1 = gevent.event.Event()
+    event2 = gevent.event.Event()
+    event3 = gevent.event.Event()
+
+    @protect_from_kill
+    def target():
+        event1.set()
+        event2.wait()
+        event3.set()
+
+    task = gevent.spawn(target)
+    event1.wait()
+    gevent.killall([task], block=False)
+    event2.set()
+    task.join()
+    assert event3.is_set()

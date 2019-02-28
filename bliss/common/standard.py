f0e3966970084d2e6bbcf23f6e7c5ec2cb8474ef
Linus Pithan <linus.pithan@esrf.fr>
2019-02-28 16:50:04 +0100

    add counter.name to cntdict and lscnt

--- bliss/common/standard.py
+++ bliss/common/standard.py
@@ -495,6 +495,7 @@ def cntdict():
                     counters_dict[cnt.fullname] = (
                         shape[len(cnt.shape)],
                         controller_name,
+                        cnt.name
                     )
     return counters_dict
 
@@ -507,7 +508,7 @@ def lscnt():
     for counter_name, counter_info in sorted(cntdict().items()):
         table_info.append(itertools.chain([counter_name], counter_info))
     print("")
-    print(str(tabulate(table_info, headers=["Name", "Shape", "Controller"])))
+    print(str(tabulate(table_info, headers=["Fullname", "Shape", "Controller","Name"])))
 
 
 def edit_roi_counters(detector, acq_time=None):

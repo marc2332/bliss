8993e3bce2e6475c4e6a39c6e6913b08ac40bf8c
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2019-05-16 16:50:16 +0200

    beacon object: first version
    uniform configuration parameters with settings parameters.

---
+++ bliss/config/beacon_object.py
@@ -0,0 +1,314 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) 2015-2019 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+
+import yaml
+from functools import wraps
+from bliss.config.settings import HashObjSetting, pipeline
+from bliss.config.channels import Cache
+from bliss.common.utils import Null, autocomplete_property
+from bliss.config.conductor.client import remote_open
+
+
+def _find_dict(name, d):
+    if d.get("name") == name:
+        return d
+    for key, value in d.items():
+        if isinstance(value, dict):
+            sub_dict = _find_dict(name, value)
+        elif isinstance(value, list):
+            sub_dict = _find_list(name, value)
+        else:
+            continue
+
+        if sub_dict is not None:
+            return sub_dict
+
+
+def _find_list(name, l):
+    for value in l:
+        if isinstance(value, dict):
+            sub_dict = _find_dict(name, value)
+        elif isinstance(value, list):
+            sub_dict = _find_list(name, value)
+        else:
+            continue
+        if sub_dict is not None:
+            return sub_dict
+
+
+class BeaconObject:
+    class _config_getter(property):
+        pass
+
+    class _property(property):
+        def __init__(
+            self,
+            fget=None,
+            fset=None,
+            fdel=None,
+            doc=None,
+            must_be_in_config=False,
+            only_in_config=False,
+            default=Null,
+            priority=0,
+        ):
+            try:
+                fget.__redefined__
+            except AttributeError:
+                if only_in_config:
+
+                    def get(self):
+                        self._initialize_with_setting()
+                        return fget(self)
+
+                else:
+
+                    def get(self):
+                        self._initialize_with_setting()
+                        object_name = self.config["name"]
+                        if self._disabled_settings.get(fget.__name__):
+                            return fget(self)
+
+                        value = self._settings.get(fget.__name__)
+                        return value if value is not None else fget(self)
+
+                get.__redefined__ = True
+                get.__default__ = default
+                get.__must_be_in_config__ = must_be_in_config or only_in_config
+                get.__only_in_config__ = only_in_config
+                get.__priority__ = priority
+            else:
+                must_be_in_config = fget.__must_be_in_config__
+                only_in_config = fget.__only_in_config__
+                default = fget.__default__
+                priority = fget.__priority__
+                get = fget
+
+            if fset is not None:
+                if only_in_config:
+
+                    def set(self, value):
+                        if not self._in_initialize_with_setting:
+                            raise RuntimeError(
+                                f"parameter {fset.__name__} is read only"
+                            )
+                        fset(self, value)
+
+                else:
+                    fence = {"in_set": False}
+
+                    def set(self, value):
+                        if fence.get("in_set"):
+                            return
+                        try:
+                            fence["in_set"] = True
+                            rvalue = fset(self, value)
+                            self._settings[fset.__name__] = (
+                                rvalue if rvalue is not None else value
+                            )
+                            self._initialize_with_setting()
+                        finally:
+                            fence["in_set"] = False
+
+            else:
+                set = None
+            super().__init__(get, set, fdel, doc)
+            self.default = default
+            self.must_be_in_config = must_be_in_config
+            self.only_in_config = only_in_config
+            self.priority = priority
+
+    def __init__(self, config):
+        try:
+            name = config["name"]
+            self._config = config
+            if not hasattr(self, "name"):
+                self.name = name
+        except KeyError:
+            raise RuntimeError("config object must have a name")
+
+        self.__initialized = Cache(self, "initialized", default_value=False)
+        self._in_initialize_with_setting = False
+
+    @autocomplete_property
+    def config(self):
+        return self._config
+
+    @autocomplete_property
+    def settings(self):
+        self._initialize_with_setting()
+        return self._settings
+
+    def __update_settings(self):
+        config = self.config
+        settings_property = self.__settings_properties()
+        default_values = {
+            name: prop.default
+            for name, prop in settings_property.items()
+            if prop.default != Null
+        }
+        must_be_in_config = set(
+            [name for name, prop in settings_property.items() if prop.must_be_in_config]
+        )
+        must_be_in_config.update(self.__config_getter().keys())
+
+        if not must_be_in_config <= config.keys():
+            missing_keys = must_be_in_config - config.keys()
+            raise RuntimeError(
+                f"For device {self.name} configuration must contains {missing_keys}"
+            )
+        config_values = {
+            name: config.get(name)
+            for name in settings_property.keys()
+            if config.get(name, Null) != Null
+        }
+        default_values.update(config_values)
+        self._settings = HashObjSetting(
+            f"{self.name}:settings", default_values=default_values
+        )
+        self._disabled_settings = HashObjSetting(f"{self.name}:disabled_settings")
+
+    def apply_config(self, reload=False):
+        name = self.config["name"]
+        if reload:
+            with remote_open(self.config.filename) as f:
+                d = yaml.safe_load(f.read())
+            if isinstance(d, dict):
+                d = _find_dict(name, d)
+            elif isinstance(d, list):
+                d = _find_list(name, d)
+            else:
+                d = None
+
+            if d is None:
+                raise RuntimeError(
+                    f"Can't find config node named:{name} "
+                    f"in file:{self.config.filename}"
+                )
+            self.config.update(d)
+        self._settings.remove(*self.__settings_properties().keys())
+        self.__initialized.value = False
+        self._initialize_with_setting()
+
+    def disable_setting(self, name):
+        """
+        If a setting is disable, hardware if always read
+        and it's not set at init
+        """
+        self._disabled_settings[name] = True
+
+    def enable_setting(self, name):
+        with pipeline(self._settings, self._disabled_settings):
+            del self._disabled_settings[name]
+            del self._settings[name]
+
+    def _initialize_with_setting(self):
+        if self._in_initialize_with_setting:
+            return
+        try:
+            self._in_initialize_with_setting = True
+            if not self.__initialized.value:
+                self.__update_settings()
+                values = self._settings.get_all()
+                error_messages = []
+                for name, prop in self.__settings_properties().items():
+                    if prop.fset is None:
+                        error_messages.append(
+                            f"object {self.name} doesn't have property setter for {name}"
+                        )
+                        continue
+                    if self._disabled_settings.get(name):
+                        continue
+                    val = values.get(name, Null)
+                    if val != Null:
+                        try:
+                            setattr(self, name, val)
+                        except AttributeError:
+                            raise AttributeError(
+                                f"can't set attribute {name} for device {self.name}"
+                            )
+                    else:  # initialize setting
+                        self._settings[name] = getattr(self, name)
+
+                if error_messages:
+                    raise NotImplementedError("\n".join(error_messages))
+                self.__initialized.value = True
+        finally:
+            self._in_initialize_with_setting = False
+
+    @property
+    def _is_initialized(self):
+        return self.__initialized.value
+
+    def __filter_attribute(self, filter):
+        # Follow the order of declaration in the class
+        # Don't use dir() which alphabetize
+        prop_dict = dict()
+        for klass in reversed(self.__class__.mro()):
+            for name, prop in klass.__dict__.items():
+                if isinstance(prop, filter):
+                    prop_dict[name] = prop
+                else:
+                    prop_dict.pop(name, None)
+        return prop_dict
+
+    def __settings_properties(self):
+        setting_properties = self.__filter_attribute(BeaconObject._property)
+        return {
+            key: value
+            for key, value in sorted(
+                setting_properties.items(), key=lambda x: x[1].priority
+            )
+        }
+
+    def __config_getter(self):
+        return self.__filter_attribute(BeaconObject._config_getter)
+
+    @staticmethod
+    def property(
+        fget=None,
+        fset=None,
+        fdel=None,
+        doc=None,
+        must_be_in_config=False,
+        only_in_config=False,
+        default=Null,
+        priority=0,
+    ):
+        if fget is None:
+
+            def f(fget):
+                return BeaconObject._property(
+                    fget,
+                    must_be_in_config=must_be_in_config,
+                    only_in_config=only_in_config,
+                    default=default,
+                    priority=priority,
+                )
+
+            return f
+        return BeaconObject._property(
+            fget, fset, fdel, doc, must_be_in_config, only_in_config, default, priority
+        )
+
+    @staticmethod
+    def config_getter(parameter_name):
+        def get(self):
+            return self.config[parameter_name]
+
+        property = BeaconObject._config_getter(get)
+        property.parameter_name = parameter_name
+        return property
+
+    @staticmethod
+    def lazy_init(func):
+        @wraps(func)
+        def f(self, *args, **kwargs):
+            self._initialize_with_setting()
+            return func(self, *args, **kwargs)
+
+        return f
---
+++ tests/config/test_beacon_object.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) 2015-2019 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+import numpy
+import pytest
+from bliss.config.beacon_object import BeaconObject
+
+
+class Ctrl(BeaconObject):
+    def __init__(self, config):
+        super().__init__(config)
+        self._speed, self._velocity = numpy.random.random(2)
+        self._reading_speed = None
+        self._mode = "blabla"
+
+    @BeaconObject.property(must_be_in_config=True)
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, value):
+        self._speed = value
+
+    @BeaconObject.property
+    def velocity(self):
+        return self._velocity
+
+    @velocity.setter
+    def velocity(self, value):
+        self._velocity = value
+
+    @BeaconObject.property(default="quick")
+    def reading_speed(self):
+        return self._reading_speed
+
+    @reading_speed.setter
+    def reading_speed(self, value):
+        self._reading_speed = value
+
+    @BeaconObject.property
+    def mode(self):
+        return self._mode
+
+    @mode.setter
+    def mode(self, value):
+        self._mode = value
+
+    @BeaconObject.lazy_init
+    def check_lazy_init(self):
+        return self._reading_speed is not None
+
+
+@pytest.mark.parametrize(
+    "ctrl_name", [("controller_setting1"), ("controller_setting2")]
+)
+def test_config_and_settings_basic_check(beacon, ctrl_name):
+    cfg = beacon.get(ctrl_name)
+    ctrl = Ctrl(cfg)
+    settings_properties = ctrl._BeaconObject__settings_properties()
+    link_cfg = {key: value for key, value in cfg.items() if key in settings_properties}
+    assert link_cfg.items() <= dict(ctrl.settings).items()
+    ctrl.speed = 42
+    assert ctrl.settings["speed"] == 42
+    assert ctrl.speed == 42
+    ctrl._speed = 12
+    assert ctrl.speed == 42
+    ctrl.apply_config()
+    assert ctrl.speed == cfg["speed"]
+    cfg_speed = ctrl.speed
+    ctrl.config["speed"] = 1234
+    assert ctrl.speed == cfg_speed
+    ctrl.apply_config()
+    assert ctrl.speed == 1234
+    ctrl.apply_config(reload=True)
+    assert ctrl.speed == cfg_speed
+
+
+def test_config_and_settings_check_missing_config(beacon):
+    cfg = beacon.get("controller_setting1")
+    cfg.pop("speed")
+    ctrl = Ctrl(cfg)
+    with pytest.raises(RuntimeError):
+        ctrl.speed
+
+
+@pytest.mark.parametrize(
+    "ctrl_name", [("controller_setting1"), ("controller_setting2")]
+)
+def test_config_and_settings_check_default_and_taken_from_hardware(beacon, ctrl_name):
+    cfg = beacon.get(ctrl_name)
+    ctrl = Ctrl(cfg)
+    assert ctrl.reading_speed != None
+    assert ctrl.mode == ctrl._mode
+    assert ctrl.settings["mode"] == ctrl._mode
+    ctrl.mode = "truc"
+    assert ctrl.mode == "truc"
+
+
+def test_config_and_settings_lazy_init(beacon):
+    cfg = beacon.get("controller_setting1")
+    ctrl = Ctrl(cfg)
+    assert ctrl.check_lazy_init()
+
+
+class Ctrl2(BeaconObject):
+    def __init__(self, config):
+        super().__init__(config)
+        self._settling_time = None
+
+    @BeaconObject.property(only_in_config=True)
+    def settling_time(self):
+        return self._settling_time
+
+    @settling_time.setter
+    def settling_time(self, value):
+        self._settling_time = value
+
+
+def test_config_and_settings_only_in_config(beacon):
+    cfg = beacon.get("controller_setting1")
+    ctrl = Ctrl2(cfg)
+    with pytest.raises(RuntimeError):
+        ctrl.settling_time
+    cfg["settling_time"] = 100
+    ctrl = Ctrl2(cfg)
+    assert ctrl.settling_time == 100
+    with pytest.raises(RuntimeError):
+        ctrl.settling_time = 200
+    assert ctrl.settling_time == 100
+
+
+class Ctrl3(BeaconObject):
+    _speed = BeaconObject.config_getter("speed")
+    _velocity = BeaconObject.config_getter("velocity")
+
+
+def test_config_and_settings_config_getter(beacon):
+    cfg = beacon.get("controller_setting1")
+    ctrl = Ctrl3(cfg)
+    assert ctrl._speed == cfg.get("speed")
+    assert ctrl._velocity == cfg.get("velocity")
+
+
+class Ctrl4(BeaconObject):
+    name = BeaconObject.config_getter("name")
+
+    def __init__(self, config):
+        super().__init__(config)
+        assert config.get("name") == self.name
+
+
+def test_config_and_settings_config_getter_constructor(beacon):
+    cfg = beacon.get("controller_setting1")
+    ctrl = Ctrl4(cfg)
+    assert ctrl.name
+
+
+class Ctrl5(BeaconObject):
+    name = BeaconObject.config_getter("name")
+
+    @BeaconObject.property
+    def velocity(self):
+        return 20
+
+    @velocity.setter
+    def velocity(self, value):
+        pass
+
+
+class Ctrl6(Ctrl5):
+    mode = BeaconObject.config_getter("mode")
+
+    @BeaconObject.property
+    def speed(self):
+        return 10
+
+    @speed.setter
+    def speed(self, val):
+        pass
+
+
+def test_config_and_settings_inherited_class(beacon):
+    cfg = beacon.get("controller_setting2")
+    ctrl = Ctrl6(cfg)
+    assert list(ctrl._BeaconObject__settings_properties().keys()) == [
+        "velocity",
+        "speed",
+    ]
+    assert list(ctrl._BeaconObject__config_getter().keys()) == ["name", "mode"]
+
+
+class Ctrl7(BeaconObject):
+    @BeaconObject.property(priority=2)
+    def speed(self):
+        return 10
+
+    @speed.setter
+    def speed(self, val):
+        pass
+
+
+class Ctrl8(Ctrl7):
+    @BeaconObject.property
+    def velocity(self):
+        return 20
+
+    @velocity.setter
+    def velocity(self, val):
+        pass
+
+    @BeaconObject.property(priority=1)
+    def mode(self):
+        return "bla"
+
+    @mode.setter
+    def mode(self, value):
+        pass
+
+
+def test_config_and_settings_priority_test(beacon):
+    cfg = beacon.get("controller_setting2")
+    ctrl = Ctrl8(cfg)
+    assert list(ctrl._BeaconObject__settings_properties().keys()) == [
+        "velocity",
+        "mode",
+        "speed",
+    ]

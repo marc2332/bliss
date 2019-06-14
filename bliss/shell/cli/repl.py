47f13ad8217e10c393ab2ad3a923a2b476c3b6f7
Matias Guijarro <guijarro@esrf.fr>
2019-06-15 01:14:31 +0200

    shell/cli/repl.py: suppress exception context when raising exception in _execute_task
    ptpython tries to execute code with 'eval', if it fails because of SyntaxError it
    executes the code with 'exec'. The problem is that the SyntaxError exception
    becomes part of the context of any further exception that can be raised.
    So, the traceback displays a scary, unhelpful traceback with SyntaxError.
    This change removes this problem, by clearing the context.

--- bliss/shell/cli/repl.py
+++ bliss/shell/cli/repl.py
@@ -256,7 +256,7 @@ class BlissRepl(PythonRepl):
                 and len(return_value) >= 3
                 and isinstance(return_value[1], (BaseException, Exception))
             ):
-                raise return_value[1].with_traceback(return_value[2])
+                raise return_value[1].with_traceback(return_value[2]) from None
         except gevent.Timeout:
             self._handle_exception(*args)
         except KeyboardInterrupt:

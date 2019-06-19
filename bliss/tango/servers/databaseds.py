2f4bad32284658a7bce7d97a867fcd52195fa804
Matias Guijarro <guijarro@esrf.fr>
2019-06-19 14:04:03 +0200

    databaseds.py: removed the mess with db_access Beacon plugin (now that we rely on newest PyTango)

--- bliss/tango/servers/databaseds.py
+++ bliss/tango/servers/databaseds.py
@@ -4,19 +4,11 @@ import argparse
 
 from tango.databaseds import db_access
 from tango.databaseds.database import main as base_main
-
 from bliss.tango import db_access as local_db_access
 
 
 def main(args=None):
-    # Give priority to the bliss db_access module
     db_access.__path__ = local_db_access.__path__ + db_access.__path__
-
-    # Safety check ???
-    # does not load ?
-    from tango.databaseds.db_access import beacon
-
-    assert beacon.__file__.startswith(local_db_access.__path__[0])
 
     # Display message (used for synchronization with parent process)
     p = argparse.ArgumentParser()

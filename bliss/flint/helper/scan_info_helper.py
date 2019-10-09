b03d894de55623b9fc64115346db65e09b47f600
Valentin Valls <valentin.valls@esrf.fr>
2019-10-09 10:39:55 +0200

    Create helper to create scan model from scan_info

--- bliss/flint/helper/scan_info_helper.py
+++ bliss/flint/helper/scan_info_helper.py
@@ -12,7 +12,11 @@ from typing import Any
 from typing import Dict
 
 import collections
+import logging
+from ..model import scan_model
 
+
+_logger = logging.getLogger(__name__)
 
 Channel = collections.namedtuple("Channel", ["name", "kind", "device", "master"])
 
@@ -46,3 +50,55 @@ def iter_channels(scan_info: Dict[str, Any]):
             device_name = channel_name.split(":")[0]
             channel = Channel(channel_name, "image", device_name, master_name)
             yield channel
+
+
+def create_scan_model(scan_info: Dict) -> scan_model.Scan:
+    scan = scan_model.Scan()
+
+    devices: Dict[str, scan_model.Device] = {}
+
+    # Mapping from scan_info to scan model
+    kinds = {
+        "scalar": scan_model.ChannelType.COUNTER,
+        "spectrum": scan_model.ChannelType.SPECTRUM,
+        "image": scan_model.ChannelType.IMAGE,
+    }
+
+    channels = iter_channels(scan_info)
+    for channel_info in channels:
+        if channel_info.device in devices:
+            device = devices[channel_info.device]
+        else:
+            # Device have to be created
+            if channel_info.master == channel_info.device:
+                device = scan_model.Device(scan)
+                device.setName(channel_info.device)
+                devices[channel_info.device] = device
+            else:
+                if channel_info.master in devices:
+                    master = devices[channel_info.master]
+                else:
+                    # Master have to be created
+                    master = scan_model.Device(scan)
+                    master.setName(channel_info.master)
+                    devices[channel_info.master] = master
+                device = scan_model.Device(scan)
+                device.setName(channel_info.device)
+                device.setMaster(master)
+                devices[channel_info.device] = device
+
+        kind = kinds.get(channel_info.kind, None)
+        if kind is None:
+            _logger.error(
+                "Channel kind '%s' unknown. Channel %s skipped.",
+                channel_info.kind,
+                channel_info.name,
+            )
+            continue
+
+        channel = scan_model.Channel(device)
+        channel.setName(channel_info.name)
+        channel.setType(kind)
+
+    scan.seal()
+    return scan
--- tests/flint/helper/test_scan_info_helper.py
+++ tests/flint/helper/test_scan_info_helper.py
@@ -3,30 +3,33 @@
 import pytest
 
 from bliss.flint.helper import scan_info_helper
+from bliss.flint.model import scan_model
+
+
+SCAN_INFO = {
+    "acquisition_chain": {
+        "timer": {
+            "display_names": {"diode:diode": "diode", "images": []},
+            "master": {
+                "display_names": {
+                    "timer:elapsed_time": "elapsed_time",
+                    "timer:epoch": "epoch",
+                },
+                "images": [],
+                "scalars": ["timer:elapsed_time", "timer:epoch"],
+                "scalars_units": {"timer:elapsed_time": "s", "timer:epoch": "s"},
+                "spectra": [],
+            },
+            "scalars": ["diode:diode"],
+            "scalars_units": {"diode:diode": None, "spectra": []},
+        },
+        "timer2": {"spectra": ["opium:mca1"], "images": ["lima:image1"]},
+    }
+}
 
 
 def test_iter_channels():
-    scan_info = {
-        "acquisition_chain": {
-            "timer": {
-                "display_names": {"diode:diode": "diode", "images": []},
-                "master": {
-                    "display_names": {
-                        "timer:elapsed_time": "elapsed_time",
-                        "timer:epoch": "epoch",
-                    },
-                    "images": [],
-                    "scalars": ["timer:elapsed_time", "timer:epoch"],
-                    "scalars_units": {"timer:elapsed_time": "s", "timer:epoch": "s"},
-                    "spectra": [],
-                },
-                "scalars": ["diode:diode"],
-                "scalars_units": {"diode:diode": None, "spectra": []},
-            },
-            "timer2": {"spectra": ["opium:mca1"], "images": ["lima:image1"]},
-        }
-    }
-    result = scan_info_helper.iter_channels(scan_info)
+    result = scan_info_helper.iter_channels(SCAN_INFO)
     expected = [
         scan_info_helper.Channel("diode:diode", "scalar", "diode", "timer"),
         scan_info_helper.Channel("timer:elapsed_time", "scalar", "timer", "timer"),
@@ -35,3 +38,34 @@ def test_iter_channels():
         scan_info_helper.Channel("lima:image1", "image", "lima", "timer2"),
     ]
     assert set(result) == set(expected)
+
+
+def test_create_scan_model():
+    scan = scan_info_helper.create_scan_model(SCAN_INFO)
+    assert scan.isSealed()
+
+    channelCount = 0
+    deviceCount = len(list(scan.devices()))
+    for device in scan.devices():
+        channelCount += len(list(device.channels()))
+    assert channelCount == 5
+    assert deviceCount == 5
+
+    expected = [
+        ("diode:diode", scan_model.ChannelType.COUNTER, "diode", "timer"),
+        ("timer:elapsed_time", scan_model.ChannelType.COUNTER, "timer", "timer"),
+        ("timer:epoch", scan_model.ChannelType.COUNTER, "timer", "timer"),
+        ("opium:mca1", scan_model.ChannelType.SPECTRUM, "opium", "timer2"),
+        ("lima:image1", scan_model.ChannelType.IMAGE, "lima", "timer2"),
+    ]
+
+    for channel_info in expected:
+        name, kind, device, master = channel_info
+        channel = scan.getChannelByName(name)
+        assert channel.name() == name
+        assert channel.type() == kind
+        assert channel.device().name() == device
+        if device == master:
+            assert channel.device().master() is None
+        else:
+            assert channel.device().master().name() == master

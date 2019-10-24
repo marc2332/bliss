5a4f6800e2b94e84146179fbeb4d4431f72d0c87
Matias Guijarro <guijarro@esrf.fr>
2019-10-24 11:35:52 +0200

    fix issue #1091: all Tango devices have access mode to DEV (no cache) by default

--- tests/tango/test_tango_compat.py
+++ tests/tango/test_tango_compat.py
@@ -1,9 +1,15 @@
-def test_tango_imports():
+def test_tango_imports(dummy_tango_server):
     from bliss.common import tango as compat
     import tango.gevent
 
-    assert compat.DeviceProxy is tango.gevent.DeviceProxy
-    assert compat.AttributeProxy is tango.gevent.AttributeProxy
+    assert compat._DeviceProxy is tango.gevent.DeviceProxy
+    assert compat._AttributeProxy is tango.gevent.AttributeProxy
     assert compat.DevState is tango.DevState
     assert compat.EventType is tango.EventType
     assert compat.AttrQuality is tango.AttrQuality
+
+    device_fqdn, proxy = dummy_tango_server
+
+    tg_proxy = tango.gevent.DeviceProxy(device_fqdn)
+
+    assert proxy.__wrapped__.__class__ == tg_proxy.__class__

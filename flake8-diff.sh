0b11742868a9a4fdef58485b2bd6b3133334df13
Benoit Formet <benoit.formet@esrf.fr>
2019-10-25 17:14:04 +0200

    do not prevent commit on linter failed

--- flake8-diff.sh
+++ flake8-diff.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Run flake8 linter on diff lines of stage area
-git diff --cached -U0 | flake8 --diff
+#   - never fails (always exit 0)
+#     => errors are reported for information purpose only
+git diff --cached -U0 | flake8 --diff --exit-zero

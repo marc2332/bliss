e5da15d9f5483e93be1eb14d5e44992def379261
Linus Pithan <pithan@lpithan.esrf.fr>
2019-10-30 13:55:41 +0100

    test scan async

---
+++ tests/scans/test_scan_async.py
@@ -0,0 +1,35 @@
+import gevent
+
+from bliss.common import scans
+
+
+def test_async_demo_test_session(session, scan_tmpdir):
+    # put scan file in a tmp directory
+    session.env_dict["SCAN_SAVING"].base_path = str(scan_tmpdir)
+
+    diode = session.config.get("diode")
+    sim_ct_gauss = session.config.get("sim_ct_gauss")
+    robz = session.config.get("robz")
+
+    s1 = scans.loopscan(20, .1, diode, run=False)
+    s2 = scans.ascan(robz, 0, 1, 20, .1, sim_ct_gauss, run=False)
+    g1 = gevent.spawn(s1.run)
+    g2 = gevent.spawn(s2.run)
+
+    gevent.joinall([g1, g2], raise_error=True)
+
+
+def test_async_demo_default(default_session, scan_tmpdir):
+    # put scan file in a tmp directory
+    default_session.env_dict["SCAN_SAVING"].base_path = str(scan_tmpdir)
+
+    diode = default_session.config.get("diode")
+    sim_ct_gauss = default_session.config.get("sim_ct_gauss")
+    robz = default_session.config.get("robz")
+
+    s1 = scans.loopscan(20, .1, diode, run=False)
+    s2 = scans.ascan(robz, 0, 1, 20, .1, sim_ct_gauss, run=False)
+    g1 = gevent.spawn(s1.run)
+    g2 = gevent.spawn(s2.run)
+
+    gevent.joinall([g1, g2], raise_error=True)

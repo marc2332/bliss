b7942ae2664dd210d2f3622055e176878055e114
pguillou <perceval.guillou@esrf.fr>
2019-12-02 17:48:02 +0100

    add a CalcChannel AcqObj

--- tests/scans/test_calc_counter.py
+++ tests/scans/test_calc_counter.py
@@ -10,6 +10,11 @@ import numpy
 
 from bliss.common.scans import ascan
 from bliss.controllers.simulation_calc_counter import MeanCalcCounterController
+from bliss.scanning.acquisition.motor import LinearStepTriggerMaster
+from bliss.scanning.acquisition.calc import CalcChannelAcquisitionSlave, CalcHook
+from bliss.scanning.scan import Scan
+from bliss.scanning.chain import AcquisitionChain
+from bliss.scanning.channel import AcquisitionChannel
 
 
 def test_calc_counter_from_config(default_session):
@@ -107,3 +112,111 @@ def test_calc_counter_on_the_fly(default_session):
     assert numpy.array_equal(
         sc.get_data()["cc1"], (sc.get_data()["cc3"] + sc.get_data()["cc2"]) / 2.
     )
+
+
+def test_calc_channels_convert_func(default_session):
+
+    npoints = 11
+    start = 0
+    stop = 10
+    count_time = 0.1
+
+    roby = default_session.config.get("roby")
+
+    acq_master = LinearStepTriggerMaster(npoints, roby, start, stop)
+
+    chain = AcquisitionChain()
+
+    # ----- build the calculated channel ------------------------------------------------------------------
+    def func(sender, data_dict):
+        return {"position": data_dict["roby"] * 0.1}
+
+    # calc_chan_out = AcquisitionChannel("position", numpy.float, ())
+    calc_chan_acq = CalcChannelAcquisitionSlave(
+        "calc_chan_acq", [acq_master], func, ["position"]
+    )
+
+    chain.add(acq_master, calc_chan_acq)
+    # -----------------------------------------------------------------------------------------------------
+
+    scan_info = {
+        "npoints": npoints,
+        "count_time": count_time,
+        "start": start,
+        "stop": stop,
+    }
+
+    sc = Scan(
+        chain,
+        name="my_scan",
+        scan_info=scan_info,
+        save=False,
+        save_images=False,
+        scan_saving=None,
+    )
+
+    sc.run()
+
+    assert numpy.array_equal(sc.get_data()["roby"] * 0.1, sc.get_data()["position"])
+
+
+def test_calc_channels_mean_position(default_session):
+
+    npoints = 11
+    start = 0
+    stop = 10
+    count_time = 0.1
+
+    roby = default_session.config.get("roby")
+
+    acq_master = LinearStepTriggerMaster(npoints, roby, start, stop)
+
+    chain = AcquisitionChain()
+
+    # ----- build the calculated channel ------------------------------------------------------------------
+    class MyHook(CalcHook):
+        def __init__(self):
+            self.last_data = None
+
+        def compute(self, sender, data_dict):
+            """ design for the simple case where data is received as a single value (not per block) """
+
+            data = data_dict["roby"]
+
+            if self.last_data is None:
+                self.last_data = data[0]
+                return
+
+            start = self.last_data
+            stop = data[-1]
+            mean = (stop - start) / 2.
+            self.last_data = stop
+            return {"mean_pos": numpy.array([mean])}
+
+    calc_chan_out = AcquisitionChannel("mean_pos", numpy.float, ())
+    calc_chan_acq = CalcChannelAcquisitionSlave(
+        "calc_chan_acq", [acq_master], MyHook(), [calc_chan_out]
+    )
+
+    chain.add(acq_master, calc_chan_acq)
+    # -----------------------------------------------------------------------------------------------------
+
+    scan_info = {
+        "npoints": npoints,
+        "count_time": count_time,
+        "start": start,
+        "stop": stop,
+    }
+
+    sc = Scan(
+        chain,
+        name="my_scan",
+        scan_info=scan_info,
+        save=False,
+        save_images=False,
+        scan_saving=None,
+    )
+
+    sc.run()
+
+    assert numpy.array_equal(sc.get_data()["mean_pos"], numpy.ones((10,)) / 2.)

5fcb8a793396da313d1759e2c0b0f7be3d09483f
Linus Pithan <pithan@lpithan.esrf.fr>
2019-12-09 11:41:32 +0100

    restrict number of know protocols

--- bliss/common/protocols.py
+++ bliss/common/protocols.py
@@ -9,7 +9,8 @@
 This file groups all protocols managed by bliss
 """
 from collections import namedtuple
-from typing_extensions import Protocol,runtime_checkable
+from typing_extensions import Protocol, runtime_checkable
+
 
 def counter_namespace(counters):
     if isinstance(counters, dict):
@@ -18,56 +19,49 @@ def counter_namespace(counters):
         dct = {counter.name: counter for counter in counters}
     return namedtuple("namespace", dct)(**dct)
 
+
 @runtime_checkable
 class CounterContainer(Protocol):
     """
     Device that contains counters.
     """
+
     @property
-    def counters(self) -> namedtuple("namespace",{}):
+    def counters(self) -> namedtuple("namespace", {}):
         """
         Return a **counter_namespace** which hold a list of counters
         attached to this device.
         """
         raise NotImplementedError
-    
+
+
 @runtime_checkable
 class Scannable(Protocol):
     """
     Any device that has this interface can be used
     in a step by step scan.
     """
+
     @property
     def name(self) -> str:
         raise NotImplementedError
-    
+
     @property
     def position(self) -> float:
         """
         Return the current position
         """
         raise NotImplementedError
+
     @property
     def state(self):
         """
         Return the current state.
         """
         raise NotImplementedError
-    
-    def move(self,target_position):
+
+    def move(self, target_position):
         """
         This should 
-        """
-        raise NotImplementedError
-    
-@runtime_checkable
-class ScannableContainer(Protocol):
-    """
-    Device that contains scannable
-    """
-    @property
-    def scannable(self) -> Scannable:
-        """
-        Return a scannable object @see Scannable
         """
         raise NotImplementedError

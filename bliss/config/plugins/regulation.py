e1ef614d114550234c8702dbc7b71130f8ab8a06
pguillou <perceval.guillou@esrf.fr>
2019-12-09 17:53:47 +0100

    fix regulation plugin

--- bliss/config/plugins/regulation.py
+++ bliss/config/plugins/regulation.py
@@ -64,7 +64,8 @@ def create_objects_from_config_node(config, node):
 
             name2items[obj_name] = new_obj
 
-            return name2items
+            yield name2items
+            return
 
     # --- whatever the object kind, first of all we instanciate the controller
     controller_name = node.get("name")  # usually is None

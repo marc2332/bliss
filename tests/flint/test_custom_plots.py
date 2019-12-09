04a4dbee6d2e1d2eeaed3306b60be94415202015
Valentin Valls <valentin.valls@esrf.fr>
2019-12-09 14:21:11 +0100

    Add unittest to check the ROIs selection API

--- tests/flint/test_custom_plots.py
+++ tests/flint/test_custom_plots.py
@@ -21,6 +21,7 @@ def test_select_points(flint_session):
 
     gevent.joinall([gevent.spawn(f) for f in [active_gui, do_actions]])
     assert len(context) == 1
+
     result = context[0]
     assert len(result) == 1
     assert len(result[0]) == 2
@@ -46,13 +47,40 @@ def test_select_shape(flint_session):
 
     gevent.joinall([gevent.spawn(f) for f in [active_gui, do_actions]])
     assert len(context) == 1
+
     result = context[0]
-
-    flint = plot.get_flint()
-    p = plot.plot()
-
-    # 'select' lock reentrant calls, execusion have to be planed before
-    # This requests to click later, good luck
     assert len(result) == 2
     assert len(result[0]) == 2
     assert len(result[1]) == 2
+
+
+def test_select_shapes(flint_session):
+    flint = plot.get_flint()
+    p = plot.plot()
+    context = []
+
+    def active_gui():
+        result = p.select_shapes()
+        context.append(result)
+
+    def do_actions():
+        gevent.sleep(1)
+        flint.test_active(p.plot_id, qaction="roi-select-rectangle")
+        flint.test_mouse(
+            p.plot_id, mode="press", position=(-5, -5), relative_to_center=True
+        )
+        flint.test_mouse(
+            p.plot_id, mode="release", position=(5, 5), relative_to_center=True
+        )
+        flint.test_active(p.plot_id, qaction="roi-apply-selection")
+
+    gevent.joinall([gevent.spawn(f) for f in [active_gui, do_actions]])
+    assert len(context) == 1
+
+    result = context[0]
+    assert len(result) == 1
+    roi = result[0]
+    assert isinstance(roi, dict)
+    expected_keys = set(["origin", "size", "label", "kind"])
+    assert len(expected_keys - roi.keys()) == 0
+    assert roi["kind"] == "Rectangle"

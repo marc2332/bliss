e1eb6363d58f1994aee2dca0ce047d447a03ac1d
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2019-12-10 18:34:07 +0100

    auto filter: first version
    wip: fixed use the active measurement group if no counters is passed in the scan

---
+++ bliss/common/auto_filter/__init__.py
@@ -0,0 +1,176 @@
+# This file is part of the bliss project
+#
+# Copyright (c) 2015-2019 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+"""
+Module to manage scan with automatic filter.
+
+Yaml config may look like this:
+- plugin: bliss
+  class: AutoFilter
+  name: autof
+  package: bliss.common.auto_filter
+  max_nb_iter: 2
+  monitor_counter_name: diode
+  min_count_rate: 0
+  max_count_rate: 10
+"""
+
+import weakref
+from tabulate import tabulate
+from bliss.config.beacon_object import BeaconObject
+from bliss.common import scans
+from bliss.scanning import chain, scan
+from bliss.common.event import dispatcher
+from bliss.common.measurementgroup import _get_counters_from_names
+from bliss.common.measurementgroup import get_active as get_active_mg
+from . import acquisition_objects
+
+
+class AutoFilter(BeaconObject):
+    max_nb_iter = BeaconObject.property_setting(
+        "max_nb_iter", must_be_in_config=True, doc="Max iteration on the same point"
+    )
+    monitor_counter_name = BeaconObject.property_setting(
+        "monitor_counter_name", must_be_in_config=True, doc="Monitor counter name"
+    )
+    min_count_rate = BeaconObject.property_setting(
+        "min_count_rate",
+        must_be_in_config=True,
+        doc="Minimum allowed count rate on monitor",
+    )
+    max_count_rate = BeaconObject.property_setting(
+        "max_count_rate",
+        must_be_in_config=True,
+        doc="Maximum allowed count rate on monitor",
+    )
+
+    def __init__(self, name, config):
+        super().__init__(config, share_hardware=False)
+
+    def ascan(self, motor, start, stop, intervals, count_time, *counter_args, **kwargs):
+        """
+        Basically same as normal ascan with auto filter management
+        """
+
+        save_flag = kwargs.get("save", True)
+        programed_device_intervals = (intervals + 1) * self.max_nb_iter
+        npoints = intervals + 1
+        scan_pars = {
+            "type": "ascan",
+            "npoints": programed_device_intervals,
+            "count_time": count_time,
+            "sleep_time": kwargs.get("sleep_time"),
+            "save": save_flag,
+        }
+
+        monitor_counter_name = self.monitor_counter_name
+        counters, missing = _get_counters_from_names([monitor_counter_name])
+        if missing:
+            raise RuntimeError(
+                f"Can't find monitor counter named {monitor_counter_name}"
+            )
+        monitor_counter = counters[0]
+
+        if not counter_args:  # use the default measurement group
+            counter_args = [get_active_mg()] + [monitor_counter]
+        else:
+            counter_args = list(counter_args) + [monitor_counter]
+        default_chain = scans.DEFAULT_CHAIN.get(scan_pars, counter_args)
+        final_chain, monitor_channel = self._patch_chain(
+            default_chain, npoints, monitor_counter
+        )
+        # cache min and max count-rate
+        min_count_rate = self.min_count_rate
+        max_count_rate = self.max_count_rate
+
+        class Validator:
+            def __init__(self, autofilter):
+                self.__autofilter = weakref.proxy(autofilter)
+                self._point_nb = 0
+                dispatcher.connect(self.new_monitor_value, "new_data", monitor_channel)
+
+            def new_monitor_value(self, event_dict=None, signal=None, sender=None):
+                data = event_dict.get("data")
+                if data is not None:
+                    valid = True
+                    if data > max_count_rate:
+                        self.__autofilter.incr_filter(
+                            data, min_count_rate, max_count_rate
+                        )
+                        valid = False
+                    elif data < min_count_rate:
+                        self.__autofilter.decr_filter(
+                            data, min_count_rate, max_count_rate
+                        )
+                        valid = False
+                    for node in final_chain.nodes_list:
+                        if hasattr(node, "validate_point"):
+                            node.validate_point(self._point_nb, valid)
+                    self._point_nb += 1
+
+        validator = Validator(self)
+        top_master = acquisition_objects.LinearStepTriggerMaster(
+            npoints, motor, start, stop
+        )
+        timer = final_chain.top_masters.pop(0)
+        final_chain.add(top_master, timer)
+        s = scan.Scan(
+            final_chain,
+            scan_info=scan_pars,
+            name=kwargs.setdefault("name", "ascan"),
+            save=kwargs.get("save", True),
+            save_images=kwargs.get("save_images"),
+            data_watch_callback=scan.StepScanDataWatch(),
+        )
+        if kwargs.get("run", True):
+            s.run()
+        return s
+
+    def _patch_chain(self, default_chain, npoints, monitor_counter):
+        final_chain = chain.AcquisitionChain(parallel_prepare=True)
+        monitor_channel = None
+        # use the built **default_chain** and replace all
+        # acquisition object with autofilter one.
+        for master in (
+            x
+            for x in default_chain._tree.expand_tree()
+            if isinstance(x, chain.AcquisitionMaster)
+        ):
+            final_master = acquisition_objects.get_new_master(self, master, npoints)
+            for slave in default_chain._tree.get_node(master).fpointer:
+                final_slave = acquisition_objects.get_new_slave(self, slave, npoints)
+                final_chain.add(final_master, final_slave)
+                if monitor_channel is None:
+                    for channel in slave.channels:
+                        if channel.fullname == monitor_counter.fullname:
+                            monitor_channel = channel
+                            break
+        return final_chain, monitor_channel
+
+    def __info__(self):
+        table_info = []
+        for sname in (
+            "max_nb_iter",
+            "monitor_counter_name",
+            "min_count_rate",
+            "max_count_rate",
+        ):
+            table_info.append([sname, getattr(self, sname)])
+        return str(tabulate(table_info, headers=["Parameter", "Value"]))
+
+    def incr_filter(self, value, min_count_rate, max_count_rate):
+        """
+        Function to increment the filter level
+        value -- measure data
+        max_count_rate -- maximum authorize value
+        """
+        pass
+
+    def decr_filter(self, value, min_count_rate, max_count_rate):
+        """
+        Function to decrement the filter level
+        value -- measure data
+        min_count_rate -- minimum authorize value
+        """
+        pass
---
+++ bliss/common/auto_filter/acquisition_objects.py
@@ -0,0 +1,241 @@
+# This file is part of the bliss project
+#
+# Copyright (c) 2015-2019 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+import weakref
+import gevent
+import numpy
+
+from bliss.scanning.acquisition.motor import (
+    LinearStepTriggerMaster as _LinearStepTriggerMaster
+)
+from bliss.scanning import chain
+from bliss.scanning.acquisition import lima
+from bliss.common.event import dispatcher
+
+
+class LinearStepTriggerMaster(_LinearStepTriggerMaster):
+    def __init__(self, *args, **keys):
+        super().__init__(*args, **keys)
+        self._valid_point = None
+        self._event = gevent.event.Event()
+
+    def __iter__(self):
+        position_iter = zip(*self._motor_pos)
+        positions = next(position_iter)
+        while True:
+            self.next_mv_cmd_arg = []
+            for axis, position in zip(self._axes, positions):
+                self.next_mv_cmd_arg += [axis, position]
+            self.reset_point_valid()
+            yield self
+            if self.is_point_valid():
+                try:
+                    positions = next(position_iter)
+                except StopIteration:
+                    self.stop_all_slaves()
+                    break
+
+    def trigger(self):
+        self.trigger_slaves()
+        self.wait_slaves()
+
+    def validate_point(self, point_nb, valid):
+        self._valid_point = valid
+        self._event.set()
+        if valid:
+            positions = [axis.position for axis in self._axes + self._monitor_axes]
+            self.channels.update_from_iterable(positions)
+
+    def reset_point_valid(self):
+        self._valid_point = None
+        self._event.clear()
+
+    def is_point_valid(self):
+        while self._valid_point is None:
+            self._event.wait()
+        return self._valid_point
+
+
+class _Base:
+    def __init__(self, normalize_func=None):
+        self._name_2_channel = weakref.WeakValueDictionary()
+        # copy all channel from the slave.
+        for channel in self.device.channels:
+            new_channel, _, _ = chain.duplicate_channel(channel)
+            self._name_2_channel[new_channel.name] = new_channel
+            dispatcher.connect(self.new_data_received, "new_data", channel)
+            self.channels.append(new_channel)
+        self.__pending_data = dict()
+        self.__last_point_rx = dict()
+        self.__valid_point = dict()
+        if normalize_func is not None:
+            self._normalize_func = normalize_func
+        else:
+            self._normalize_func = lambda x, y, z: z
+
+    def prepare(self):
+        return self.device.prepare()
+
+    def start(self):
+        return self.device.start()
+
+    def stop(self):
+        return self.device.stop()
+
+    def trigger(self):
+        return self.device.trigger()
+
+    def reading(self):
+        return self.device.reading()
+
+    def trigger_ready(self):
+        return self.device.trigger_ready()
+
+    def wait_ready(self):
+        return self.device.wait_ready()
+
+    def fill_meta_at_scan_init(self, scan_meta):
+        return self.device.fill_meta_at_scan_init(scan_meta)
+
+    def fill_meta_at_scan_end(self, scan_meta):
+        return self.device.fill_meta_at_scan_end(scan_meta)
+
+    def new_data_received(self, event_dict=None, signal=None, sender=None):
+        channel_data = event_dict.get("data")
+        if channel_data is None:
+            return
+
+        channel = sender
+        channel_name = channel.name
+        last_point_rx = self.__last_point_rx.setdefault(channel_name, 0)
+        valid = self.__valid_point.get(last_point_rx)
+        # three cases
+        # valid = False -> not valid
+        # valid = True -> is valid
+        # valid == None -> not yet validated
+        if valid is None:
+            previous_data = self.__pending_data.get(channel_name)
+            if previous_data is None:
+                self.__pending_data[channel_name] = channel_data
+            else:
+                self.__pending_data[channel_name] = numpy.append(
+                    previous_data, channel_data
+                )
+        else:  # valid is True or False
+            if valid:
+                my_channel = self._name_2_channel[channel_name]
+                normalized_data = self._normalize_func(
+                    last_point_rx, channel_name, channel_data
+                )
+                my_channel.emit(normalized_data)
+        self.__last_point_rx[channel_name] = last_point_rx + len(channel_data)
+
+    def validate_point(self, point_nb, valid_flag):
+        # for now we just do simple thing we remove all the
+        # pending... to check if it too simple.  Doesn't take into
+        # account data block receiving...
+        self.__valid_point[point_nb] = valid_flag
+        if not valid_flag:
+            # clean pending_data
+            self.__pending_data = dict()
+        else:
+            pending_data = self.__pending_data
+            self.__pending_data = dict()
+            for channel_name, data in pending_data.items():
+                channel = self._name_2_channel[channel_name]
+                normalized_data = self._normalize_func(point_nb, channel_name, data)
+                channel.emit(normalized_data)
+
+
+class _Slave(_Base, chain.AcquisitionSlave):
+    def __init__(self, slave, npoints=1, normalize_func=None):
+        chain.AcquisitionSlave.__init__(
+            self,
+            slave,
+            name=slave.name,
+            npoints=npoints,
+            trigger_type=slave.trigger_type,
+            prepare_once=slave.prepare_once,
+            start_once=slave.start_once,
+        )
+        _Base.__init__(self, normalize_func=normalize_func)
+
+
+class _SlaveIter(_Slave):
+    def __iter__(self):
+        for i in self.device:
+            yield self
+
+
+class _Master(_Base, chain.AcquisitionMaster):
+    def __init__(self, master, npoints=1):
+        chain.AcquisitionMaster.__init__(
+            self,
+            master,
+            name=master.name,
+            npoints=npoints,
+            trigger_type=master.trigger_type,
+            prepare_once=master.prepare_once,
+            start_once=master.start_once,
+        )
+        _Base.__init__(self)
+        # hack slaves of master
+        # replace buy our slaves
+        master._AcquisitionMaster__slaves = self.slaves
+
+    @property
+    def parent(self):
+        return chain.AcquisitionMaster.parent.fget(self)
+
+    @parent.setter
+    def parent(self, new_parent):
+        chain.AcquisitionMaster.parent.fset(self, new_parent)
+        # give to the embeded AcqMaster the same parent
+        # to avoid to trig on start (i.e:Timer)
+        self.device.parent = new_parent
+
+
+class _MasterIter(_Master):
+    def __iter__(self):
+        for i in self.device:
+            yield self
+
+
+class _Lima(_MasterIter):
+    pass
+
+
+_MASTERS = weakref.WeakKeyDictionary()
+
+
+def get_new_master(auto_filter, master, npoints):
+    new_master = _MASTERS.get(master)
+    if new_master is None:
+        new_master = _get_new_master(auto_filter, master, npoints)
+        _MASTERS[master] = new_master
+    return new_master
+
+
+def _get_new_master(auto_filter, master, npoints):
+    try:
+        iter(master)
+    except TypeError:
+        return _Master(master, npoints=npoints)
+    else:
+        # check if it Lima
+        if isinstance(master, lima.LimaAcquisitionMaster):
+            return _Lima(auto_filter, master, npoints=npoints)
+        else:
+            return _MasterIter(master, npoints=npoints)
+
+
+def get_new_slave(auto_filter, slave, npoints):
+    if isinstance(slave, chain.AcquisitionMaster):
+        return get_new_master(auto_filter, slave, npoints)
+    try:
+        iter(slave)
+    except TypeError:  # not iterable
+        return _Slave(slave, npoints=npoints)
+    else:  # can be iterable
+        return _SlaveIter(slave, npoints=npoints)

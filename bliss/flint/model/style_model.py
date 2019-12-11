fe3c4091d70449c5554a7af603c1d85e6c9c585c
Valentin Valls <valentin.valls@esrf.fr>
2019-12-11 16:59:22 +0100

    Rework style as named tuple

--- bliss/flint/model/style_model.py
+++ bliss/flint/model/style_model.py
@@ -10,22 +10,37 @@
 from __future__ import annotations
 from typing import Tuple
 from typing import Union
+from typing import Optional
+from typing import NamedTuple
 
 import enum
 
 
 class FillStyle(enum.Enum):
+    NO_FILL = None
     SCATTER_INTERPOLATION = "scatter-interpolation"
     SCATTER_REGULAR_GRID = "scatter-regular-grid"
 
 
 class LineStyle(enum.Enum):
+    NO_LINE = None
     SCATTER_SEQUENCE = "scatter-sequence"
 
 
-class Style:
-    def __init__(
-        self,
+class _Style(NamedTuple):
+    lineStyle: Union[None, str, LineStyle]
+    lineColor: Optional[Tuple[int, int, int]]
+    linePalette: Optional[int]
+    symbolStyle: Optional[str]
+    symbolSize: Optional[float]
+    symbolColor: Optional[Tuple[int, int, int]]
+    colormapLut: Optional[str]
+    fillStyle: Union[None, str, FillStyle]
+
+
+class Style(_Style):
+    def __new__(
+        cls,
         lineStyle: Union[None, str, LineStyle] = None,
         lineColor: Tuple[int, int, int] = None,
         linePalette: int = None,
@@ -35,52 +50,23 @@ class Style:
         colormapLut: str = None,
         fillStyle: Union[None, str, FillStyle] = None,
     ):
-        super(Style, self).__init__()
-        self.__lineStyle: Union[None, str, LineStyle]
         try:
-            self.__lineStyle = LineStyle(lineStyle)
+            lineStyle = LineStyle(lineStyle)
         except ValueError:
-            self.__lineStyle = lineStyle
-        self.__lineColor = lineColor
-        self.__linePalette = linePalette
-        self.__symbolStyle = symbolStyle
-        self.__symbolSize = symbolSize
-        self.__symbolColor = symbolColor
-        self.__colormapLut = colormapLut
-        self.__fillStyle: Union[None, str, FillStyle]
+            pass
         try:
-            self.__fillStyle = FillStyle(fillStyle)
+            fillStyle = FillStyle(fillStyle)
         except ValueError:
-            self.__fillStyle = fillStyle
+            pass
 
-    @property
-    def lineStyle(self) -> Union[None, str, LineStyle]:
-        return self.__lineStyle
-
-    @property
-    def lineColor(self):
-        return self.__lineColor
-
-    @property
-    def linePalette(self):
-        return self.__linePalette
-
-    @property
-    def fillStyle(self) -> Union[None, str, FillStyle]:
-        return self.__fillStyle
-
-    @property
-    def symbolStyle(self):
-        return self.__symbolStyle
-
-    @property
-    def symbolSize(self):
-        return self.__symbolSize
-
-    @property
-    def symbolColor(self):
-        return self.__symbolColor
-
-    @property
-    def colormapLut(self):
-        return self.__colormapLut
+        return super().__new__(
+            cls,
+            lineStyle=lineStyle,
+            lineColor=lineColor,
+            linePalette=linePalette,
+            symbolStyle=symbolStyle,
+            symbolSize=symbolSize,
+            symbolColor=symbolColor,
+            colormapLut=colormapLut,
+            fillStyle=fillStyle,
+        )

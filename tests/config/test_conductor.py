a69991ac22784d32f8fd8a92e6f53963b0e73b3b
Matias Guijarro <guijarro@esrf.fr>
2019-12-13 17:37:49 +0100

    add test for issue #1203

--- tests/config/test_conductor.py
+++ tests/config/test_conductor.py
@@ -5,10 +5,14 @@
 # Copyright (c) 2015-2019 Beamline Control Unit, ESRF
 # Distributed under the GNU LGPLv3. See LICENSE for more info.
 
-from bliss.config.conductor import client
 from bliss.config import static
+from bliss.config.conductor import client, connection
+import contextlib
+import greenlet
 import pytest
 import socket
+import gevent
+import gc
 import os
 
 
@@ -95,3 +99,33 @@ def test_2_clients_1_dead(beacon, two_clients):
     assert not conductor_conn2.who_locked(roby.name)
     conductor_conn2.lock(roby.name)
     assert conductor_conn2.who_locked(roby.name) == {roby.name: "test2"}
+
+@contextlib.contextmanager
+def new_conductor_conn(port):
+    conductor_conn = connection.Connection("localhost", port)
+    yield conductor_conn
+    conductor_conn.close()
+
+def test_multiple_greenlets(ports):
+    # make a new connection to Beacon, so it is not already connected
+    with new_conductor_conn(ports.beacon_port) as conductor_conn:
+        def get_redis_conn():
+            # retrieve redis connection from Beacon:
+            # this will call 'connect' concurrently
+            redis_conn = conductor_conn.get_redis_connection()
+
+            redis_keys = redis_conn.keys("*")
+
+            return redis_conn, redis_keys
+
+        # start 2 greenlets
+        g1 = gevent.spawn(get_redis_conn)
+        g2 = gevent.spawn(get_redis_conn)
+
+        redis_conn1, keys1 = g1.get()
+        redis_conn2, keys2 = g2.get()
+
+        assert redis_conn1 is redis_conn2
+
+        assert len(conductor_conn._redis_connection) == 1
+

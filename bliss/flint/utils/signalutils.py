10421ef66e66acd959ae4993112f1df08d82bd23
Valentin Valls <valentin.valls@esrf.fr>
2020-01-06 17:45:30 +0100

    Create a generic EventAggregator

--- bliss/flint/utils/signalutils.py
+++ bliss/flint/utils/signalutils.py
@@ -7,7 +7,13 @@
 
 """Helper about string formatting"""
 
+from __future__ import annotations
+from typing import MutableMapping
+from typing import Callable
+from typing import List
+from typing import Tuple
 
+import weakref
 from silx.gui import qt
 
 
@@ -48,3 +54,49 @@ class InvalidatableSignal(qt.QObject):
         """Trigger the signal, only if this object was invalidated."""
         if self.__invalidated:
             self.trigger()
+
+
+class EventAggregator:
+    """Allow to stack events and to trig them time to time"""
+
+    def __init__(self):
+        self.__eventStack = []
+        self.__callbacks: MutableMapping[
+            Callable, Callable
+        ] = weakref.WeakKeyDictionary()
+
+    def empty(self):
+        """Returns true if there is no stored events."""
+        return len(self.__eventStack) == 0
+
+    def callbackTo(self, callback):
+        """Create a callback for events which have to be emitted to this
+        `callback`
+
+        Returns a callable which have to be used to receive the event
+        """
+        internalCallback = self.__callbacks.get(callback, None)
+        if internalCallback is None:
+
+            def func(*args, **kwargs):
+                self.__eventStack.append((callback, args, kwargs))
+
+            internalCallback = func
+            self.__callbacks[callback] = internalCallback
+
+        return internalCallback
+
+    def flush(self):
+        """Flush all the stored event to the targetted callbacks.
+        """
+        eventStack, self.__eventStack = self.reduce(self.__eventStack)
+        for callback, args, kwargs in eventStack:
+            callback(*args, **kwargs)
+
+    def reduce(self, eventStack: List) -> Tuple[List, List]:
+        """This method can be implemented to reduce the amount of event in the
+        stack before emitting them.
+
+        Returns the events to process now, and the events to process next time.
+        """
+        return eventStack, []

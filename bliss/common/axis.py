bd67a5246096ccdc01415595514282ba7ac5322e
Emmanuel Papillon <papillon@esrf.fr>
2020-02-03 18:28:15 +0100

    on common axis : correct _wait_home on elmo controller : debug homing procedure correct jog range try to sync pos after jog/home methods added input_register and status_register to help debugging

--- bliss/common/axis.py
+++ bliss/common/axis.py
@@ -1460,7 +1460,7 @@ class Axis:
         velocity = motion.target_pos
         direction = motion.delta
 
-        self._move_loop(polling_time)
+        return self._move_loop(polling_time)
 
     def _jog_cleanup(self, saved_velocity, reset_position):
         self.velocity = saved_velocity
@@ -1580,7 +1580,7 @@ class Axis:
             self.wait_move()
 
     def _wait_home(self, *args):
-        self._move_loop(ctrl_state_funct="home_state")
+        return self._move_loop(ctrl_state_funct="home_state")
 
     @lazy_init
     def hw_limit(self, limit, wait=True, polling_time=DEFAULT_POLLING_TIME):

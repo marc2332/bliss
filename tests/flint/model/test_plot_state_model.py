9a3393d32623c910a051d8e4e12cfadd64d81b7b
Valentin Valls <valentin.valls@esrf.fr>
2020-02-04 18:05:12 +0100

    Add coverage for derivative function

--- tests/flint/model/test_plot_state_model.py
+++ tests/flint/model/test_plot_state_model.py
@@ -4,6 +4,7 @@ import numpy
 from silx.gui import qt
 from bliss.flint.model import plot_state_model
 from bliss.flint.model import plot_item_model
+from bliss.flint.model import scan_model
 
 
 class CurveMock(qt.QObject, plot_item_model.CurveMixIn):
@@ -81,3 +82,18 @@ def test_max_incremental_compute_2():
     assert result.max_location_x == 20
     assert result.max_location_y == 500
     assert result.min_y_value == -10
+
+
+def test_derivative_compute():
+    """Compute the derivative function"""
+    scan = scan_model.Scan()
+    yy = [0] * 5 + list(range(10)) + list(reversed(range(10))) + [0] * 5
+    yy = numpy.cumsum(yy)
+    xx = numpy.arange(len(yy)) * 10
+
+    item = plot_state_model.DerivativeItem()
+    curveItem = CurveMock(xx=xx, yy=yy)
+    item.setSource(curveItem)
+    result = item.compute(scan)
+    assert result is not None
+    assert len(item.xArray(scan)) == len(xx) - 5 * 2

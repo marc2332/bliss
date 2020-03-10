f00d0ad393025c7795e1bf31dec3990df5db90bb
Matias Guijarro <guijarro@esrf.fr>
2020-03-10 09:58:24 +0100

    tests: test disabled state of undulators, test wid function

--- tests/test_undulator.py
+++ tests/test_undulator.py
@@ -4,6 +4,7 @@
 #
 # Copyright (c) 2015-2020 Beamline Control Unit, ESRF
 # Distributed under the GNU LGPLv3. See LICENSE for more info.
+from bliss.shell.standard import wa, wm, wid, info
 
 
 def test_undulator(beacon, dummy_tango_server):
@@ -16,3 +17,44 @@ def test_undulator(beacon, dummy_tango_server):
     assert u23a.velocity == 5
 
     assert u23a.acceleration == 125
+
+
+def test_undulator_disabled(default_session, dummy_tango_server, capsys):
+    _, und_dev = dummy_tango_server
+    u23a = default_session.config.get("u23a")
+
+    und_dev.setDisabled(True)
+
+    assert "DISABLED" in u23a.state
+
+    wa()
+
+    wa_output = capsys.readouterr().out
+
+    assert "*DIS*" in wa_output
+
+    wm(u23a)
+
+    assert "u23a *DISABLED*" in capsys.readouterr().out
+
+    info(u23a)
+
+
+def test_wid(default_session, dummy_tango_server, capsys):
+    _, und_dev = dummy_tango_server
+    u23a = default_session.config.get("u23a")
+
+    wid()
+
+    wid_output = capsys.readouterr().out
+
+    assert (
+        wid_output
+        == "\n    ---------------------------------------\n    ID Device Server id00/tango/dummy\n            Power: 0.000 /  0.0  KW\n    Power density: 0.000 / 0.0  KW/mr2\n\n    u23a - GAP:1.408 - ENABLED   \n\n"
+    )
+
+    und_dev.setDisabled(True)
+
+    wid()
+
+    assert "DISABLED" in capsys.readouterr().out

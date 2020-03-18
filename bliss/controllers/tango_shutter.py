5edce1ac798068017a4267ad7eebed48c3118f63
CyrilGuilloud@lid21nano <cyril.guilloud@esrf.fr>
2020-03-19 00:04:01 +0100

    improve messages when opening/closing a tango shutter

--- bliss/controllers/tango_shutter.py
+++ bliss/controllers/tango_shutter.py
@@ -29,7 +29,7 @@ from gevent import Timeout, sleep
 from bliss import global_map
 from bliss.common.shutter import BaseShutter, BaseShutterState
 from bliss.common.tango import DeviceProxy, DevFailed
-from bliss.common.logtools import log_warning
+from bliss.common.logtools import log_warning, lprint
 
 
 TangoShutterState = Enum(
@@ -136,16 +136,19 @@ class TangoShutter(BaseShutter):
         """
         state = self.state
         if state.name in ("OPEN", "RUNNING"):
-            log_warning(self, "Already open, command ignored")
+            log_warning(self, f"{self.name} already open, command ignored")
         elif state == TangoShutterState.CLOSED:
             try:
                 self.__control.open()
                 self._wait(TangoShutterState.OPEN, timeout)
+                lprint(f"{self.name} was {state.name} and is now {self.state.name}")
             except RuntimeError as err:
                 print(err)
                 raise
         else:
-            raise RuntimeError(f"Cannot open: {state.value}")
+            raise RuntimeError(
+                f"Cannot open {self.name}, current state is: {state.value}"
+            )
 
     def close(self, timeout=60):
         """Close
@@ -156,16 +159,19 @@ class TangoShutter(BaseShutter):
         """
         state = self.state
         if state == TangoShutterState.CLOSED:
-            log_warning(self, "Already closed, command ignored")
+            log_warning(self, f"{self.name} already closed, command ignored")
         elif state.name in ("OPEN", "RUNNING"):
             try:
                 self.__control.close()
                 self._wait(TangoShutterState.CLOSED, timeout)
+                lprint(f"{self.name} was {state.name} and is now {self.state.name}")
             except RuntimeError as err:
                 print(err)
                 raise
         else:
-            raise RuntimeError(f"Cannot close: {state.value}")
+            raise RuntimeError(
+                f"Cannot close {self.name}, current state is: {state.value}"
+            )
 
     @property
     def mode(self):

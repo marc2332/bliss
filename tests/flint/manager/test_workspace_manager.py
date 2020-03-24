9c32dc7dbfc2929bc9c05e9475876098461eacec
Valentin Valls <valentin.valls@esrf.fr>
2020-03-24 15:52:34 +0100

    Clean up tests

--- tests/flint/manager/test_workspace_manager.py
+++ tests/flint/manager/test_workspace_manager.py
@@ -64,8 +64,6 @@ def test_dumps_loads__workspace(with_plot, local_flint):
     data = workspace_manager.WorkspaceData()
     data.setWorkspace(workspace, includePlots=with_plot)
     string = pickle.dumps(data)
-    print("-----------------------------------")
-    print(string)
     data2 = pickle.loads(string)
 
     workspace2 = flint_model.Workspace()

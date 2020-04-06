52638668c88344bd2263588bb2d68dc5ae393e27
Matias Guijarro <guijarro@esrf.fr>
2020-04-06 15:52:41 +0200

    tests: add test for 'connect_data_channels' function

--- tests/scans/test_preset.py
+++ tests/scans/test_preset.py
@@ -5,6 +5,9 @@
 # Copyright (c) 2015-2020 Beamline Control Unit, ESRF
 # Distributed under the GNU LGPLv3. See LICENSE for more info.
 
+import collections
+import numpy
+
 from bliss import setup_globals
 from bliss.scanning.chain import AcquisitionChain, ChainPreset, ChainIterationPreset
 from bliss.scanning.scan import ScanPreset
@@ -116,3 +119,42 @@ def test_scan_preset(session):
     assert preset.prepare_counter == 1
     assert preset.start_counter == 1
     assert preset.stop_counter == 1
+
+
+def test_connect_data_channels(session):
+    # issue 1561
+    class Preset(ScanPreset):
+        def __init__(self, counters):
+            self._counters = counters
+            self._cnt_data = collections.defaultdict(list)
+            self._channel_names = {}
+
+        def prepare(self, scan):
+            self.connect_data_channels(self._counters, self.new_data_received)
+
+        def start(self, scan):
+            pass
+
+        def stop(self, scan):
+            pass
+
+        def new_data_received(self, counter, channel_name, data):
+            self._cnt_data[counter].extend(data)
+            self._channel_names[counter] = channel_name
+
+    diode = session.config.get("diode")
+    diode2 = session.config.get("diode2")
+
+    preset = Preset([diode, diode2])
+
+    s = scans.loopscan(2, 0, diode, diode2, run=False)
+    s.add_preset(preset)
+    s.run()
+
+    assert preset._channel_names[diode] == "simulation_diode_sampling_controller:diode"
+    assert (
+        preset._channel_names[diode2] == "simulation_diode_sampling_controller:diode2"
+    )
+    scan_data = s.get_data()
+    numpy.testing.assert_array_equal(preset._cnt_data[diode], scan_data[diode])
+    numpy.testing.assert_array_equal(preset._cnt_data[diode2], scan_data[diode2])

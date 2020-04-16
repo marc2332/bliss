99ea114b62195cc75676bb5be290efe08325664f
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2020-04-16 15:49:19 +0200

    display: avoid eating 100% cpu if beacon dies

--- bliss/data/start_listener.py
+++ bliss/data/start_listener.py
@@ -7,16 +7,22 @@
 
 
 import sys
+import time
+import gevent
 from bliss.data.display import ScanDataListener
 
 
 def main(session_name):
+
     while True:
         try:
+            start_time = time.time()
             sdl = ScanDataListener(session_name)
             sdl.start()
         except Exception as e:
             print(e)
+            if (time.time() - start_time) < 5.0:
+                gevent.sleep(5)
 
 
 if __name__ == "__main__":

837e3a21431d9a1a8d3b7f953566b895aef22d3b
Linus Pithan <linus.pithan@esrf.fr>
2020-04-27 15:19:40 +0200

    keep constants in a BeaconObject so that they can be changed by the user

--- bliss/controllers/expression_based_calc.py
+++ bliss/controllers/expression_based_calc.py
@@ -1,15 +1,38 @@
 from bliss.common.counter import CalcCounter
 from bliss.controllers.counter import CalcCounterController
+from bliss.config.beacon_object import BeaconObject
 import numexpr
+
+
+class ExprCalcParameters(BeaconObject):
+    def __init__(self, config, name):
+        super().__init__(config, name=name, share_hardware=False, path=["constants"])
+
+    def to_dict(self):
+        return {key: getattr(self, key) for key in self.config.to_dict().keys()}
+
+    def __info__(self):
+        # TODO: make nicer!
+        return str(self.to_dict())
 
 
 class ExpressionCalcCounter(CalcCounter):
     def __init__(self, name, config):
+        class ExprCalcCounterParameters(ExprCalcParameters):
+            # has to be a local class as we set properties on it later (on runtime)
+            pass
 
         self._expression = config["expression"]
-        self._constants = config.get("constants", None)
-        if self._constants is not None:
-            self._constants = self._constants.to_dict()
+
+        if "constants" in config:
+            for key, value in config["constants"].to_dict().items():
+                setattr(
+                    ExprCalcCounterParameters,
+                    key,
+                    BeaconObject.property_setting(key, default=value),
+                )
+        self.constants = ExprCalcCounterParameters(config, name)
+
         calc_ctrl_config = {
             "inputs": config["inputs"],
             "outputs": [{"name": name, "tags": name}],
@@ -17,10 +40,7 @@ class ExpressionCalcCounter(CalcCounter):
         self.__controller = CalcCounterController(name + "_ctrl", calc_ctrl_config)
 
         def _calc_function(input_dict):
-            if self._constants is not None:
-                exp_dict = self._constants.copy()
-            else:
-                exp_dict = {}
+            exp_dict = self.constants.to_dict()
             for cnt in self.__controller.inputs:
                 exp_dict.update(
                     {
@@ -44,9 +64,18 @@ class ExpressionCalcCounter(CalcCounter):
 
 class ExpressionCalcCounterController(CalcCounterController):
     def __init__(self, name, config):
-        self._constants = config.get("constants", None)
-        if self._constants is not None:
-            self._constants = self._constants.to_dict()
+        class ExprCalcCntCtrlParameters(ExprCalcParameters):
+            # has to be a local class as we set properties on it later (on runtime)
+            pass
+
+        if "constants" in config:
+            for key, value in config["constants"].to_dict().items():
+                setattr(
+                    ExprCalcCntCtrlParameters,
+                    key,
+                    BeaconObject.property_setting(key, default=value),
+                )
+        self.constants = ExprCalcCntCtrlParameters(config, name)
 
         self._expressions = dict()
         for o in config["outputs"]:
@@ -55,10 +84,7 @@ class ExpressionCalcCounterController(CalcCounterController):
         super().__init__(name, config)
 
         def _calc_function(input_dict):
-            if self._constants is not None:
-                exp_dict = self._constants.copy()
-            else:
-                exp_dict = {}
+            exp_dict = self.constants.to_dict()
             for cnt in self.inputs:
                 exp_dict.update({self.tags[cnt.name]: input_dict[self.tags[cnt.name]]})
             return {

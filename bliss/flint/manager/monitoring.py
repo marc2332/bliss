112cc96783a82994a64707097192e7a0dc9c21f2
Valentin Valls <valentin.valls@esrf.fr>
2020-04-27 20:21:28 +0200

    Create a MonitoringScan to poll Lima detector

---
+++ bliss/flint/manager/monitoring.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) 2015-2020 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+
+"""
+Scan class to manage monitoring of a Lima detector
+"""
+
+from __future__ import annotations
+
+import logging
+import gevent
+import datetime
+
+from bliss.common import tango
+from bliss.flint.model import scan_model
+from bliss.data.nodes import lima
+
+_logger = logging.getLogger(__name__)
+
+
+class MonitoringScan(scan_model.Scan):
+    def __init__(self, parent, channel_name, tango_address, exposure_time):
+        scan_model.Scan.__init__(self, parent=parent)
+        topMaster = scan_model.Device(self)
+        topMaster.setName("monitor")
+        device = scan_model.Device(self)
+        device.setMaster(topMaster)
+        device_name = channel_name.split(":", 1)[0]
+        device.setName(device_name)
+        channel = scan_model.Channel(device)
+        channel.setName(channel_name)
+        channel.setType(scan_model.ChannelType.IMAGE)
+        self._channel = channel
+
+        "lima_tomo_simulator:image"
+        scanInfo = {
+            "type": "monitoring",
+            "acquisition_chain": {"mon": {"images": [channel_name]}},
+            "start_time": datetime.datetime.now(),
+            "count_time": exposure_time,
+            "title": "Monitoring",
+        }
+        self.setScanInfo(scanInfo)
+
+        self.seal()
+        self.__exposure_time = exposure_time
+        self.__task = None
+        self.__proxy = None
+        self.__tango_address = tango_address
+
+    def _get_proxy(self):
+        if self.__proxy is not None:
+            return self.__proxy
+        try:
+            proxy = tango.DeviceProxy(self.__tango_address)
+        except Exception:
+            _logger.error(
+                "Error while create Tango proxy to '%s'" % self.__tango_address,
+                exc_info=True,
+            )
+            return None
+        self.__proxy = proxy
+        return proxy
+
+    def __runMonitoring(self):
+        while self.isMonitoring():
+            gevent.sleep(self.__exposure_time)
+
+            proxy = self._get_proxy()
+            _logger.info("Polling detector %s", proxy)
+            try:
+                result = lima.read_video_last_image(proxy)
+            except:
+                _logger.error("Error while reading data", exc_info=True)
+                raise
+            if not self.isMonitoring():
+                break
+            try:
+                if result is None:
+                    data = scan_model.Data(
+                        None,
+                        array=None,
+                        frameId=None,
+                        source="video-mon",
+                        receivedTime=datetime.datetime.now(),
+                    )
+                else:
+                    frame, frame_number = result
+                    data = scan_model.Data(
+                        None,
+                        array=frame,
+                        frameId=frame_number,
+                        source="video-mon",
+                        receivedTime=datetime.datetime.now(),
+                    )
+                self._channel.setData(data)
+                self._fireScanDataUpdated(channelName=self._channel.name())
+            except:
+                _logger.error("Error while propagating data", exc_info=True)
+                raise
+        # FIXME: It have to be joined
+
+    def isMonitoring(self):
+        return self.__task is not None
+
+    def __exceptionOrrured(self, future_exception):
+        try:
+            future_exception.get()
+        except Exception:
+            _logger.error(
+                "Error occurred while monitoring '%s'",
+                self.__tango_address,
+                exc_info=True,
+            )
+        self.__task = None
+        self._setState(scan_model.ScanState.FINISHED)
+        self.scanFailed.emit()
+        self.scanFinished.emit()
+
+        delay = 5
+        _logger.warning("Retry the monitoring in %s seconds", delay)
+        gevent.sleep(delay)
+        self.startMonitoring()
+
+    def startMonitoring(self):
+        if self.__task is not None:
+            raise RuntimeError("Monitoring already started")
+        _logger.debug("Start monitoring")
+        self._setState(scan_model.ScanState.PROCESSING)
+        self.scanStarted.emit()
+        task = gevent.spawn(self.__runMonitoring)
+        task.link_exception(self.__exceptionOrrured)
+        self.__task = task
+
+    def stopMonitoring(self):
+        if self.__task is None:
+            raise RuntimeError("Monitoring already stopped")
+        _logger.debug("Stop monitoring")
+        task = self.__task
+        self.__task = None
+        task.join()
+        self._setState(scan_model.ScanState.FINISHED)
+        self.scanSuccessed.emit()
+        self.scanFinished.emit()

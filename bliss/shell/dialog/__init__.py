f1dec5d9d570d8214e80246fca4b41dd61320fa9
Piergiorgio Pancino <piergiorgio.pancino@esrf.fr>
2020-05-18 09:42:29 +0200

    shell.dialog: - added menu for: measurementgroups, ascan, wago, transfocator, white_beam_attenuator - updated menu for: multiplexer, lima

--- bliss/shell/dialog/__init__.py
+++ bliss/shell/dialog/__init__.py
@@ -1,1 +1,8 @@
-
+from bliss.shell.dialog.common import measurementgroups, scans
+from bliss.shell.dialog.controller import (
+    transfocator,
+    wago,
+    white_beam_attenuator,
+    lima_dialogs,
+    multiplexer_dialogs,
+)
---
+++ bliss/shell/dialog/common/measurementgroups.py
@@ -0,0 +1,28 @@
+from bliss.shell.dialog.helpers import dialog
+from bliss.shell.cli.user_dialog import UserCheckBox
+from bliss.shell.cli.pt_widgets import BlissDialog
+
+
+@dialog("MeasurementGroup", "selection")
+def measurement_group_selection(obj, *args, **kwargs):
+    dialogs = []
+    counters_info = []
+    for fullname in obj.available:
+        if fullname in obj.enabled:
+            counters_info.append((fullname, True))
+        else:
+            counters_info.append((fullname, False))
+
+    for fullname, enabled in counters_info:
+        dialogs.append([UserCheckBox(label=fullname, defval=enabled)])
+
+    choices = BlissDialog(
+        dialogs, title=f"MeasurementGroup {obj.name} Counter selection", paddings=(3, 3)
+    ).show()
+    if choices:
+        values = list(zip(obj.available, choices.values()))
+        for fullname, enabled in values:
+            if enabled:
+                obj.enable(fullname)
+            else:
+                obj.disable(fullname)

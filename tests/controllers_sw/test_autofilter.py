40051bfa14f3752293d739fd57abb279fa270fb0
Linus Pithan <linus.pithan@esrf.fr>
2020-05-18 07:38:42 +0200

    autof: add test for transmission counter

--- tests/controllers_sw/test_autofilter.py
+++ tests/controllers_sw/test_autofilter.py
@@ -9,7 +9,7 @@ import pytest
 import numpy
 
 
-def test_autofilter_ascan(default_session):
+def test_autofilter_ascan_basics(default_session):
     energy = default_session.config.get("energy")
     energy.move(16)
 
@@ -18,7 +18,35 @@ def test_autofilter_ascan(default_session):
 
     s = autof.ascan(sim.axis, 0, 1, sim.npoints, .1, sim.counter, save=False)
 
-    assert "TestFilterCounterAxis:value" in s.get_data()
-    assert "TestFilterCounterAxis:value_corr" in s.get_data()
 
-    assert numpy.allclose(s.get_data()["TestFilterCounterAxis:value_corr"], sim._data)
+def test_autofilter_ascan(default_session):
+    energy = default_session.config.get("energy")
+    energy.move(16)
+
+    autof = default_session.config.get("autof")
+    filterw = default_session.config.get("filtW0")
+    filterw.filter = 0
+    sim = default_session.config.get("sim_diffraction_peak")
+
+    s = autof.ascan(sim.axis, 0, 1, sim.npoints, .1, sim.counter, autof, save=False)
+
+    scan_data = s.get_data()
+
+    # test that all nessesary counters are published
+    assert "TestFilterCounterAxis:value" in scan_data
+    assert "TestFilterCounterAxis:value_corr" in scan_data
+    assert "autof:curratt" in scan_data
+    assert "autof:transm" in scan_data
+
+    # test that the corrected values are the expected ones
+    assert numpy.allclose(scan_data["TestFilterCounterAxis:value_corr"], sim._data)
+
+    # test that transm fits to measured value
+    assert numpy.allclose(
+        scan_data["TestFilterCounterAxis:value"] / scan_data["autof:transm"],
+        scan_data["TestFilterCounterAxis:value_corr"],
+    )
+
+    # test that all filters are out at beginning and end of scan
+    assert scan_data["autof:transm"][0] == 1.0
+    assert scan_data["autof:transm"][-1] == 1.0

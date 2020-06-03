0a5b74a0705fe44f9994089f2e32750349b12de7
Valentin Valls <valentin.valls@esrf.fr>
2020-06-03 10:23:00 +0200

    Add test with arugment which is not a count time

--- tests/scans/test_timescan.py
+++ tests/scans/test_timescan.py
@@ -44,6 +44,13 @@ def test_ct_count(session):
     assert s.scan_info["count_time"] == pytest.approx(2.0)
 
 
+def test_ct_countable(session):
+    # Test ct with a single argument which is not a number
+    integ_diode = session.config.get("integ_diode")
+    with pytest.raises(RuntimeError):
+        scans.ct(integ_diode)
+
+
 def test_long_trigger_timescan(session, diode_acq_device_factory):
     chain = AcquisitionChain()
     acquisition_device_1, _ = diode_acq_device_factory.get(

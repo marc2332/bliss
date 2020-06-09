6a73cf071e12a32abb29025def4e7bb48ff2541e
Linus Pithan <linus.pithan@esrf.fr>
2020-06-09 12:16:11 +0200

    introduce sct - from now on ct has no save option anymore

--- bliss/common/scans/__init__.py
+++ bliss/common/scans/__init__.py
@@ -7,6 +7,6 @@
 
 from bliss.common.scans import step_by_step
 from bliss.common.scans.step_by_step import *
-from bliss.common.scans.ct import ct
+from bliss.common.scans.ct import ct, sct
 
-__all__ = ["ct"] + step_by_step.__all__
+__all__ = ["ct", "sct"] + step_by_step.__all__
--- bliss/common/scans/ct.py
+++ bliss/common/scans/ct.py
@@ -21,16 +21,14 @@ def ct(
     *counter_args: _countables,
     name: str = "ct",
     title: Optional[str] = None,
-    save: bool = False,
-    save_images: Optional[bool] = None,
-    sleep_time: Optional[_float] = None,
     run: bool = True,
     return_scan: bool = True,
     scan_info: Optional[dict] = None,
 ):
 
     """
-    Counts for a specified time
+    Counts for a specified time. The collected data is not saved and 
+    metadata is not collected.
 
     Use `ct(..., run=False)` to create a count object and
     its acquisition chain without executing the actual count.
@@ -63,19 +61,74 @@ def ct(
         name=name,
         title=title,
         scan_type="ct",
-        save=save,
-        save_images=save_images,
-        sleep_time=sleep_time,
+        save=False,
         run=False,
         return_scan=return_scan,
         scan_info=scan_info,
     )
 
-    if save == False:
-        # disable metadata collection if save = False
-        s._update_scan_info_with_user_scan_meta = lambda: None
+    s._update_scan_info_with_user_scan_meta = lambda: None
 
     if run:
         s.run()
 
     return s
+
+
+@typeguardTypeError_to_hint
+@shorten_signature(hidden_kwargs=["title", "name", "scan_type", "return_scan"])
+@typeguard.typechecked
+def sct(
+    count_time: _float_or_countables = 1.0,
+    *counter_args: _countables,
+    name: str = "ct",
+    title: Optional[str] = None,
+    save: bool = True,
+    save_images: Optional[bool] = None,
+    run: bool = True,
+    return_scan: bool = True,
+    scan_info: Optional[dict] = None,
+):
+
+    """
+    like ct only that 
+    Counts for a specified time. The collected data is not saved and 
+    metadata is not collected.
+
+    Use `ct(..., run=False)` to create a count object and
+    its acquisition chain without executing the actual count.
+
+    Note:
+        This function blocks the current :class:`Greenlet`
+
+    Args:
+        count_time (float): count time (seconds)
+        counter_args (counter-providing objects):
+            each argument provides counters to be integrated in the scan.
+            if no counter arguments are provided, use the active measurement group.
+
+    Keyword Args:
+        name (str): scan name in data nodes tree and directories [default: 'scan']
+        title (str): scan title [default: 'ct <count_time>']
+        save (bool): save scan data to file [default: True]
+        run (bool): if True (default), run the scan. False means just create
+                    scan object and acquisition chain
+        return_scan (bool): True by default
+    """
+    if isinstance(count_time, _countables.__args__):
+        counter_args = [count_time] + list(counter_args)
+        count_time = 1.0
+
+    return timescan(
+        count_time,
+        *counter_args,
+        npoints=1,
+        name=name,
+        title=title,
+        scan_type="ct",
+        save=save,
+        save_images=save_images,
+        run=run,
+        return_scan=return_scan,
+        scan_info=scan_info,
+    )

2dbe91d25602ef316bba27ceb5af0fe4e0c0bafc
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2020-06-24 11:25:56 +0200

    correlator flex: use taco server

--- bliss/controllers/correlator/flex/__init__.py
+++ bliss/controllers/correlator/flex/__init__.py
@@ -15,10 +15,12 @@ Minimalistic configuration example:
    module: correlator.flex
    name: flex
    class: Flex
-   address: tcp://lid312:8909
+   #address: tcp://lid312:8909
+   taco_url: id10/flex/2
+   taco_db_host: ting           # optional 
 """
 from bliss import global_map
-from bliss.comm import rpc
+from bliss.comm.taco.client import Client
 from . import counters
 from .card import MODE
 
@@ -27,15 +29,13 @@ class Flex:
     MODE = MODE
 
     def __init__(self, name, config):
-        kwargs = dict()
-        if "timeout" in config:
-            kwargs["timeout"] = config["timeout"]
-        self._proxy = rpc.Client(config["address"], **kwargs)
+        self._name = name
+        self._proxy = Client(config["taco_url"], config.get("taco_db_host"))
         global_map.register(self, parents_list=["counters", "controllers"])
 
     @property
     def name(self):
-        return self._proxy.name
+        return self._name
 
     @property
     def fullname(self):
@@ -47,21 +47,21 @@ class Flex:
 
     @property
     def mode(self):
-        return self._proxy.mode
+        return MODE(self._proxy.DevFlexGetMode())
 
     @mode.setter
     def mode(self, mo):
-        self._proxy.mode = mo
+        self._proxy.DevFlexSetMode(mo.value)
 
     def start_acquisition(self):
-        return self._proxy.start_acquisition()
+        return self._proxy.DevFlexStart()
 
     def stop_acquisition(self):
-        return self._proxy.stop_acquisition()
+        return self._proxy.DevFlexStop()
 
     @property
     def intensities_and_acqtime(self):
-        return self._proxy.intensities_and_acqtime
+        return self._proxy.DevFlexGetInt()
 
     @property
     def trace(self):
@@ -69,7 +69,15 @@ class Flex:
 
     @property
     def data(self):
-        return self._proxy.data
+        data = self._proxy.DevFlexReadData()
+        mode = self.mode
+        if mode == MODE.SINGLE_AUTO or mode == MODE.SINGLE_CROSS:
+            data.shape = 2, -1
+        elif mode == MODE.DUAL_AUTO or mode == MODE.DUAL_CROSS:
+            data.shape = 3, -1
+        elif mode == MODE.QUAD:
+            data.shape = 5, -1
+        return data
 
     def __info__(self):
         return f"Flex ({self.name}):\n\tmode: {self.mode}"

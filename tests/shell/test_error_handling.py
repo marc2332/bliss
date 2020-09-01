b53461769a62310d0283e26653f665208caf5605
woutdenolf <woutdenolf@users.sf.net>
2020-09-01 11:42:56 +0200

    [test] add test for error reporting

---
+++ tests/shell/test_error_handling.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) 2015-2020 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+
+import gevent
+from bliss.shell.cli import repl
+from bliss.common import logtools
+
+
+def test_error_report():
+    error_report = repl.install_excepthook()
+    errors = error_report._last_error.errors
+    assert len(errors) == 0
+    MYERROR = "MYERROR"
+
+    def raise_exception():
+        raise RuntimeError(MYERROR)
+
+    def raise_hub_exception(nerrors=1):
+        w = gevent.get_hub().loop.async_()
+        w.start(raise_exception)
+        n = len(errors) + nerrors
+        try:
+            w.send()
+            with gevent.Timeout(10):
+                while len(errors) != n:
+                    gevent.sleep(0.1)
+        finally:
+            w.stop()
+            w.close()
+
+    # Exception in greenlet
+    MYERROR = "MYERROR1"
+    gevent.spawn(raise_exception).join()
+    assert len(errors) == 1
+    assert f"RuntimeError: {MYERROR}" in errors[-1]
+
+    # Exception in gevent loop callback
+    MYERROR = "MYERROR2"
+    raise_hub_exception()
+    assert len(errors) == 2
+    assert f"RuntimeError: {MYERROR}" in errors[-1]
+
+    logtools.logbook_on = True
+
+    # Exception in greenlet
+    MYERROR = "MYERROR3"
+    gevent.spawn(raise_exception).join()
+    assert len(errors) == 4
+    assert f"RuntimeError: {MYERROR}" in errors[-2]
+    assert "send_to_elogbook" in errors[-1]
+
+    # Exception in gevent loop callback
+    MYERROR = "MYERROR4"
+    raise_hub_exception(2)
+    assert len(errors) == 6
+    assert f"RuntimeError: {MYERROR}" in errors[-2]
+    assert "send_to_elogbook" in errors[-1]

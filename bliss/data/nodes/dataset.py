3c4bbf7ed6bf4c61f50ff1fa5f64c8190c7e717e
Linus Pithan <pithan@esrf.fr>
2020-09-24 15:20:45 +0200

    make DatasetContainer more specific

--- bliss/data/nodes/dataset.py
+++ bliss/data/nodes/dataset.py
@@ -13,3 +13,17 @@ class DatasetNode(DataNodeContainer):
 
     def __init__(self, name, **kwargs):
         super().__init__(self._NODE_TYPE, name, **kwargs)
+
+    @property
+    def metadata(self):
+        return dict(
+            filter(lambda elem: not elem[0].startswith("__"), self.info.items())
+        )
+
+    @property
+    def is_closed(self):
+        return self.info.get("__closed__", False)
+
+    @property
+    def techniques(self):
+        return self.info.get("__techniques__", set())

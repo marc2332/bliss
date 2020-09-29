marc2332/bliss

commit 360ee52b8e6b80605cad8182b6f2943133c54a4c

motors: fixed check motor's limits for mesh scans.
For mesh scans, motor's limits check was done before the motors positions
was completely build.

bliss/scanning/acquisition/motor.py
@@ -8,7 +8,7 @@
 
 import sys
 import time
-
+from itertools import groupby
 import numpy
 import gevent.event
 
@@ -365,31 +365,41 @@ class _StepTriggerMaster(AcquisitionMaster):
             raise TypeError(
                 "_StepTriggerMaster: argument is a mot1,start,stop,nb points,mot2,start2..."
             )
-        self._motor_pos = []
-        self._axes = []
-        controller_2_axes_position = {}
-        for axis, start, stop, nb_point in grouped(args, 4):
-            self._axes.append(axis)
-            positions = numpy.linspace(start, stop, nb_point)
-            self._motor_pos.append(positions)
-            axes_position = controller_2_axes_position.setdefault(axis.controller, [])
-            axes_position.extend((axis, positions))
-        for controller, axes_position in controller_2_axes_position.items():
-            controller.check_limits(*axes_position)
+
+        self._axes = {
+            axis: (start, stop, npoints)
+            for axis, start, stop, npoints in grouped(args, 4)
+        }
+        self._motor_pos = self._get_axis_positions_dict()
+
+        for controller, axes_pos in groupby(
+            self._motor_pos.items(), lambda item: item[0].controller
+        ):
+            controller.check_limits(axes_pos)
 
         mot_group = Group(*self._axes)
 
         AcquisitionMaster.__init__(self, mot_group, trigger_type=trigger_type, **keys)
-        self._monitor_axes = _init_motor_master_channels(self.channels, self._axes)
+        self._monitor_axes = _init_motor_master_channels(self.channels, self.axes_list)
+
+    @property
+    def axes_list(self):
+        # return a list for axes, **sorted by controller**
+        # (axes with same controller come together),
+        # this is important for the "groupby" call in __init__
+        return sorted(self._axes, key=lambda axis: id(axis.controller))
+
+    def _get_axis_positions_dict(self):
+        return {axis: numpy.linspace(*self._axes[axis]) for axis in self.axes_list}
 
     @property
     def npoints(self):
-        return min((len(x) for x in self._motor_pos))
+        return min((len(pos_array) for pos_array in self._motor_pos.values()))
 
     def __iter__(self):
-        for positions in zip(*self._motor_pos):
+        for positions in zip(*self._motor_pos.values()):
             self.next_mv_cmd_arg = list()
-            for axis, position in zip(self._axes, positions):
+            for axis, position in zip(self.axes_list, positions):
                 self.next_mv_cmd_arg += [axis, position]
             yield self
 
@@ -404,7 +414,7 @@ class _StepTriggerMaster(AcquisitionMaster):
 
     def trigger(self):
         self.trigger_slaves()
-        positions = [axis.position for axis in self._axes + self._monitor_axes]
+        positions = [axis.position for axis in self.axes_list + self._monitor_axes]
         self.channels.update_from_iterable(positions)
         self.wait_slaves()
 
@@ -421,24 +431,38 @@ class MeshStepTriggerMaster(_StepTriggerMaster):
     """
 
     def __init__(self, *args, **keys):
-        backnforth = keys.pop("backnforth", False)
-        _StepTriggerMaster.__init__(self, *args, **keys)
-        self._motor_pos = self._interleaved_motor_pos(
-            *self._motor_pos, backnforth=backnforth
-        )
+        self._backnforth = keys.pop("backnforth", False)
+
+        super().__init__(*args, **keys)
+
+    @property
+    def backnforth(self):
+        return self._backnforth
+
+    def _get_axis_positions_dict(self):
+        motors_pos = super()._get_axis_positions_dict()
+        return {
+            axis: positions
+            for axis, positions in zip(
+                motors_pos,
+                self._interleaved_motor_pos(
+                    *motors_pos.values(), backnforth=self.backnforth
+                ),
+            )
+        }
 
     @staticmethod
     def _interleaved_motor_pos(*motor_pos, backnforth=False):
         """
-        Compute the motor positions for each steps of the scan.
+        Compute motor positions for each step of the scan.
 
         Arguments:
             motor_pos: Individual motor position
-            backnforth: Compute back and forth motion for the first motors.
+            backnforth: Compute back and forth motion for the first motor.
                 Only the slowest motor will not change.
 
         Returns:
-            A list containing a numpy array per motors. Each arrays contains
+            A list containing numpy arrays per motor. Each array contains
             motor position for each steps of the scan
         """
         motor_pos = [numpy.array(mp) for mp in motor_pos]

bliss/controllers/motor.py
@@ -110,8 +110,14 @@ class Controller:
         """
         check limits for list of axis and positions
         """
-        for axis, position in grouped(axis_positions, 2):
-            self._check_limits(axis, position)
+        if len(axis_positions) == 1:
+            # assuming axis_positions is just a grouper object with axis, positions
+            for axis, positions in axis_positions[0]:
+                self._check_limits(axis, positions)
+        else:
+            # backward compatibility
+            for axis, positions in grouped(axis_positions, 2):
+                self._check_limits(axis, positions)
 
     def _check_limits(self, axis, user_positions):
         try:
@@ -507,17 +513,22 @@ class CalcController(Controller):
         self.check_limits(axis, positions)
 
     def check_limits(self, *axis_positions):
-        axes = [axis for axis, _ in grouped(axis_positions, 2)]
+        if len(axis_positions) == 1:
+            # assuming axis_positions is just a grouper object with axis, positions
+            grouped_axis_positions = list(axis_positions[0])
+        else:
+            # backward compatibility
+            grouped_axis_positions = list(grouped(axis_positions, 2))
+        axes = set()
         positions_len = []
-        for axis, pos in grouped(axis_positions, 2):
-            axes.append(axis)
+        for axis, pos in grouped_axis_positions:
+            axes.add(axis)
             try:
                 iter(pos)
             except TypeError:
                 positions_len.append(1)
             else:
                 positions_len.append(len(pos))
-        axes = set(axes)
         try:
             left_axis = axes - set(self.pseudos)
             assert not left_axis
@@ -547,7 +558,7 @@ class CalcController(Controller):
                 for tag, pos in axis_to_positions.items()
             }
         axis_to_positions.update(
-            {self._axis_tag(axis): pos for axis, pos in grouped(axis_positions, 2)}
+            {self._axis_tag(axis): pos for axis, pos in grouped_axis_positions}
         )
         real_positions = self.calc_to_real(axis_to_positions)
         real_min_max = dict()

bliss/controllers/motors/mockup.py
@@ -9,6 +9,7 @@ import time
 import random
 import gevent
 import collections
+import numpy as np
 
 from bliss.physics.trajectory import LinearTrajectory
 from bliss.controllers.motor import Controller, CalcController
@@ -775,3 +776,48 @@ class calc_motor_mockup(CalcController):
         real_pos = positions_dict["calc_mot"] / s_param
 
         return {"real_mot": real_pos}
+
+
+# issue 1909
+class llangle_mockup(CalcController):
+    def initialize(self):
+        CalcController.initialize(self)
+        self.bend_zero = self.config.get("bend_zero", float)
+        self.bend_y = self.config.get("bend_y", float)
+        self.ty_zero = self.config.get("ty_zero", float)
+
+    def calc_from_real(self, positions_dict):
+        # Angle due to pusher not being a rotation
+        # Effect of bending
+        # Effect of translation
+        bend = positions_dict["bend"]
+        rz = positions_dict["rz"]
+        ty = positions_dict["ty"]
+
+        truebend = bend - self.bend_zero  # pass through
+        absty = ty - self.ty_zero
+        bend_offset = np.degrees(truebend * absty / self.bend_y)
+        # only for bent crystal and mono in beam
+        valid = (truebend > 0) & (absty < 75.)
+        angle = np.where(valid, rz + bend_offset, rz)
+        calc_dict = {"angle": angle, "truebend": truebend, "absty": absty}  # computed
+        return calc_dict
+
+    def calc_to_real(self, positions_dict):
+        #
+        angle = positions_dict["angle"]
+        # Effect of bending
+        truebend = positions_dict["truebend"]
+        bend = truebend + self.bend_zero
+        # Effect of translation
+        absty = positions_dict["absty"]  # llty1 / llty2
+        ty = absty + self.ty_zero
+        # Assume we go to the destination ty / bend.
+        # Compute the effect for the angle only
+        bend_offset = np.degrees(truebend * absty / self.bend_y)
+        # only for bent crystal and mono in beam
+        valid = (truebend > 0) & (absty < 75.)
+        # - versus + above:
+        rz = np.where(valid, angle - bend_offset, angle)
+        calc_dict = {"bend": bend, "ty": ty, "rz": rz}
+        return calc_dict

tests/motors/test_calccontroller.py
@@ -7,7 +7,7 @@
 
 import pytest
 from bliss.common.axis import Axis
-from bliss.common.standard import ascan
+from bliss.common.standard import ascan, dmesh
 from bliss.common.motor_group import Group
 from bliss.common import scans
 
@@ -205,3 +205,11 @@ def test_calc_motor_no_settings_axis(beacon, calc_mot3, nsa):
     assert calc_mot3.position == pytest.approx(2)
     calc_mot3.move(1)
     assert nsa.position == pytest.approx(0.5)
+
+
+def test_issue_1909(session):
+    llbragg1 = session.config.get("llbragg1")
+    llbeamy1 = session.config.get("llbeamy1")
+    diode = session.config.get("diode")
+
+    dmesh(llbragg1, -0.005, 0.005, 3, llbeamy1, -1, 1, 2, 0.01, diode)

tests/test_configuration/motors/mockup.yml
@@ -261,6 +261,41 @@ controller:
       high_limit: 16.
     - name: wavelength
       tags: wavelength
+- class: mockup
+  axes:
+    - name: llbend1
+      steps_per_unit: -480000
+      sign: 1
+      velocity: 0.06666
+      acceleration: 0.16666
+    - name: llty1
+      steps_per_unit: -12800
+      sign: 1
+      velocity: 1
+      acceleration: 6.25
+    - name: llrz1
+      steps_per_unit: 625488
+      sign: 1
+      velocity: 0.0511
+      acceleration: 0.1279
+- class: llangle_mockup
+  module: mockup
+  bend_zero: 0.0
+  bend_y:  43688.0
+  ty_zero: 30.
+  axes:
+        - name: $llbend1
+          tags: real bend
+        - name: $llty1
+          tags: real ty
+        - name: $llrz1
+          tags: real rz
+        - name: lltruebend1
+          tags: truebend
+        - name: llbragg1
+          tags: angle
+        - name: llbeamy1
+          tags: absty
 hooks:
 - name: hook0
   plugin: bliss

commit bc7d30af1fa7cf2ad27532c788f840badee0d165

fix issue 2100: order of axes in display (bug previously introduced by 360ee52b

bliss/scanning/acquisition/motor.py
@@ -318,7 +318,9 @@ class JogMotorMaster(AcquisitionMaster):
 
 
 def _init_motor_master_channels(channels, axes):
-    monitor_axes = list()
+    # it is important to return monitor_axes in the same order
+    # as channels (for data emitting)
+    monitor_axes = list(axes)
     channels.extend(
         (
             AcquisitionChannel(f"axis:{axis.name}", numpy.double, (), unit=axis.unit)
@@ -372,25 +374,31 @@ class _StepTriggerMaster(AcquisitionMaster):
         }
         self._motor_pos = self._get_axis_positions_dict()
 
+        # iterate through controllers and (axis, positions) ; iteration results are
+        # grouped by controller
+        # id(item[0].controller) is a trick for groupby to work, just to ensure
+        # that controllers with same id -hence same controllers- come together,
+        # read groupby doc for more explanation
         for controller, axes_pos in groupby(
-            self._motor_pos.items(), lambda item: item[0].controller
+            sorted(self._motor_pos.items(), key=lambda item: id(item[0].controller)),
+            lambda item: item[0].controller,
         ):
             controller.check_limits(axes_pos)
 
         mot_group = Group(*self._axes)
 
         AcquisitionMaster.__init__(self, mot_group, trigger_type=trigger_type, **keys)
-        self._monitor_axes = _init_motor_master_channels(self.channels, self.axes_list)
-
-    @property
-    def axes_list(self):
-        # return a list for axes, **sorted by controller**
-        # (axes with same controller come together),
-        # this is important for the "groupby" call in __init__
-        return sorted(self._axes, key=lambda axis: id(axis.controller))
+        # build the channels list, and return list of axis in the same order as channels
+        # the channels order is important for display; we do not use the sorted axes list
+        # because it is preferrable to have channels in the same order as user specified
+        # in the scan command
+        self._monitor_axes = _init_motor_master_channels(self.channels, self._axes)
 
     def _get_axis_positions_dict(self):
-        return {axis: numpy.linspace(*self._axes[axis]) for axis in self.axes_list}
+        return {
+            axis: numpy.linspace(*start_stop_npoints)
+            for axis, start_stop_npoints in self._axes.items()
+        }
 
     @property
     def npoints(self):
@@ -399,7 +407,7 @@ class _StepTriggerMaster(AcquisitionMaster):
     def __iter__(self):
         for positions in zip(*self._motor_pos.values()):
             self.next_mv_cmd_arg = list()
-            for axis, position in zip(self.axes_list, positions):
+            for axis, position in zip(self._axes, positions):
                 self.next_mv_cmd_arg += [axis, position]
             yield self
 
@@ -414,7 +422,7 @@ class _StepTriggerMaster(AcquisitionMaster):
 
     def trigger(self):
         self.trigger_slaves()
-        positions = [axis.position for axis in self.axes_list + self._monitor_axes]
+        positions = [axis.position for axis in self._monitor_axes]
         self.channels.update_from_iterable(positions)
         self.wait_slaves()
 
@@ -569,16 +577,17 @@ class VariableStepTriggerMaster(AcquisitionMaster):
 
     def trigger(self):
         self.trigger_slaves()
-        axes = self._axes + self._monitor_axes
         if self.broadcast_len > 1:
             self.channels.update_from_iterable(
                 [
                     numpy.ones(self.broadcast_len, numpy.float) * axis.position
-                    for axis in axes
+                    for axis in self._monitor_axes
                 ]
             )
         else:
-            self.channels.update_from_iterable([axis.position for axis in axes])
+            self.channels.update_from_iterable(
+                [axis.position for axis in self._monitor_axes]
+            )
 
         self.wait_slaves()
 
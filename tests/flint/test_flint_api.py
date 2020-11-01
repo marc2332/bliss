603e56642ed934a7ad112cec300f3c267655fc56
Valentin Valls <valentin.valls@esrf.fr>
2020-11-01 19:56:47 +0100

    Use common flint session for custom plot tests

--- tests/flint/test_flint_api.py
+++ tests/flint/test_flint_api.py
@@ -14,18 +14,12 @@ from bliss.controllers.lima import roi as lima_roi
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.skip(reason="This test often segfault the bcu-ci")
-@pytest.mark.usefixtures("flint_norpc")
+@pytest.mark.usefixtures("flint_session")
 class TestFlint(TestCaseQt):
     def test_empty_plot(self):
-        p = plot.plot()
-        pid = plot.get_flint()._pid
-        assert "flint_pid={}".format(pid) in repr(p)
-        assert p.name == "Plot {}".format(p._plot_id)
-
-        p = plot.plot(name="Some name")
-        assert "flint_pid={}".format(pid) in repr(p)
-        assert p.name == "Some name"
+        flint = plot.get_flint()
+        p = flint.get_plot(plot_class="curve", name="foo-empty")
+        assert p is not None
 
     def test_remove_custom_plot(self):
         flint = plot.get_flint()
@@ -34,16 +28,17 @@ class TestFlint(TestCaseQt):
         assert flint.is_plot_exists("foo-rm") is False
 
     def test_custom_plot_curveplot(self):
-        widget = plots.CurvePlot(name="foo")
+        flint = plot.get_flint()
+        p = flint.get_plot(plot_class="curve", name="foo-cp")
 
         cos_data = numpy.cos(numpy.linspace(0, 2 * numpy.pi, 10))
         sin_data = numpy.sin(numpy.linspace(0, 2 * numpy.pi, 10))
 
-        widget.add_data({"cos": cos_data, "sin": sin_data})
-        widget.select_data("sin", "cos")
-        widget.select_data("sin", "cos", color="green", symbol="x")
-        widget.deselect_data("sin", "cos")
-        widget.clear_data()
+        p.add_data({"cos": cos_data, "sin": sin_data})
+        p.select_data("sin", "cos")
+        p.select_data("sin", "cos", color="green", symbol="x")
+        p.deselect_data("sin", "cos")
+        p.clear_data()
 
 
 def test_used_object():

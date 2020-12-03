fc73eda123b591efda510af39db04618c44f7e6b
Valentin Valls <valentin.valls@esrf.fr>
2020-12-03 13:37:56 +0100

    Typo

--- bliss/controllers/lima/limatools.py
+++ bliss/controllers/lima/limatools.py
@@ -153,7 +153,7 @@ def limatake(
 
     scan_info = lima_params
     scan_info.update(acq_params)
-    scan_info["title"] = "limatake {0:.4f} {1}".format(expotime, nbframes)
+    scan_info["title"] = f"limatake {expotime:.4f} {nbframes}"
     scan_info["type"] = "limatake"
     scan = Scan(
         chain,

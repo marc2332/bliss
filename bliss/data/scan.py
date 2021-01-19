e85c77cd186343c299bbab2d47c8e9c12e14dfe8
Valentin Valls <valentin.valls@esrf.fr>
2021-01-19 17:28:20 +0100

    Rework watch_session_scans as a class

--- bliss/data/scan.py
+++ bliss/data/scan.py
@@ -11,6 +11,9 @@ import numpy
 import gevent
 import typing
 import warnings
+
+from typing import Dict
+
 from bliss.common.counter import Counter
 from bliss.common.axis import Axis
 from bliss.data.nodes.scan import get_data_from_nodes
@@ -80,6 +83,254 @@ def get_data(scan):
     return data
 
 
+class ScansObserver:
+    """
+    Observer for the `ScansWatcher`.
+
+    Provides methods which can be inherited to follow the life cycle of the
+    scans of a session.
+    """
+
+    def on_scan_started(self, scan_info: Dict):
+        """
+        Called upon scan start.
+
+        Arguments:
+            scan_info: Dictionary containing scan metadata
+        """
+        pass
+
+    def on_child_created(self, scan_info: Dict, node):
+        """
+        Called upon scan child creation (e.g. channel node)
+
+        Arguments:
+            scan_info: Dictionary containing metadata of this child
+            node: Redis node of this child
+        """
+        pass
+
+    def on_data_received(self, dim: str, top_master: str, data: Dict):
+        """
+        Data processing callback.
+
+        Arguments:
+            dim: One of "0d", "1d", "2d". (for now there is no 3 or 4d data)
+            top_master: Name of the top master
+            data: Structure containing the data, which is not the same for 0d
+                  and for others.
+        """
+        pass
+
+    def on_scan_finished(self, scna_info: Dict):
+        """
+        Called upon scan end.
+
+        Arguments:
+            scan_info: Dictionary containing scan metadata. It can be different
+                       from the one at start.
+        """
+        pass
+
+
+class ScansWatcher:
+    """
+    Watch scans from a specific session.
+
+    Arguments:
+        session_name: Name of the BLISS session
+    """
+
+    def __init__(self, session_name: str):
+        self._session_name = session_name
+        self._exclude_existing_scans = False
+        self._watch_scan_group = False
+        self._observer: ScansObserver = None
+
+        self._started = False
+        """True if processing"""
+
+        self._running_scans = {}
+        """Store running scans"""
+
+    def set_exclude_existing_scans(self, exclude: bool):
+        """
+        Include or exclude existing scans. Default is False.
+
+        Will become True by default in the future.
+
+        It have to be set before start.
+        """
+        assert not self._started
+        self._exclude_existing_scans = exclude
+
+    def set_watch_scan_group(self, watch: bool):
+        """
+        Set to True to include scan groups like any other scans. Default is False.
+
+        It have to be set before start.
+        """
+        assert not self._started
+        self._watch_scan_group = watch
+
+    def set_observer(self, observer: ScansObserver):
+        """
+        Set the observer to use with this watcher process.
+
+        If not set, the `run` method will raise an exception.
+        """
+        assert not self._started
+        self._observer = observer
+
+    def run(self, ready_event=None, stop_handler=None):
+        """
+        Run watching scan events. This method will never ending.
+
+        Any scan node that is created before the `ready_event` will not be watched
+        when `exclude_existing_scans` is True.
+
+        :param Event ready_event: started listening to Redis
+        :param DataStreamReaderStopHandler stop_handler:
+        """
+        assert not self._started
+        self._started = True
+
+        session_node = get_or_create_node(self._session_name, node_type="session")
+        if session_node is None:
+            return
+
+        observer = self._observer
+        if observer is None:
+            raise RuntimeError("No observer was set")
+
+        def _get_scan_info(db_name):
+            for key, scan_dict in self._running_scans.items():
+                if db_name.startswith(key):
+                    return scan_dict["info"], key
+            return None, None
+
+        if self._exclude_existing_scans:
+            exclude_existing_children = "scan", "scan_group"
+        else:
+            exclude_existing_children = None
+
+        for event_type, node, event_data in session_node.walk_on_new_events(
+            stop_handler=stop_handler,
+            exclude_existing_children=exclude_existing_children,
+            started_event=ready_event,
+        ):
+            if event_type == event_type.NEW_NODE:
+                node_type = node.type
+                db_name = node.db_name
+                if node_type == "scan":
+                    # New scan was created
+                    scan_dictionnary = self._running_scans.setdefault(db_name, dict())
+                    if not scan_dictionnary:
+                        scan_info = node.info.get_all()
+                        scan_dictionnary["info"] = scan_info
+                        observer.on_scan_started(scan_info)
+                elif node_type == "scan_group":
+                    if self._watch_scan_group:
+                        # New scan was created
+                        scan_dictionnary = self._running_scans.setdefault(
+                            db_name, dict()
+                        )
+                        if not scan_dictionnary:
+                            scan_info = node.info.get_all()
+                            scan_dictionnary["info"] = scan_info
+                            observer.on_scan_started(scan_info)
+                else:
+                    scan_info, scan_db_name = _get_scan_info(db_name)
+                    if scan_info:  # scan_found
+                        try:
+                            observer.on_child_created(scan_info, node)
+                        except Exception:
+                            sys.excepthook(*sys.exc_info())
+            elif event_type == event_type.NEW_DATA:
+                index, data, description = (
+                    event_data.first_index,
+                    event_data.data,
+                    event_data.description,
+                )
+                db_name = node.db_name
+                if not hasattr(node, "fullname"):
+                    # not a node we want to do anything with here
+                    continue
+
+                fullname = node.fullname
+
+                scan_info, scan_db_name = _get_scan_info(db_name)
+                if scan_info:
+                    nodes_data = self._running_scans[scan_db_name].setdefault(
+                        "nodes_data", dict()
+                    )
+                    if node.type == "channel":
+                        shape = description.get("shape")
+                        dim = len(shape)
+                        # in case of zerod, we keep all data value during the scan
+                        if dim == 0:
+                            prev_data = nodes_data.get(fullname, [])
+                            nodes_data[fullname] = numpy.concatenate((prev_data, data))
+                            for master, channels in scan_info[
+                                "acquisition_chain"
+                            ].items():
+                                channels_set = channels["master"][
+                                    "scalars"
+                                ] + channels.get("scalars", [])
+                                if fullname in channels_set:
+                                    try:
+                                        observer.on_data_received(
+                                            "0d",
+                                            master,
+                                            {
+                                                "data": nodes_data,
+                                                "scan_info": scan_info,
+                                            },
+                                        )
+                                    except Exception:
+                                        sys.excepthook(*sys.exc_info())
+                            continue
+                    elif node.type == "lima":
+                        dim = 2
+
+                    for master, channels in scan_info["acquisition_chain"].items():
+                        other_names: typing.List[str] = []
+                        other_names += channels.get("spectra", [])
+                        other_names += channels.get("images", [])
+                        other_names += channels.get("master", {}).get("images", [])
+                        other_names += channels.get("master", {}).get("spectra", [])
+                        if fullname in other_names:
+                            try:
+                                observer.on_data_received(
+                                    f"{dim}d",
+                                    master,
+                                    {
+                                        "index": index,
+                                        "data": data,
+                                        "description": description,
+                                        "channel_name": fullname,
+                                        "channel_data_node": node,
+                                        "scan_info": scan_info,
+                                    },
+                                )
+                            except Exception:
+                                sys.excepthook(*sys.exc_info())
+
+            elif event_type == event_type.END_SCAN:
+                node_type = node.type
+                if self._watch_scan_group or node_type == "scan":
+                    db_name = node.db_name
+                    scan_dict = self._running_scans.pop(db_name, None)
+                    if scan_dict:
+                        scan_info = node.info.get_all()
+                        try:
+                            observer.on_scan_finished(scan_info)
+                        except Exception:
+                            sys.excepthook(*sys.exc_info())
+
+            gevent.idle()
+
+
 def watch_session_scans(
     session_name,
     scan_new_callback,
@@ -107,127 +358,25 @@ def watch_session_scans(
     if exclude_existing_scans is None:
         exclude_existing_scans = False
         warnings.warn("'exclude_existing_scans' will be True by default", FutureWarning)
-    session_node = get_or_create_node(session_name, node_type="session")
-    if session_node is None:
-        return
-    running_scans = dict()
 
-    def _get_scan_info(db_name):
-        for key, scan_dict in running_scans.items():
-            if db_name.startswith(key):
-                return scan_dict["info"], key
-        return None, None
+    watcher = ScansWatcher(session_name)
+    watcher.set_exclude_existing_scans(exclude_existing_scans)
+    watcher.set_watch_scan_group(watch_scan_group)
 
-    if exclude_existing_scans:
-        exclude_existing_children = "scan", "scan_group"
-    else:
-        exclude_existing_children = None
-    for event_type, node, event_data in session_node.walk_on_new_events(
-        stop_handler=stop_handler,
-        exclude_existing_children=exclude_existing_children,
-        started_event=ready_event,
-    ):
-        if event_type == event_type.NEW_NODE:
-            node_type = node.type
-            db_name = node.db_name
-            if node_type == "scan":
-                # New scan was created
-                scan_dictionnary = running_scans.setdefault(db_name, dict())
-                if not scan_dictionnary:
-                    scan_info = node.info.get_all()
-                    scan_dictionnary["info"] = scan_info
-                    scan_new_callback(scan_info)
-            elif node_type == "scan_group":
-                if watch_scan_group:
-                    # New scan was created
-                    scan_dictionnary = running_scans.setdefault(db_name, dict())
-                    if not scan_dictionnary:
-                        scan_info = node.info.get_all()
-                        scan_dictionnary["info"] = scan_info
-                        scan_new_callback(scan_info)
-            else:
-                scan_info, scan_db_name = _get_scan_info(db_name)
-                if scan_info:  # scan_found
-                    try:
-                        scan_new_child_callback(scan_info, node)
-                    except Exception:
-                        sys.excepthook(*sys.exc_info())
-        elif event_type == event_type.NEW_DATA:
-            index, data, description = (
-                event_data.first_index,
-                event_data.data,
-                event_data.description,
-            )
-            db_name = node.db_name
-            if not hasattr(node, "fullname"):
-                # not a node we want to do anything with here
-                continue
+    class Observer(ScansObserver):
+        def on_scan_started(self, scan_info: Dict):
+            scan_new_callback(scan_info)
 
-            fullname = node.fullname
+        def on_scan_finished(self, scna_info: Dict):
+            if scan_end_callback is not None:
+                scan_end_callback(scna_info)
 
-            scan_info, scan_db_name = _get_scan_info(db_name)
-            if scan_info:
-                nodes_data = running_scans[scan_db_name].setdefault(
-                    "nodes_data", dict()
-                )
-                if node.type == "channel":
-                    shape = description.get("shape")
-                    dim = len(shape)
-                    # in case of zerod, we keep all data value during the scan
-                    if dim == 0:
-                        prev_data = nodes_data.get(fullname, [])
-                        nodes_data[fullname] = numpy.concatenate((prev_data, data))
-                        for master, channels in scan_info["acquisition_chain"].items():
-                            channels_set = channels["master"]["scalars"] + channels.get(
-                                "scalars", []
-                            )
-                            if fullname in channels_set:
-                                try:
-                                    scan_data_callback(
-                                        "0d",
-                                        master,
-                                        {"data": nodes_data, "scan_info": scan_info},
-                                    )
-                                except Exception:
-                                    sys.excepthook(*sys.exc_info())
-                        continue
-                elif node.type == "lima":
-                    dim = 2
+        def on_child_created(self, scan_info: Dict, node):
+            scan_new_child_callback(scan_info, node)
 
-                for master, channels in scan_info["acquisition_chain"].items():
-                    other_names: typing.List[str] = []
-                    other_names += channels.get("spectra", [])
-                    other_names += channels.get("images", [])
-                    other_names += channels.get("master", {}).get("images", [])
-                    other_names += channels.get("master", {}).get("spectra", [])
-                    if fullname in other_names:
-                        try:
-                            scan_data_callback(
-                                f"{dim}d",
-                                master,
-                                {
-                                    "index": index,
-                                    "data": data,
-                                    "description": description,
-                                    "channel_name": fullname,
-                                    "channel_data_node": node,
-                                    "scan_info": scan_info,
-                                },
-                            )
-                        except Exception:
-                            sys.excepthook(*sys.exc_info())
+        def on_data_received(self, dim: str, top_master: str, data: Dict):
+            scan_data_callback(dim, top_master, data)
 
-        elif event_type == event_type.END_SCAN:
-            node_type = node.type
-            if watch_scan_group or node_type == "scan":
-                db_name = node.db_name
-                scan_dict = running_scans.pop(db_name, None)
-                if scan_dict:
-                    scan_info = node.info.get_all()
-                    if scan_end_callback:
-                        try:
-                            scan_end_callback(scan_info)
-                        except Exception:
-                            sys.excepthook(*sys.exc_info())
-
-        gevent.idle()
+    observer = Observer()
+    watcher.set_observer(observer)
+    watcher.run(ready_event=ready_event, stop_handler=stop_handler)

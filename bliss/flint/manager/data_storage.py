ca5882a852230d645faf2ceb62bf02898dc11720
Valentin Valls <valentin.valls@esrf.fr>
2021-02-19 10:57:16 +0100

    Mitigate use of concate for very fast scans

--- bliss/flint/manager/data_storage.py
+++ bliss/flint/manager/data_storage.py
@@ -19,6 +19,27 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
+class _Data:
+    def __init__(self):
+        self.__items = []
+        self.__data = []
+        self.__size = 0
+
+    def size(self) -> int:
+        return self.__size
+
+    def append(self, array):
+        self.__items.append(array)
+        self.__size += len(array)
+
+    def array(self):
+        """Returns a contiguous numpy array"""
+        if self.__items != []:
+            self.__data = numpy.concatenate((self.__data, *self.__items))
+            self.__items = []
+        return self.__data
+
+
 class DataStorage:
     def __init__(self):
         self.__data: Dict[str, numpy.ndarray] = {}
@@ -41,10 +62,16 @@ class DataStorage:
         return channel_name in self.__groups
 
     def get_data_else_none(self, channel_name) -> Optional[numpy.ndarray]:
-        return self.__data.get(channel_name, None)
+        holder = self.__data.get(channel_name, None)
+        if holder is None:
+            return None
+        return holder.array()
 
     def get_data(self, channel_name) -> numpy.ndarray:
-        return self.__data[channel_name]
+        return self.__data[channel_name].array()
+
+    def _get_data_size(self, channel_name):
+        return self.__data[channel_name].size()
 
     def get_last_group_size(self, group_name: str) -> int:
         """Returns the last stored group size."""
@@ -58,8 +85,7 @@ class DataStorage:
         size = None
         for channel_name in self.__group[group_name]:
             if channel_name in self.__data:
-                data = self.get_data(channel_name)
-                data_size = len(data)
+                data_size = self._get_data_size(channel_name)
             else:
                 # This channel is not yet there
                 # Then it's the smaller one
@@ -67,7 +93,7 @@ class DataStorage:
             if size is None:
                 size = data_size
             elif data_size < size:
-                size = len(data)
+                size = data_size
         assert size is not None
         if self.get_last_group_size(group_name) == size:
             return None
@@ -77,12 +103,16 @@ class DataStorage:
     def append_data(self, channel_name: str, data: numpy.ndarray):
         # NOTE: We could avoid reallocation by allocating bigger arrays and use
         # memory view. But this was not speeding up anything for tested use cases
-        prev_data = self.__data.get(channel_name, [])
-        data = numpy.concatenate((prev_data, data))
-        self.__data[channel_name] = data
+        holder = self.__data.get(channel_name, None)
+        if holder is None:
+            holder = _Data()
+            self.__data[channel_name] = holder
+        holder.append(data)
 
     def set_data(self, channel_name: str, data: numpy.ndarray):
-        self.__data[channel_name] = data
+        holder = _Data()
+        holder.append(data)
+        self.__data[channel_name] = holder
 
     def groups(self) -> List[str]:
         return list(self.__group.keys())

d9924a94f1fd7b73b0a2671fa2d8016846845249
Valentin Valls <valentin.valls@esrf.fr>
2021-03-10 10:15:30 +0100

    Remove ExtTestLogging (the patch is part of silx)

--- tests/flint_client/test_flint_factory.py
+++ tests/flint_client/test_flint_factory.py
@@ -1,8 +1,6 @@
 """Testing Flint creating."""
 
 import time
-import os
-import signal
 import pytest
 import gevent
 import psutil
@@ -11,17 +9,6 @@ from contextlib import contextmanager
 from silx.utils import testutils
 from bliss.common import plot
 from bliss.flint.client import proxy
-
-
-class ExtTestLogging(testutils.TestLogging):
-    def __exit__(self, exc_type, exc_value, traceback):
-        try:
-            testutils.TestLogging.__exit__(self, exc_type, exc_value, traceback)
-        except RuntimeError:
-            # In case of problem, display the received logs
-            for r in self.records:
-                print(r.levelname, r.name, self.format(r))
-            raise
 
 
 @contextmanager
@@ -58,7 +45,7 @@ def test_created_flint(flint_session):
     flint = plot.get_flint(creation_allowed=False)
 
     # Check messages and stdout
-    listener = ExtTestLogging(proxy.FLINT_OUTPUT_LOGGER.name, info=1)
+    listener = testutils.TestLogging(proxy.FLINT_OUTPUT_LOGGER.name, info=1)
     with listener:
         flint.ping()
         for _ in range(10):
@@ -74,7 +61,7 @@ def test_attached_flint(attached_flint_session):
     """
     flint = plot.get_flint()
     # Check messages and stdout
-    listener = ExtTestLogging(proxy.FLINT_OUTPUT_LOGGER.name, info=1)
+    listener = testutils.TestLogging(proxy.FLINT_OUTPUT_LOGGER.name, info=1)
     with listener:
         flint.ping()
         for _ in range(10):

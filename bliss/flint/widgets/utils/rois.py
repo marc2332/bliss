803ce8c8e77999204276d33362b92702bd294b21
Valentin Valls <valentin.valls@esrf.fr>
2021-03-10 18:37:00 +0100

    Add a size label to the Lima ROIs

--- bliss/flint/widgets/utils/rois.py
+++ bliss/flint/widgets/utils/rois.py
@@ -27,11 +27,18 @@ class LimaRectRoi(silx_rois.RectangleROI):
     def __init__(self, parent=None):
         silx_rois.RectangleROI.__init__(self, parent=parent)
         self.sigEditingFinished.connect(self.__normalizeGeometry)
+        self.__sizeLabel = self.addLabelHandle()
 
     def setFirstShapePoints(self, points):
         # Normalize the ROI position to the pixel
         points = points.astype(int)
         silx_rois.RectangleROI.setFirstShapePoints(self, points)
+
+    def _updateGeometry(self, origin=None, size=None, center=None):
+        silx_rois.RectangleROI._updateGeometry(
+            self, origin=origin, size=size, center=center
+        )
+        self.__updateSizeLabel()
 
     def __normalizeGeometry(self):
         # Normalize the ROI position to the pixel
@@ -39,7 +46,14 @@ class LimaRectRoi(silx_rois.RectangleROI):
         pos1 = self.getOrigin()
         pos2 = (pos1 + self.getSize() + pixelcenter).astype(int)
         pos1 = (pos1 + pixelcenter).astype(int)
-        self.setGeometry(origin=pos1, size=pos2 - pos1)
+        size = pos2 - pos1
+        self.setGeometry(origin=pos1, size=size)
+
+    def __updateSizeLabel(self):
+        size = self.getSize().astype(int)
+        pos = self.getOrigin().astype(int)
+        self.__sizeLabel.setText(f"{size[0]}x{size[1]}")
+        self.__sizeLabel.setPosition(*(pos + size * 0.5))
 
 
 class LimaArcRoi(silx_rois.ArcROI):

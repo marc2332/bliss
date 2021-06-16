9b6329ca0614c86ae320f520c5c0fc705e25dc47
Valentin Valls <valentin.valls@esrf.fr>
2021-06-16 13:03:30 +0200

    Use the UserCheckBoxList to select the selected list of counters

--- bliss/shell/dialog/common/measurementgroups.py
+++ bliss/shell/dialog/common/measurementgroups.py
@@ -1,27 +1,30 @@
 from bliss.shell.dialog.helpers import dialog
-from bliss.shell.cli.user_dialog import UserCheckBox
+from bliss.shell.cli.user_dialog import UserCheckBoxList
 from bliss.shell.cli.pt_widgets import BlissDialog
 
 
 @dialog("MeasurementGroup", "selection")
 def measurement_group_selection(obj, *args, **kwargs):
-    dialogs = []
-    counters_info = []
+
+    values = []
+    selection = []
     for fullname in obj.available:
+        label = fullname
+        values.append((fullname, label))
         if fullname in obj.enabled:
-            counters_info.append((fullname, True))
-        else:
-            counters_info.append((fullname, False))
+            selection.append(fullname)
 
-    for fullname, enabled in counters_info:
-        dialogs.append([UserCheckBox(label=fullname, defval=enabled)])
-
-    choices = BlissDialog(
-        dialogs, title=f"MeasurementGroup {obj.name} Counter selection", paddings=(0, 0)
+    widget = UserCheckBoxList(label="Counters", values=values, defval=selection)
+    result = BlissDialog(
+        [[widget]],
+        title=f"MeasurementGroup {obj.name} Counter selection",
+        paddings=(0, 0),
     ).show()
-    if choices:
-        values = list(zip(obj.available, choices.values()))
-        for fullname, enabled in values:
+
+    if result:
+        selection = set(result[widget])
+        for fullname, label in values:
+            enabled = fullname in selection
             if enabled:
                 obj.enable(fullname)
             else:
